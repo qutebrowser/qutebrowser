1e636eaba260d579cf1d3270451425df9dc238cb
Florian Bruhin <git@the-compiler.org>
2014-01-27 23:19:03 +0100

    Add missing utils module

---
+++ qutebrowser/utils/__init__.py
@@ -0,0 +1,8 @@
+from PyQt5.QtCore import QUrl
+
+def qurl(url):
+    if isinstance(url, QUrl):
+        return url
+    if not url.startswith('http://'):
+        url = 'http://' + url
+    return QUrl(url)
