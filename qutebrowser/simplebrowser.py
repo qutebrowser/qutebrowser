299b887531c9116e51ee1c8ab538dfb60b1d27d4
Florian Bruhin <git@the-compiler.org>
2014-01-28 15:58:37 +0100

    Add simplebrowser.py

---
+++ qutebrowser/simplebrowser.py
@@ -0,0 +1,13 @@
+"""Simple browser for testing purposes"""
+
+import sys
+
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWebKitWidgets import QWebView
+
+app = QApplication(sys.argv)
+wv = QWebView()
+wv.load(QUrl(sys.argv[1]))
+wv.show()
+app.exec_()
