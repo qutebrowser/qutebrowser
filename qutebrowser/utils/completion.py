a0bcfe19604361f564fac526c3a182ed0413faba
Florian Bruhin <git@the-compiler.org>
2014-02-04 18:46:33 +0100

    Add FIXME for self.invalidate()

--- qutebrowser/utils/completion.py
+++ qutebrowser/utils/completion.py
@@ -314,6 +314,10 @@ class CompletionFilterModel(QSortFilterProxyModel):
                 srcmodel.sort(sortcol)
             except NotImplementedError:
                 self.sort(sortcol)
+            # FIXME  we should call self.invalidate() here so sorting actually works.
+            # However, this seems to provoke a lot of weird problems, like
+            # random race condition segfaults :(
+            # self.invalidate()
 
     def filterAcceptsRow(self, row, parent):
         """Custom filter implementation.
