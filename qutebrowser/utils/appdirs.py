638fdca573a31f985dc673dbec5cff84c670bf7e
Florian Bruhin <git@the-compiler.org>
2014-03-25 06:23:55 +0100

    appdirs.py update

--- qutebrowser/utils/appdirs.py
+++ qutebrowser/utils/appdirs.py
@@ -49,7 +49,7 @@ def user_data_dir(appname=None, appauthor=None, version=None, roaming=False):
         Win 7  (roaming):       C:\Users\<username>\AppData\Roaming\<AppAuthor>\<AppName>
 
     For Unix, we follow the XDG spec and support $XDG_DATA_HOME.
-    That means, by deafult "~/.local/share/<AppName>".
+    That means, by default "~/.local/share/<AppName>".
     """
     if sys.platform == "win32":
         if appauthor is None:
@@ -158,8 +158,8 @@ def user_config_dir(appname=None, appauthor=None, version=None, roaming=False):
         Unix:                   ~/.config/<AppName>     # or in $XDG_CONFIG_HOME, if defined
         Win *:                  same as user_data_dir
 
-    For Unix, we follow the XDG spec and support $XDG_DATA_HOME.
-    That means, by deafult "~/.local/share/<AppName>".
+    For Unix, we follow the XDG spec and support $XDG_CONFIG_HOME.
+    That means, by deafult "~/.config/<AppName>".
     """
     if sys.platform in [ "win32", "darwin" ]:
         path = user_data_dir(appname, appauthor, None, roaming)
