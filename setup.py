f5968643245bfffcc01ae0f7808f0522135c2869
Florian Bruhin <git@the-compiler.org>
2014-04-28 23:20:24 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+import qutebrowser
+
+def read_file(name):
+    with open(name) as f:
+        return f.read()
+
+setup(
+    name='qutebrowser',
+    version=qutebrowser.__version__,
+    description="A keyboard-driven, vim-like browser based on PyQt5 and QtWebKit.",
+    long_description=read_file('README'),
+    url='http://www.qutebrowser.org/',
+    author="Florian Bruhin",
+    author_email='me@qutebrowser.org',
+    license='GPL',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: X11 Applications :: Qt',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Microsoft :: Windows :: Windows XP',
+        'Operating System :: Microsoft :: Windows :: Windows 7',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Browsers',
+    ],
+    keywords='pyqt browser web qt webkit',
+    packages=find_packages(exclude=['qutebrowser.test']),
+    package_data={ 'qutebrowser': ['html/*.html'] },
+    entry_points={'gui_scripts': [ 'qutebrowser = qutebrowser.__main__:main' ],},
+)
