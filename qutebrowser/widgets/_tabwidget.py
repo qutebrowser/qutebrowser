63ce8e0966834d6c51ecf3aa838ccad317709bee
Florian Bruhin <git@the-compiler.org>
2014-05-09 17:59:13 +0200

    Set min-/max-width for tabs

--- qutebrowser/widgets/_tabwidget.py
+++ qutebrowser/widgets/_tabwidget.py
@@ -49,6 +49,8 @@ class TabWidget(QTabWidget):
             {color[tab.fg]}
             padding: 0px 5px 0px 5px;
             border-right: 2px solid {color[tab.seperator]};
+            min-width: 100px;
+            max-width: 200px;
         }}
 
         QTabBar::tab:selected {{
