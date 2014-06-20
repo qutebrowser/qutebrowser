917bc820a4362e185b441be1bc0ad0aefb8e9a7e
Florian Bruhin <git@the-compiler.org>
2014-06-20 08:14:52 +0200

    Fix missing list in setupcommon.py

--- scripts/setupcommon.py
+++ scripts/setupcommon.py
@@ -92,7 +92,7 @@ setupdata = {
                     "QtWebKit."),
     'long_description': read_file('README'),
     'url': 'http://www.qutebrowser.org/',
-    'requires': 'rfc6266',
+    'requires': ['rfc6266'],
     'author': _get_constant('author'),
     'author_email': _get_constant('email'),
     'license': _get_constant('license'),
