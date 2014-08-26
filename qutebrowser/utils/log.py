45608ee9f8623640df22a7865936d0d03baaeb19
Florian Bruhin <git@the-compiler.org>
2014-08-26 06:35:58 +0200

    Add bugs URL to colorama issue

--- qutebrowser/utils/log.py
+++ qutebrowser/utils/log.py
@@ -43,7 +43,9 @@ except ImportError:
     ColoredFormatter = None
 else:
     # colorlog calls colorama.init() which we don't want, also it breaks our
-    # sys.stdout/sys.stderr if they are None.
+    # sys.stdout/sys.stderr if they are None. Bugreports:
+    # https://code.google.com/p/colorama/issues/detail?id=61
+    # https://github.com/borntyping/python-colorlog/issues/13
     if colorama is not None:
         colorama.deinit()
 
