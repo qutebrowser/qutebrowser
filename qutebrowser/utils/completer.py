qutebrowser/qutebrowser

commit fb3682f5fa9ff702488277521e60c5425cafc64d

Load completion with a singleShot QTimer.
It still slows stuff down while typing, but at least it's a lot more responsive
for the initial load.

See #189.

qutebrowser/utils/completer.py
@@ -19,7 +19,9 @@
 
 """Completer attached to a CompletionView."""
 
-from PyQt5.QtCore import pyqtSlot, pyqtSignal, QObject
+import functools
+
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, QObject, QTimer
 
 from qutebrowser.config import config, configdata
 from qutebrowser.commands import cmdutils
@@ -181,9 +183,19 @@ class Completer(QObject):
 
     @pyqtSlot(str, list, int)
     def on_update_completion(self, prefix, parts, cursor_part):
-        """Check if completions are available and activate them.
+        """Schedule updating/enabling completion.
 
         Slot for the textChanged signal of the statusbar command widget.
+
+        For performance reasons we don't want to block here, instead we do this
+        in the background.
+        """
+        QTimer.singleShot(0, functools.partial(
+            self._on_update_completion, prefix, parts, cursor_part))
+
+    @pyqtSlot(str, list, int)
+    def _on_update_completion(self, prefix, parts, cursor_part):
+        """Check if completions are available and activate them.
 
         Args:
             text: The new text

commit e8fa8fadceb40866c8f16c775ec5d80f5af325e2

Fix completion tabbing.
I accidentally broke this in fb3682f5fa9ff702488277521e60c5425cafc64d because
the variable gets reset before the slot is executed now.

See #189.

qutebrowser/utils/completer.py
@@ -179,7 +179,6 @@ class Completer(QObject):
         else:
             self._ignore_change = True
             self.change_completed_part.emit(data, False)
-            self._ignore_change = False
 
     @pyqtSlot(str, list, int)
     def on_update_completion(self, prefix, parts, cursor_part):
@@ -202,6 +201,7 @@ class Completer(QObject):
             cursor_part: The part the cursor is currently over.
         """
         if self._ignore_change:
+            self._ignore_change = False
             log.completion.debug("Ignoring completion update")
             return
 