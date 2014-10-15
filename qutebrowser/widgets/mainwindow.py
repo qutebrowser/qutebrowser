999474c751bcef4d4dfe0bf03eba4652674cbb03
Florian Bruhin <git@the-compiler.org>
2014-10-15 23:24:09 +0200

    Set title directly instead of using signals.
    See #198, but this didn't fix it.

--- qutebrowser/widgets/mainwindow.py
+++ qutebrowser/widgets/mainwindow.py
@@ -87,7 +87,6 @@ class MainWindow(QWidget):
         self._downloadview.show()
 
         self._tabbed_browser = tabbedbrowser.TabbedBrowser(win_id)
-        self._tabbed_browser.title_changed.connect(self.setWindowTitle)
         objreg.register('tabbed-browser', self._tabbed_browser, scope='window',
                         window=win_id)
         self._vbox.addWidget(self._tabbed_browser)
