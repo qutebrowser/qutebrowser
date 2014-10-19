dbd7f2a16bb9ecd3be894b1a5f6745f8b4fac771
Florian Bruhin <git@the-compiler.org>
2014-10-20 00:30:16 +0200

    Update ez_setup to 7.0

--- scripts/ez_setup.py
+++ scripts/ez_setup.py
@@ -36,7 +36,7 @@ try:
 except ImportError:
     USER_SITE = None
 
-DEFAULT_VERSION = "6.1"
+DEFAULT_VERSION = "7.0"
 DEFAULT_URL = "https://pypi.python.org/packages/source/s/setuptools/"
 
 def _python_cmd(*args):
