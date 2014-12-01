cd5f77210c420d0d2288d1dc27ebcaf7dc3aa9ed
Florian Bruhin <git@the-compiler.org>
2014-12-01 23:11:18 +0100

    Add keybinding Ss to open qute:settings.
    See #13.

--- qutebrowser/config/configdata.py
+++ qutebrowser/config/configdata.py
@@ -1018,6 +1018,7 @@ KEY_DATA = collections.OrderedDict([
         ('home', ['<Ctrl-h>']),
         ('stop', ['<Ctrl-s>']),
         ('print', ['<Ctrl-Alt-p>']),
+        ('open qute:settings', ['Ss']),
     ])),
 
     ('insert', collections.OrderedDict([
