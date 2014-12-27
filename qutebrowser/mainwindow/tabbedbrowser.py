89c7f3ecfedd3a0d3f1f9b6385f3580f88310e8c
Florian Bruhin <git@the-compiler.org>
2014-12-28 00:41:50 +0100

    Re-focus web view when leaving prompt/yesno mode.

--- qutebrowser/mainwindow/tabbedbrowser.py
+++ qutebrowser/mainwindow/tabbedbrowser.py
@@ -508,9 +508,11 @@ class TabbedBrowser(tabwidget.TabWidget):
     @pyqtSlot(usertypes.KeyMode)
     def on_mode_left(self, mode):
         """Give focus to current tab if command mode was left."""
-        if mode == usertypes.KeyMode.command:
+        if mode in (usertypes.KeyMode.command, usertypes.KeyMode.prompt,
+                    usertypes.KeyMode.yesno):
             widget = self.currentWidget()
-            log.modes.debug("Left command mode, focusing {!r}".format(widget))
+            log.modes.debug("Left status-input mode, focusing {!r}".format(
+                widget))
             if widget is None:
                 return
             widget.setFocus()
