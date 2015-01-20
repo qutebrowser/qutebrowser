qutebrowser/qutebrowser

commit 6a7e454789b8b47ad58b8261b32a69f9f0bc592c

Add proper virtualenv support to run_checks.py

scripts/run_checks.py
@@ -91,10 +91,15 @@ def run(name, target=None, print_version=False):
     if target is not None:
         args.append(target)
     with _adjusted_pythonpath(name):
+        # for virtualenvs
+        executable = os.path.join(os.path.dirname(sys.executable), name)
+        if not os.path.exists(executable):
+            # in $PATH
+            executable = name
         if print_version:
-            subprocess.call([name, '--version'])
+            subprocess.call([executable, '--version'])
         try:
-            status = subprocess.call([name] + args)
+            status = subprocess.call([executable] + args)
         except OSError:
             traceback.print_exc()
             status = None

commit 532ec30d00ddc0a1da31e3fecb542efcb04a781b

Fix executing of virtualenv pylint on Windows.
6a7e454789b8b47ad58b8261b32a69f9f0bc592c broke executing pylint on Windows,
because there was a pylint Python script in vev\Scripts, and subprocess tried
to execute that instead of the .exe.

scripts/run_checks.py
@@ -91,8 +91,12 @@ def run(name, target=None, print_version=False):
     if target is not None:
         args.append(target)
     with _adjusted_pythonpath(name):
+        if os.name == 'nt':
+            exename = name + '.exe'
+        else:
+            exename = name
         # for virtualenvs
-        executable = os.path.join(os.path.dirname(sys.executable), name)
+        executable = os.path.join(os.path.dirname(sys.executable), exename)
         if not os.path.exists(executable):
             # in $PATH
             executable = name