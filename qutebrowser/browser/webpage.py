qutebrowser/qutebrowser

commit 8f1d81a6441ef9df88799ca9969122fb7fcf4f3b

Add session support.
Closes #12.
See #499.
See #11.

This adds PyYAML as a new dependency.

It adds the following new commands:

    :session-delete <name>
    Delete a session.

    :session-load <name>
    Load a session.

    :session-save [<name>]
    Save a session.

    :wq [<name>]
    Save open pages and quit.

And the following new settings:

    general -> save-session:
    Whether to always save the open pages.

qutebrowser/browser/webpage.py
@@ -21,7 +21,8 @@
 
 import functools
 
-from PyQt5.QtCore import pyqtSlot, pyqtSignal, PYQT_VERSION, Qt, QUrl
+from PyQt5.QtCore import (pyqtSlot, pyqtSignal, PYQT_VERSION, Qt, QUrl, QPoint,
+                          QTimer)
 from PyQt5.QtGui import QDesktopServices
 from PyQt5.QtNetwork import QNetworkReply, QNetworkRequest
 from PyQt5.QtWidgets import QFileDialog
@@ -29,7 +30,7 @@ from PyQt5.QtPrintSupport import QPrintDialog
 from PyQt5.QtWebKitWidgets import QWebPage
 
 from qutebrowser.config import config
-from qutebrowser.browser import http
+from qutebrowser.browser import http, tabhistory
 from qutebrowser.browser.network import networkmanager
 from qutebrowser.utils import (message, usertypes, log, jinja, qtutils, utils,
                                objreg)
@@ -73,6 +74,10 @@ class BrowserPage(QWebPage):
         self.loadStarted.connect(self.on_load_started)
         self.featurePermissionRequested.connect(
             self.on_feature_permission_requested)
+        self.saveFrameStateRequested.connect(
+            self.on_save_frame_state_requested)
+        self.restoreFrameStateRequested.connect(
+            self.on_restore_frame_state_requested)
 
     if PYQT_VERSION > 0x050300:
         # WORKAROUND (remove this when we bump the requirements to 5.3.1)
@@ -213,6 +218,23 @@ class BrowserPage(QWebPage):
         else:
             nam.shutdown()
 
+    def load_history(self, entries):
+        """Load the history from a list of TabHistoryItem objects."""
+        stream, _data, user_data = tabhistory.serialize(entries)
+        history = self.history()
+        qtutils.deserialize_stream(stream, history)
+        for i, data in enumerate(user_data):
+            history.itemAt(i).setUserData(data)
+        cur_data = history.currentItem().userData()
+        if cur_data is not None:
+            frame = self.mainFrame()
+            if 'zoom' in cur_data:
+                frame.setZoomFactor(cur_data['zoom'])
+            if ('scroll-pos' in cur_data and
+                    frame.scrollPosition() == QPoint(0, 0)):
+                QTimer.singleShot(0, functools.partial(
+                    frame.setScrollPosition, cur_data['scroll-pos']))
+
     def display_content(self, reply, mimetype):
         """Display a QNetworkReply with an explicitely set mimetype."""
         self.mainFrame().setContent(reply.readAll(), mimetype, reply.url())
@@ -337,6 +359,37 @@ class BrowserPage(QWebPage):
         """
         if frame is cancelled_frame and feature == cancelled_feature:
             question.abort()
+
+    def on_save_frame_state_requested(self, frame, item):
+        """Save scroll position and zoom in history.
+
+        Args:
+            frame: The QWebFrame which gets saved.
+            item: The QWebHistoryItem to be saved.
+        """
+        if frame != self.mainFrame():
+            return
+        data = {
+            'zoom': frame.zoomFactor(),
+            'scroll-pos': frame.scrollPosition(),
+        }
+        item.setUserData(data)
+
+    def on_restore_frame_state_requested(self, frame):
+        """Restore scroll position and zoom from history.
+
+        Args:
+            frame: The QWebFrame which gets restored.
+        """
+        if frame != self.mainFrame():
+            return
+        data = self.history().currentItem().userData()
+        if data is None:
+            return
+        if 'zoom' in data:
+            frame.setZoomFactor(data['zoom'])
+        if 'scroll-pos' in data and frame.scrollPosition() == QPoint(0, 0):
+            frame.setScrollPosition(data['scroll-pos'])
 
     def userAgentForUrl(self, url):
         """Override QWebPage::userAgentForUrl to customize the user agent."""

.flake8
@@ -6,5 +6,6 @@
 # F841: unused variable
 # F401: Unused import
 # E402: module level import not at top of file
-ignore=E265,E501,F841,F401,E402
+# E266: too many leading '#' for block comment
+ignore=E265,E501,F841,F401,E402,E266
 max_complexity = 12

README.asciidoc
@@ -93,6 +93,7 @@ The following software and libraries are required to run qutebrowser:
 * http://fdik.org/pyPEG/[pyPEG2]
 * http://jinja.pocoo.org/[jinja2]
 * http://pygments.org/[pygments]
+* http://pyyaml.org/wiki/PyYAML[PyYAML]
 
 To generate the documentation for the `:help` command, when using the git
 repository (rather than a release), http://asciidoc.org/[asciidoc] is needed.

doc/help/commands.asciidoc
@@ -36,6 +36,9 @@
 |<<restart,restart>>|Restart qutebrowser while keeping existing tabs open.
 |<<save,save>>|Save configs and state.
 |<<search,search>>|Search for a text on the current page.
+|<<session-delete,session-delete>>|Delete a session.
+|<<session-load,session-load>>|Load a session.
+|<<session-save,session-save>>|Save a session.
 |<<set,set>>|Set an option.
 |<<set-cmd-text,set-cmd-text>>|Preset the statusbar to some text.
 |<<spawn,spawn>>|Spawn a command in a shell.
@@ -50,6 +53,7 @@
 |<<unbind,unbind>>|Unbind a keychain.
 |<<undo,undo>>|Re-open a closed tab (optionally skipping [count] closed tabs).
 |<<view-source,view-source>>|Show the source of the current page.
+|<<wq,wq>>|Save open pages and quit.
 |<<yank,yank>>|Yank the current URL/title to the clipboard or primary selection.
 |<<zoom,zoom>>|Set the zoom level for the current tab.
 |<<zoom-in,zoom-in>>|Increase the zoom level for the current tab.
@@ -396,6 +400,33 @@ Search for a text on the current page.
 ==== optional arguments
 * +*-r*+, +*--reverse*+: Reverse search direction.
 
+[[session-delete]]
+=== session-delete
+Syntax: +:session-delete 'name'+
+
+Delete a session.
+
+==== positional arguments
+* +'name'+: The name of the session.
+
+[[session-load]]
+=== session-load
+Syntax: +:session-load 'name'+
+
+Load a session.
+
+==== positional arguments
+* +'name'+: The name of the session.
+
+[[session-save]]
+=== session-save
+Syntax: +:session-save ['name']+
+
+Save a session.
+
+==== positional arguments
+* +'name'+: The name of the session.
+
 [[set]]
 === set
 Syntax: +:set [*--temp*] [*--print*] ['section'] ['option'] ['value']+
@@ -537,6 +568,15 @@ Re-open a closed tab (optionally skipping [count] closed tabs).
 [[view-source]]
 === view-source
 Show the source of the current page.
+
+[[wq]]
+=== wq
+Syntax: +:wq ['name']+
+
+Save open pages and quit.
+
+==== positional arguments
+* +'name'+: The name of the session.
 
 [[yank]]
 === yank

doc/help/settings.asciidoc
@@ -20,6 +20,7 @@
 |<<general-default-encoding,default-encoding>>|Default encoding to use for websites.
 |<<general-new-instance-open-target,new-instance-open-target>>|How to open links in an existing instance if a new one is launched.
 |<<general-log-javascript-console,log-javascript-console>>|Whether to log javascript console messages.
+|<<general-save-session,save-session>>|Whether to always save the open pages.
 |==============
 
 .Quick reference for section ``ui''
@@ -378,6 +379,17 @@ Default: +pass:[window]+
 [[general-log-javascript-console]]
 === log-javascript-console
 Whether to log javascript console messages.
+
+Valid values:
+
+ * +true+
+ * +false+
+
+Default: +pass:[false]+
+
+[[general-save-session]]
+=== save-session
+Whether to always save the open pages.
 
 Valid values:
 

doc/qutebrowser.1.asciidoc
@@ -43,6 +43,9 @@ It was inspired by other browsers/addons like dwb and Vimperator/Pentadactyl.
 *-s* 'SECTION' 'OPTION' 'VALUE', *--set* 'SECTION' 'OPTION' 'VALUE'::
     Set a temporary setting for this session.
 
+*-r* 'SESSION', *--restore* 'SESSION'::
+    Restore a named session.
+
 === debug arguments
 *-l* 'LOGLEVEL', *--loglevel* 'LOGLEVEL'::
     Set loglevel

qutebrowser/app.py
@@ -43,7 +43,8 @@ from qutebrowser.config import style, config, websettings, configexc
 from qutebrowser.browser import quickmarks, cookies, cache, adblock, history
 from qutebrowser.browser.network import qutescheme, proxy
 from qutebrowser.mainwindow import mainwindow
-from qutebrowser.misc import crashdialog, readline, ipc, earlyinit, savemanager
+from qutebrowser.misc import (crashdialog, readline, ipc, earlyinit,
+                              savemanager, sessions)
 from qutebrowser.misc import utilcmds  # pylint: disable=unused-import
 from qutebrowser.keyinput import modeman
 from qutebrowser.utils import (log, version, message, utils, qtutils, urlutils,
@@ -179,6 +180,9 @@ class Application(QApplication):
         history.init()
         log.init.debug("Initializing crashlog...")
         self._handle_segfault()
+        log.init.debug("Initializing sessions...")
+        session_manager = sessions.SessionManager(self)
+        objreg.register('session-manager', session_manager)
         log.init.debug("Initializing js-bridge...")
         js_bridge = qutescheme.JSBridge(self)
         objreg.register('js-bridge', js_bridge)
@@ -199,11 +203,13 @@ class Application(QApplication):
         log.init.debug("Initializing cache...")
         diskcache = cache.DiskCache(self)
         objreg.register('cache', diskcache)
-        log.init.debug("Initializing main window...")
-        win_id = mainwindow.MainWindow.spawn(
-            False if self._args.nowindow else True)
-        main_window = objreg.get('main-window', scope='window', window=win_id)
-        self.setActiveWindow(main_window)
+        if not session_manager.exists(self._args.session):
+            log.init.debug("Initializing main window...")
+            win_id = mainwindow.MainWindow.spawn(
+                False if self._args.nowindow else True)
+            main_window = objreg.get('main-window', scope='window',
+                                     window=win_id)
+            self.setActiveWindow(main_window)
 
     def _init_icon(self):
         """Initialize the icon of qutebrowser."""
@@ -261,9 +267,26 @@ class Application(QApplication):
             except (configexc.Error, configparser.Error) as e:
                 message.error('current', "set: {} - {}".format(
                     e.__class__.__name__, e))
+        self._load_session(self._args.session)
         self.process_pos_args(self._args.command)
         self._open_startpage()
         self._open_quickstart()
+
+    def _load_session(self, name):
+        """Load the default session.
+
+        Args:
+            name: The name of the session to load.
+        """
+        session_manager = objreg.get('session-manager')
+        try:
+            session_manager.load(name)
+        except sessions.SessionNotFoundError:
+            pass
+        except sessions.SessionError:
+            log.init.exception("Failed to load default session")
+        else:
+            session_manager.delete('default')
 
     def _get_window(self, via_ipc, force_window=False, force_tab=False):
         """Helper function for process_pos_args to get a window id.

qutebrowser/browser/tabhistory.py
@@ -1,0 +1,179 @@
+# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+
+# Copyright 2015 Florian Bruhin (The Compiler) <mail@qutebrowser.org>
+#
+# This file is part of qutebrowser.
+#
+# qutebrowser is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# qutebrowser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Utilities related to QWebHistory."""
+
+
+from PyQt5.QtCore import QByteArray, QDataStream, QIODevice, QUrl
+
+from qutebrowser.utils import utils, qtutils
+
+
+HISTORY_STREAM_VERSION = 2
+BACK_FORWARD_TREE_VERSION = 2
+
+
+class TabHistoryItem:
+
+    """A single item in the tab history.
+
+    Attributes:
+        url: The QUrl of this item.
+        title: The title as string of this item.
+        active: Whether this item is the item currently navigated to.
+        user_data: The user data for this item.
+    """
+
+    def __init__(self, url, original_url, title, active=False, user_data=None):
+        self.url = url
+        self.original_url = original_url
+        self.title = title
+        self.active = active
+        self.user_data = user_data
+
+    def __repr__(self):
+        return utils.get_repr(self, constructor=True, url=self.url,
+                              original_url=self.original_url, title=self.title,
+                              active=self.active, user_data=self.user_data)
+
+
+def _encode_url(url):
+    """Encode an QUrl suitable to pass to QWebHistory."""
+    data = bytes(QUrl.toPercentEncoding(url.toString(), b':/#?&+=@%*'))
+    return data.decode('ascii')
+
+
+def _serialize_item(i, item, stream):
+    """Serialize a single WebHistoryItem into a QDataStream.
+
+    Args:
+        i: The index of the current item.
+        item: The WebHistoryItem to write.
+        stream: The QDataStream to write to.
+    """
+    ### Source/WebCore/history/qt/HistoryItemQt.cpp restoreState
+    ## urlString
+    stream.writeQString(_encode_url(item.url))
+    ## title
+    stream.writeQString(item.title)
+    ## originalURLString
+    stream.writeQString(_encode_url(item.original_url))
+
+    ### Source/WebCore/history/HistoryItem.cpp decodeBackForwardTree
+    ## backForwardTreeEncodingVersion
+    stream.writeUInt32(BACK_FORWARD_TREE_VERSION)
+    ## size (recursion stack)
+    stream.writeUInt64(0)
+    ## node->m_documentSequenceNumber
+    # If two HistoryItems have the same document sequence number, then they
+    # refer to the same instance of a document.  Traversing history from one
+    # such HistoryItem to another preserves the document.
+    stream.writeInt64(i + 1)
+    ## size (node->m_documentState)
+    stream.writeUInt64(0)
+    ## node->m_formContentType
+    # info used to repost form data
+    stream.writeQString(None)
+    ## hasFormData
+    stream.writeBool(False)
+    ## node->m_itemSequenceNumber
+    # If two HistoryItems have the same item sequence number, then they are
+    # clones of one another.  Traversing history from one such HistoryItem to
+    # another is a no-op.  HistoryItem clones are created for parent and
+    # sibling frames when only a subframe navigates.
+    stream.writeInt64(i + 1)
+    ## node->m_referrer
+    stream.writeQString(None)
+    ## node->m_scrollPoint (x)
+    try:
+        stream.writeInt32(item.user_data['scroll-pos'].x())
+    except (KeyError, TypeError):
+        stream.writeInt32(0)
+    ## node->m_scrollPoint (y)
+    try:
+        stream.writeInt32(item.user_data['scroll-pos'].y())
+    except (KeyError, TypeError):
+        stream.writeInt32(0)
+    ## node->m_pageScaleFactor
+    try:
+        stream.writeFloat(item.user_data['zoom'])
+    except (KeyError, TypeError):
+        stream.writeFloat(1)
+    ## hasStateObject
+    # Support for HTML5 History
+    stream.writeBool(False)
+    ## node->m_target
+    stream.writeQString(None)
+
+    ### Source/WebCore/history/qt/HistoryItemQt.cpp restoreState
+    ## validUserData
+    # We could restore the user data here, but we prefer to use the
+    # QWebHistoryItem API for that.
+    stream.writeBool(False)
+
+
+def serialize(items):
+    """Serialize a list of QWebHistoryItems to a data stream.
+
+    Args:
+        items: An iterable of WebHistoryItems.
+
+    Return:
+        A (stream, data, user_data) tuple.
+            stream: The resetted QDataStream.
+            data: The QByteArray with the raw data.
+            user_data: A list with each item's user data.
+
+    Warning:
+        If 'data' goes out of scope, reading from 'stream' will result in a
+        segfault!
+    """
+
+    data = QByteArray()
+    stream = QDataStream(data, QIODevice.ReadWrite)
+    user_data = []
+
+    current_idx = None
+
+    for i, item in enumerate(items):
+        if item.active:
+            if current_idx is not None:
+                raise ValueError("Multiple active items ({} and {}) "
+                                 "found!".format(current_idx, i))
+            else:
+                current_idx = i
+
+    if items:
+        if current_idx is None:
+            raise ValueError("No active item found!")
+    else:
+        current_idx = 0
+
+    ### Source/WebKit/qt/Api/qwebhistory.cpp operator<<
+    stream.writeInt(HISTORY_STREAM_VERSION)
+    stream.writeInt(len(items))
+    stream.writeInt(current_idx)
+
+    for i, item in enumerate(items):
+        _serialize_item(i, item, stream)
+        user_data.append(item.user_data)
+
+    stream.device().reset()
+    qtutils.check_qdatastream(stream)
+    return stream, data, user_data

commit 87951ee3a8a2ea1a38f2144b038bf6c710dfaa5b

Fix crash when closing tabs on Qt 5.2.1 (Trusty)
This issue was introduced in 8f1d81a6441ef9df88799ca9969122fb7fcf4f3b.

qutebrowser/browser/webpage.py
@@ -368,7 +368,15 @@ class BrowserPage(QWebPage):
             frame: The QWebFrame which gets saved.
             item: The QWebHistoryItem to be saved.
         """
-        if frame != self.mainFrame():
+        try:
+            if frame != self.mainFrame():
+                return
+        except RuntimeError:
+            # With Qt 5.2.1 (Ubuntu Trusty) we get this when closing a tab:
+            #     RuntimeError: wrapped C/C++ object of type BrowserPage has
+            #     been deleted
+            # Since the information here isn't that important for closing
+            # webviews anyways, we ignore this error.
             return
         data = {
             'zoom': frame.zoomFactor(),