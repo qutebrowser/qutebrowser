edf762e2107f8d7278a1ef5bcc35bd95d4291c09
Florian Bruhin <git@the-compiler.org>
2015-03-06 17:04:56 +0100

    Avoid pylint duplicate-code warning.

--- qutebrowser/test/log.py
+++ qutebrowser/test/log.py
@@ -52,6 +52,8 @@ def qt_message_handler(msg_type, context, msg):
         QtFatalMsg: logging.CRITICAL,
     }
     level = qt_to_logging[msg_type]
+    # There's very similiar code in utils.log, but we want it duplicated here
+    # for the tests.
     if context.function is None:
         func = 'none'
     else:
