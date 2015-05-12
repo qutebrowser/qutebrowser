e62ba57291abd66b65a22e6037f14b6aa1d92928
Florian Bruhin <git@the-compiler.org>
2015-05-12 21:04:18 +0200

    Always save last window session.
    len(objreg.window_registry) can actually lag behind because single-shot QTimers
    are used to remove the windows from the registry - but actually it doesn't even
    matter if this is the last window or not. We just always save to
    SessionManager._last_window_session, and it gets used in SessionManager.save.
    
    Fixes #650.

--- qutebrowser/mainwindow/mainwindow.py
+++ qutebrowser/mainwindow/mainwindow.py
@@ -416,8 +416,7 @@ class MainWindow(QWidget):
                 e.ignore()
                 return
         e.accept()
-        if len(objreg.window_registry) == 1:
-            objreg.get('session-manager').save_last_window_session()
+        objreg.get('session-manager').save_last_window_session()
         self._save_geometry()
         log.destroy.debug("Closing window {}".format(self.win_id))
         self._tabbed_browser.shutdown()
