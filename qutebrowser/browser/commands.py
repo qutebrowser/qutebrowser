70956aaeca3df4bf0c9842fe443202370f683ff8
Martin Tournoij <martin@lico.nl>
2015-05-29 23:57:57 +0200

    oops

--- qutebrowser/browser/commands.py
+++ qutebrowser/browser/commands.py
@@ -657,7 +657,7 @@ class CommandDispatcher:
             count: multiplier
         """
         frame = self._current_widget().page().currentFrame()
-        if not frame.url().isValid() == '':  # Issue 701
+        if not frame.url().isValid():  # Issue 701
             return
 
         if (bottom_navigate is not None and
