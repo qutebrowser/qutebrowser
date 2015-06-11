ba99861844a6137ea3784e9a9857a2b8b9047a1a
Florian Bruhin <git@the-compiler.org>
2015-06-11 16:56:52 +0200

    Make pylint shut up about ci_install.

--- scripts/ci_install.py
+++ scripts/ci_install.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
 
-# pylint: disable=open-without-encoding
+# pylint: skip-file
 
 """Install needed prerequisites on the AppVeyor/Travis CI.
 
