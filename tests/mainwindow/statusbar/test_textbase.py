9b066ec50af4cc38c0bc8fb909dd97469b521406
Florian Bruhin <git@the-compiler.org>
2015-06-12 13:44:10 +0200

    Set geometry in test_textbase.py.
    See 83f7cf84a9af99bfad52bc4e07aff157fae70804 - it seems with the Qt 5.4.2
    upgrade there are some more warnings on Windows.

--- tests/mainwindow/statusbar/test_textbase.py
+++ tests/mainwindow/statusbar/test_textbase.py
@@ -46,6 +46,7 @@ def test_elided_text(qtbot, elidemode, check):
     """
     label = TextBase(elidemode=elidemode)
     qtbot.add_widget(label)
+    label.setGeometry(200, 200, 200, 200)
     long_string = 'Hello world! ' * 20
     label.setText(long_string)
     label.resize(100, 50)
