qutebrowser/qutebrowser

commit 6dbdea0ee3fa52d08cc135dba65924792b38395c

Set maxsplit=0 for :spawn and split manually.
Fixes #759.

qutebrowser/browser/commands.py
@@ -21,6 +21,7 @@
 
 import re
 import os
+import shlex
 import posixpath
 import functools
 import xml.etree.ElementTree
@@ -919,8 +920,9 @@ class CommandDispatcher:
         finally:
             self._tabbed_browser.setUpdatesEnabled(True)
 
-    @cmdutils.register(instance='command-dispatcher', scope='window')
-    def spawn(self, userscript=False, verbose=False, detach=False, *args):
+    @cmdutils.register(instance='command-dispatcher', scope='window',
+                       maxsplit=0)
+    def spawn(self, cmdline, userscript=False, verbose=False, detach=False):
         """Spawn a command in a shell.
 
         Note the {url} variable which gets replaced by the current URL might be
@@ -930,11 +932,15 @@ class CommandDispatcher:
             userscript: Run the command as an userscript.
             verbose: Show notifications when the command started/exited.
             detach: Whether the command should be detached from qutebrowser.
-            *args: The commandline to execute.
+            cmdline: The commandline to execute.
         """
-        log.procs.debug("Executing: {}, userscript={}".format(
-            args, userscript))
-        cmd, *args = args
+        try:
+            cmd, *args = shlex.split(cmdline)
+        except ValueError as e:
+            raise cmdexc.CommandError("Error while splitting command: "
+                                      "{}".format(e))
+        log.procs.debug("Executing {} with args {}, userscript={}".format(
+            cmd, args, userscript))
         if userscript:
             self.run_userscript(cmd, *args, verbose=verbose)
         else:

doc/help/commands.asciidoc
@@ -529,19 +529,23 @@ Preset the statusbar to some text.
 
 [[spawn]]
 === spawn
-Syntax: +:spawn [*--userscript*] [*--verbose*] [*--detach*] 'args' ['args' ...]+
+Syntax: +:spawn [*--userscript*] [*--verbose*] [*--detach*] 'cmdline'+
 
 Spawn a command in a shell.
 
 Note the {url} variable which gets replaced by the current URL might be useful here.
 
 ==== positional arguments
-* +'args'+: The commandline to execute.
+* +'cmdline'+: The commandline to execute.
 
 ==== optional arguments
 * +*-u*+, +*--userscript*+: Run the command as an userscript.
 * +*-v*+, +*--verbose*+: Show notifications when the command started/exited.
 * +*-d*+, +*--detach*+: Whether the command should be detached from qutebrowser.
+
+==== note
+* This command does not split arguments after the last argument and handles quotes literally.
+* With this command, +;;+ is interpreted literally instead of splitting off a second command.
 
 [[stop]]
 === stop

commit 59cdbd780c8911d3ada47c3010730067c91db414

Fix {url} substitution with :spawn.
See #759.

This is a regression introduced in 6dbdea0ee3fa52d08cc135dba65924792b38395c.

qutebrowser/browser/commands.py
@@ -36,7 +36,7 @@ import pygments
 import pygments.lexers
 import pygments.formatters
 
-from qutebrowser.commands import userscripts, cmdexc, cmdutils
+from qutebrowser.commands import userscripts, cmdexc, cmdutils, runners
 from qutebrowser.config import config, configexc
 from qutebrowser.browser import webelem, inspector
 from qutebrowser.keyinput import modeman
@@ -939,6 +939,9 @@ class CommandDispatcher:
         except ValueError as e:
             raise cmdexc.CommandError("Error while splitting command: "
                                       "{}".format(e))
+
+        args = runners.replace_variables(self._win_id, args)
+
         log.procs.debug("Executing {} with args {}, userscript={}".format(
             cmd, args, userscript))
         if userscript: