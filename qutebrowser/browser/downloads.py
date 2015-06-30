c7cd51a7d5d75fe81fc37703f9c1224051b31111
Martin Tournoij <martin@arp242.net>
2015-06-30 12:01:05 +0200

    "Fix" silly pep8 warnings

--- qutebrowser/browser/downloads.py
+++ qutebrowser/browser/downloads.py
@@ -452,11 +452,14 @@ class DownloadItem(QObject):
         if os.path.isfile(self._filename):
             # The file already exists, so ask the user if it should be
             # overwritten.
-            self._ask_confirm_question(self._filename + " already exists. Overwrite?")
+            self._ask_confirm_question(self._filename +
+                                       " already exists. Overwrite?")
         # FIFO, device node, etc. Make sure we want to do this
         elif (os.path.exists(self._filename) and not
                 os.path.isdir(self._filename)):
-            self._ask_confirm_question(self._filename + " already exists and is a special file. Write to this?")
+            self._ask_confirm_question(self._filename +
+                                       " already exists and is a special file."
+                                       " Write to this?")
         else:
             self._create_fileobj()
 
