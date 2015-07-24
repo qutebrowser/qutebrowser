ebdfa0be733bace5ef0af2d95451bf2135bb2edb
Florian Bruhin <git@the-compiler.org>
2015-07-24 17:49:09 +0200

    Add hypothesis tests for configtypes.
    See #830.

---
+++ tests/config/test_configtypes_hypothesis.py
@@ -0,0 +1,50 @@
+# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+# Copyright 2015 Florian Bruhin (The Compiler) <mail@qutebrowser.org>
+
+# This file is part of qutebrowser.
+#
+# qutebrowser is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# qutebrowser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Hypothesis tests for qutebrowser.config.configtypes."""
+
+import inspect
+import functools
+
+import pytest
+import hypothesis
+from hypothesis import strategies
+
+from qutebrowser.config import configtypes, configexc
+
+
+def gen_classes():
+    for name, member in inspect.getmembers(configtypes, inspect.isclass):
+        if member is configtypes.BaseType:
+            pass
+        elif member is configtypes.MappingType:
+            pass
+        elif member is configtypes.FormatString:
+            yield functools.partial(member, fields=['a', 'b'])
+        elif issubclass(member, configtypes.BaseType):
+            yield member
+
+
+@pytest.mark.parametrize('klass', gen_classes())
+@hypothesis.given(strategies.text())
+@hypothesis.example('\x00')
+def test_configtypes_hypothesis(klass, s):
+    try:
+        klass().validate(s)
+    except configexc.ValidationError:
+        pass
