041315b65fc53d203a83aec4f6cd3cade8787680
Florian Bruhin <git@the-compiler.org>
2015-07-31 17:02:42 +0200

    Honour right-padding in tabs correctly.

--- qutebrowser/mainwindow/tabwidget.py
+++ qutebrowser/mainwindow/tabwidget.py
@@ -635,7 +635,7 @@ class TabBarStyle(QCommonStyle):
         text_rect = QRect(opt.rect)
         qtutils.ensure_valid(text_rect)
         indicator_width = config.get('tabs', 'indicator-width')
-        text_rect.adjust(padding, 0, 0, 0)
+        text_rect.adjust(padding, 0, -padding, 0)
         if indicator_width != 0:
             text_rect.adjust(indicator_width +
                              config.get('tabs', 'indicator-space'), 0, 0, 0)
