591a5b8c56f44658805f68b84ba8021860553e1a
Florian Bruhin <git@the-compiler.org>
2015-08-12 18:27:41 +0200

    Revert "Add a workaround for pytest-html surrogate issue."
    This reverts commit 9c3c46f6777e97baf909c07bd606a493c3ed3f2f.

--- tests/config/test_configtypes.py
+++ tests/config/test_configtypes.py
@@ -1146,12 +1146,10 @@ class TestFileAndUserStyleSheet:
         with pytest.raises(configexc.ValidationError):
             configtypes.File().validate('foobar')
 
-    UNICODE = object()
-
     @pytest.mark.parametrize('configtype, value, raises', [
         (configtypes.File(), 'foobar', True),
         (configtypes.UserStyleSheet(), 'foobar', False),
-        (configtypes.UserStyleSheet(), UNICODE, True),
+        (configtypes.UserStyleSheet(), '\ud800', True),
     ])
     def test_validate_rel_inexistent(self, os_mock, monkeypatch, configtype,
                                      value, raises):
@@ -1161,10 +1159,6 @@ class TestFileAndUserStyleSheet:
             lambda: 'this/does/not/exist')
         os_mock.path.isabs.return_value = False
         os_mock.path.isfile.side_effect = os.path.isfile
-
-        if value is self.UNICODE:
-            # WORKAROUND for https://github.com/davehunt/pytest-html/issues/12
-            value = '\ud800'
 
         if raises:
             with pytest.raises(configexc.ValidationError):
