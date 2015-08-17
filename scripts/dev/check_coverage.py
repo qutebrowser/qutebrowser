00d81a74c28c3e13724f0a49cf2122e5b0fb44d7
Florian Bruhin <git@the-compiler.org>
2015-08-17 07:16:33 +0200

    100% coverage for browser.network.networkreply.

--- scripts/dev/check_coverage.py
+++ scripts/dev/check_coverage.py
@@ -41,6 +41,7 @@ PERFECT_FILES = [
     'qutebrowser/browser/webelem.py',
     'qutebrowser/browser/network/schemehandler.py',
     'qutebrowser/browser/network/filescheme.py',
+    'qutebrowser/browser/network/networkreply.py',
     'qutebrowser/browser/signalfilter.py',
 
     'qutebrowser/misc/readline.py',
