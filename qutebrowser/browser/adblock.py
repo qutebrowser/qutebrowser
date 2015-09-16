ccdb59cce10f385b0079a32508fa3a2afaeaa96d
Daniel <kingdread@gmx.de>
2015-09-16 16:31:17 +0200

    Add adblock host whitelisting
    The config option "content host-blocking-whitelist" may contain comma
    separated domains that are exempt from host blocking.
    
    The listed domains may contain the wildcards "*" and "?" to match many
    and one character, respectively.
    
    You need to run :adblock-update after modifying the list.

--- qutebrowser/browser/adblock.py
+++ qutebrowser/browser/adblock.py
@@ -24,6 +24,8 @@ import os.path
 import functools
 import posixpath
 import zipfile
+import fnmatch
+import re
 
 from qutebrowser.config import config
 from qutebrowser.utils import objreg, standarddir, log, message
@@ -57,6 +59,24 @@ def get_fileobj(byte_io):
     else:
         byte_io.seek(0)  # rewind what zipfile.is_zipfile did
     return io.TextIOWrapper(byte_io, encoding='utf-8')
+
+
+def is_whitelisted_domain(host):
+    """Check if the given host is on the adblock whitelist.
+
+    Args:
+        host: The host as given by the adblocker as string.
+    """
+    whitelist = objreg.get('config').get('content', 'host-blocking-whitelist')
+    if whitelist is None:
+        return False
+
+    for domain in whitelist:
+        fnmatch_translated = fnmatch.translate(domain)
+        domain_regex = re.compile(fnmatch_translated, re.IGNORECASE)
+        if domain_regex.match(host):
+            return True
+    return False
 
 
 class FakeDownload:
@@ -188,7 +208,8 @@ class HostBlocker:
             else:
                 error_count += 1
                 continue
-            if host not in self.WHITELISTED:
+            if (host not in self.WHITELISTED
+                    and not is_whitelisted_domain(host)):
                 self.blocked_hosts.add(host)
         log.misc.debug("{}: read {} lines".format(byte_io.name, line_count))
         if error_count > 0:
