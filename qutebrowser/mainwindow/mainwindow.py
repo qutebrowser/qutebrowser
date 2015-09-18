239b25cc8ab897c2a511449a4a359daf99d17e57
Florian Bruhin <git@the-compiler.org>
2015-09-18 07:01:51 +0200

    Save search parameters in tabbed_browser.

--- qutebrowser/mainwindow/mainwindow.py
+++ qutebrowser/mainwindow/mainwindow.py
@@ -109,8 +109,6 @@ class MainWindow(QWidget):
         self._commandrunner = None
         self.win_id = next(win_id_gen)
         self.registry = objreg.ObjectRegistry()
-        self.search_text = None
-        self.search_flags = 0
         objreg.window_registry[self.win_id] = self
         objreg.register('main-window', self, scope='window',
                         window=self.win_id)
