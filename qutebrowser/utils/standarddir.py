ab099ea6eac74da7f1b841c01ebf43b39200b3fc
Florian Bruhin <git@the-compiler.org>
2015-10-14 07:02:50 +0200

    Fix another spelling mistake.

--- qutebrowser/utils/standarddir.py
+++ qutebrowser/utils/standarddir.py
@@ -176,7 +176,7 @@ def _maybe_create(path):
 
     From the XDG basedir spec:
         If, when attempting to write a file, the destination directory is
-        non-existant an attempt should be made to create it with permission
+        non-existent an attempt should be made to create it with permission
         0700. If the destination directory exists already the permissions
         should not be changed.
     """
