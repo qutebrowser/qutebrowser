ac148c11ecce52ed436991b4f540816908513d2b
Florian Bruhin <git@the-compiler.org>
2015-10-15 08:26:28 +0200

    Skip bdd tests when frozen for now.
    See:
    
    https://github.com/pytest-dev/pytest-bdd/issues/158
    https://github.com/pytest-dev/pytest-bdd/issues/159

--- tests/integration/features/test_features.py
+++ tests/integration/features/test_features.py
@@ -17,9 +17,15 @@
 # You should have received a copy of the GNU General Public License
 # along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
 
-import pytest_bdd as bdd
+import sys
 
-bdd.scenarios('.')
+import pytest
+
+if hasattr(sys, 'frozen'):
+    pytest.skip("test")
+else:
+    import pytest_bdd as bdd
+    bdd.scenarios('.')
 
 
 @bdd.given(bdd.parsers.parse("I set {sect} -> {opt} to {value}"))
