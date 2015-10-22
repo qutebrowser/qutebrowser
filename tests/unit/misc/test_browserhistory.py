426dceba5ad2dfcee2d53eba14331dba89edbbc2
Nathan Isom <nathanisom27@gmail.com>
2015-10-22 15:27:52 -0500

    Change test to mock attr with an instance of parser.

--- tests/unit/misc/test_browserhistory.py
+++ tests/unit/misc/test_browserhistory.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Tests for misc.cmdhistory.History."""
+"""Tests for qutebrowser.browser.History."""
 
 import pytest
 
@@ -27,6 +27,9 @@ from unittest import mock
 
 from qutebrowser.misc import cmdhistory
 from qutebrowser.browser.history import WebHistory
+from qutebrowser.browser.history import HistoryEntry
+from PyQt5.QtCore import QUrl
+import datetime
 
 from qutebrowser.browser import cookies
 from qutebrowser.utils import objreg
@@ -37,6 +40,9 @@ from qutebrowser.misc import lineparser, savemanager
 
 HISTORY = ['first', 'second', 'third', 'fourth', 'fifth']
 
+CONFIG_NOT_PRIVATE = {'general': {'private-browsing': False}}
+CONFIG_PRIVATE = {'general': {'private-browsing': True}}
+
 @pytest.yield_fixture(autouse=True)
 def fake_save_manager():
     """Create a mock of save-manager and register it into objreg."""
@@ -46,12 +52,21 @@ def fake_save_manager():
     objreg.delete('save-manager')
 
 
-def test_async_read(qapp):
+def test_contained(qapp, config_stub, monkeypatch):
+    """ Test historyContains() """
+    config_stub.data = CONFIG_NOT_PRIVATE
+
     wb = WebHistory()
-    wb._lineparser = LineparserSaveStub()
-    wb.async_read()
-    rp = wb.__repr__()
-    assert rp  == '<qutebrowser.browser.history.WebHistory length=0>'
+    #monkeypatch.setattr(wb, '_lineparser', LineparserSaveStub)
+    lp = LineparserSaveStub(None, 'temp.stub')
+    monkeypatch.setattr(wb, '_lineparser', lp)
+
+    #entry = HistoryEntry('1444397977', 'http://asdf.com')
+    wb.addHistoryEntry('http://asdf.com')
+    wb.save()
+
+    result = wb.historyContains('http://asdf.com')
+    assert result == True
 
 
 class LineparserSaveStub(lineparser.BaseLineParser):
@@ -67,12 +82,10 @@ class LineparserSaveStub(lineparser.BaseLineParser):
         self.data = []
 
     def save(self):
-        self.saved = self.data
+        self.saved = self.new_data
 
     def __iter__(self):
         return iter(self.data)
 
     def __getitem__(self, key):
         return self.data[key]
-
-
