28f5403158a7e7f3e23ab3f3cd252f7da23d9239
Nathan Isom <nathanisom27@gmail.com>
2015-10-22 11:36:46 -0500

    Initialy basic test.

---
+++ tests/unit/misc/test_browserhistory.py
@@ -0,0 +1,80 @@
+# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+
+# Copyright 2015 Florian Bruhin (The-Compiler) <me@the-compiler.org>
+#
+# This file is part of qutebrowser.
+#
+# qutebrowser is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# qutebrowser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Tests for misc.cmdhistory.History."""
+
+import pytest
+
+import qutebrowser.browser.history
+
+from unittest import mock
+
+from qutebrowser.misc import cmdhistory
+from qutebrowser.browser.history import WebHistory
+
+from qutebrowser.browser import cookies
+from qutebrowser.utils import objreg
+from qutebrowser.misc import lineparser, savemanager
+
+# autouse fixtures (so no need to pass to tests)
+
+HISTORY = ['first', 'second', 'third', 'fourth', 'fifth']
+
+@pytest.yield_fixture
+def fake_save_manager():
+    """Create a mock of save-manager and register it into objreg."""
+    fake_save_manager = mock.Mock(spec=savemanager.SaveManager)
+    objreg.register('save-manager', fake_save_manager)
+    yield
+    objreg.delete('save-manager')
+
+# web history object to be used in the tests
+@pytest.yield_fixture
+def web_history():
+    wb = WebHistory()
+    yield
+
+@pytest.yield_fixture(autouse=True)
+def test_readhistory(qapp):
+    rp = wb.__repr__()
+    assert rp  == 'asdf'
+
+class LineparserSaveStub(lineparser.BaseLineParser):
+    """A stub for LineParser's save()
+    Attributes:
+        data: The data before the write
+        saved: The .data before save()
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.saved = []
+        self.data = []
+
+    def save(self):
+        self.saved = self.data
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+
+
