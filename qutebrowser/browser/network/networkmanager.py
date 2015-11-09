qutebrowser/qutebrowser

commit 976f758da1d5ad086bc539b41a3afd534e64b282

Fix getting of the current URL.

qutebrowser/browser/network/networkmanager.py
@@ -338,8 +338,9 @@ class NetworkManager(QNetworkAccessManager):
         req.setRawHeader('DNT'.encode('ascii'), dnt)
         req.setRawHeader('X-Do-Not-Track'.encode('ascii'), dnt)
 
-        current_url = objreg.get('tabbed-browser', scope='window',
-                                 window=self._win_id).currentWidget().url()
+        current_url = objreg.get('webview', scope='tab', window=self._win_id,
+                                 tab=self._tab_id).url()
+
         if config.get('network', 'referer-header') == 'never':
             # Note: using ''.encode('ascii') sends a header with no value,
             # instead of no header at all

commit b59a56921e5ce2df1b2a7d68045529765c693ac6

Fix crashes with downloads in a closed tab.
When a download was redirected or failed after a tab was closed, there was a
KeyError in the object registry.

Fixes #889. This is a regression introduced in
976f758da1d5ad086bc539b41a3afd534e64b282 / #731.

qutebrowser/browser/network/networkmanager.py
@@ -363,9 +363,14 @@ class NetworkManager(QNetworkAccessManager):
         if self._tab_id is None:
             current_url = QUrl()  # generic NetworkManager, e.g. for downloads
         else:
-            webview = objreg.get('webview', scope='tab', window=self._win_id,
-                                 tab=self._tab_id)
-            current_url = webview.url()
+            try:
+                webview = objreg.get('webview', scope='tab',
+                                     window=self._win_id, tab=self._tab_id)
+            except KeyError:
+                # https://github.com/The-Compiler/qutebrowser/issues/889
+                current_url = QUrl()
+            else:
+                current_url = webview.url()
 
         self.set_referer(req, current_url)
 