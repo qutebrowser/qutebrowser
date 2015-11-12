69a92af097c200cedb9b6119d3bdb80af479d006
Daniel Schadt <kingdread@gmx.de>
2015-11-12 20:57:38 +0100

    mhtml: add "complex" test case
    The html page is very ugly, but tests the downloader pretty throughout

---
+++ tests/integration/data/downloads/mhtml/complex/script.js
@@ -0,0 +1,1 @@
+document.write("The story so far: In the beginning the Universe was created. This has made a lot of people very angry and has been widely regarded as a bad move. - Douglas Adams<br>")
