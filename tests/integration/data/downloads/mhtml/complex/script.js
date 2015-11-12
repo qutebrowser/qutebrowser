354259777adf9d8c233a51a67313759d51dbefce
Daniel Schadt <kingdread@gmx.de>
2015-11-12 21:07:14 +0100

    mhtml: don't test with dynamically modified page

--- tests/integration/data/downloads/mhtml/complex/script.js
+++ tests/integration/data/downloads/mhtml/complex/script.js
@@ -1,1 +1,2 @@
-document.write("The story so far: In the beginning the Universe was created. This has made a lot of people very angry and has been widely regarded as a bad move. - Douglas Adams<br>")
+function noop() {}
+noop();
