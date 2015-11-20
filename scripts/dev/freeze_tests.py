fe1c8ee4e84452f68891f42f223c0e086aa17324
Daniel Schadt <kingdread@gmx.de>
2015-11-20 15:27:17 +0100

    Also add cherrypy.wsgiserver.wsgiserver3 to freeze

--- scripts/dev/freeze_tests.py
+++ scripts/dev/freeze_tests.py
@@ -56,7 +56,8 @@ def get_build_exe_options():
     opts['includes'] += pytest.freeze_includes()  # pylint: disable=no-member
     opts['includes'] += ['unittest.mock', 'PyQt5.QtTest', 'hypothesis', 'bs4',
                          'httpbin', 'jinja2.ext', 'xvfbwrapper',
-                         'cherrypy.wsgiserver']
+                         'cherrypy.wsgiserver',
+                         'cherrypy.wsgiserver.wsgiserver3']
 
     httpbin_dir = os.path.dirname(httpbin.__file__)
     opts['include_files'] += [
