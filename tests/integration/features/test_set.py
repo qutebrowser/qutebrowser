162c8b30e64ccd36ab9f22ce93f52a42e2fcbf41
Florian Bruhin <git@the-compiler.org>
2015-11-23 14:49:37 +0100

    Remove unused import.

--- tests/integration/features/test_set.py
+++ tests/integration/features/test_set.py
@@ -17,8 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
 
-import logging
-
 import pytest_bdd as bdd
 bdd.scenarios('set.feature')
 
