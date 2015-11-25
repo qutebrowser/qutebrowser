67f726c9d4ad5af0cacb591c2785aeea2a908323
Florian Bruhin <git@the-compiler.org>
2015-11-25 18:47:16 +0100

    tests: Add another BOM test for javascript_escape.

--- tests/unit/browser/test_webelem.py
+++ tests/unit/browser/test_webelem.py
@@ -588,6 +588,7 @@ class TestJavascriptEscape:
         '\x80Ā': '\x80Ā',
         '𐀀\x00𐀀\x00': r'𐀀\x00𐀀\x00',
         '𐀀\ufeff': r'𐀀\ufeff',
+        '\ufeff': r'\ufeff',
         # http://stackoverflow.com/questions/2965293/
         '\u2028': r'\u2028',
         '\u2029': r'\u2029',
