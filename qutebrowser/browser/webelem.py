24a71e5c2ebbffd9021694f32fa9ec51d0046d5a
Florian Bruhin <git@the-compiler.org>
2015-11-25 10:32:22 +0100

    Fix webelem.javascript_escape with U+2028/U+2029.

--- qutebrowser/browser/webelem.py
+++ qutebrowser/browser/webelem.py
@@ -308,6 +308,9 @@ def javascript_escape(text):
         ('\n', r'\n'),  # We also need to escape newlines for some reason.
         ('\r', r'\r'),
         ('\x00', r'\x00'),
+        # http://stackoverflow.com/questions/2965293/
+        ('\u2028', r'\u2028'),
+        ('\u2029', r'\u2029'),
     )
     for orig, repl in replacements:
         text = text.replace(orig, repl)
