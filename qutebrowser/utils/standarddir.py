qutebrowser/qutebrowser

commit f5febc4807007f1dffa19f9808cde41d54712e45

Log basedir and save it in QuteProcess for tests.

qutebrowser/utils/standarddir.py
@@ -190,6 +190,7 @@ def _maybe_create(path):
 def init(args):
     """Initialize all standard dirs."""
     global _args
+    log.init.debug("Base directory: {}".format(args.basedir))
     _args = args
     _init_cachedir_tag()
 

tests/integration/quteprocess.py
@@ -119,6 +119,7 @@ class QuteProc(testprocess.Process):
         _delay: Delay to wait between commands.
         _ipc_socket: The IPC socket of the started instance.
         _httpbin: The HTTPBin webserver.
+        basedir: The base directory for this instance.
 
     Signals:
         got_error: Emitted when there was an error log line.
@@ -134,6 +135,7 @@ class QuteProc(testprocess.Process):
         self._delay = delay
         self._httpbin = httpbin
         self._ipc_socket = None
+        self.basedir = None
 
     def _parse_line(self, line):
         try:
@@ -163,6 +165,11 @@ class QuteProc(testprocess.Process):
         elif (log_line.category == 'webview' and
                 log_line.message == start_okay_message):
             self.ready.emit()
+        elif (log_line.category == 'init' and
+                log_line.module == 'standarddir' and
+                log_line.function == 'init' and
+                log_line.message.startswith('Base directory:')):
+            self.basedir = log_line.message.split(':', maxsplit=1)[1].strip()
         elif log_line.loglevel > logging.INFO:
             self.got_error.emit()
 

commit 6a46aea934a49e3715cd9e6397f28be73fba16b5

Fix tests for basedir logging.
f5febc4 broke various tests which called standarddir.init() without a valid
basedir.

qutebrowser/utils/standarddir.py
@@ -190,7 +190,9 @@ def _maybe_create(path):
 def init(args):
     """Initialize all standard dirs."""
     global _args
-    log.init.debug("Base directory: {}".format(args.basedir))
+    if args is not None:
+        # args can be None during tests
+        log.init.debug("Base directory: {}".format(args.basedir))
     _args = args
     _init_cachedir_tag()
 

tests/unit/config/test_config.py
@@ -315,7 +315,8 @@ class TestConfigInit:
 
     def test_config_none(self, monkeypatch):
         """Test initializing with config path set to None."""
-        args = types.SimpleNamespace(confdir='', datadir='', cachedir='')
+        args = types.SimpleNamespace(confdir='', datadir='', cachedir='',
+                                     basedir='')
         for k, v in self.env.items():
             monkeypatch.setenv(k, v)
         standarddir.init(args)

tests/unit/utils/test_standarddir.py
@@ -177,27 +177,28 @@ class TestArguments:
     def test_confdir(self, testcase):
         """Test --confdir."""
         args = types.SimpleNamespace(confdir=testcase.arg, cachedir=None,
-                                     datadir=None)
+                                     datadir=None, basedir=None)
         standarddir.init(args)
         assert standarddir.config() == testcase.expected
 
     def test_cachedir(self, testcase):
         """Test --cachedir."""
         args = types.SimpleNamespace(confdir=None, cachedir=testcase.arg,
-                                     datadir=None)
+                                     datadir=None, basedir=None)
         standarddir.init(args)
         assert standarddir.cache() == testcase.expected
 
     def test_datadir(self, testcase):
         """Test --datadir."""
         args = types.SimpleNamespace(confdir=None, cachedir=None,
-                                     datadir=testcase.arg)
+                                     datadir=testcase.arg, basedir=None)
         standarddir.init(args)
         assert standarddir.data() == testcase.expected
 
     def test_confdir_none(self):
         """Test --confdir with None given."""
-        args = types.SimpleNamespace(confdir=None, cachedir=None, datadir=None)
+        args = types.SimpleNamespace(confdir=None, cachedir=None, datadir=None,
+                                     basedir=None)
         standarddir.init(args)
         assert standarddir.config().split(os.sep)[-1] == 'qute_test'
 
@@ -206,7 +207,8 @@ class TestArguments:
         monkeypatch.setattr(
             'qutebrowser.utils.standarddir.QStandardPaths.writableLocation',
             lambda _typ: str(tmpdir))
-        args = types.SimpleNamespace(confdir=None, cachedir=None, datadir=None)
+        args = types.SimpleNamespace(confdir=None, cachedir=None, datadir=None,
+                                     basedir=None)
         standarddir.init(args)
         assert standarddir.runtime() == str(tmpdir / 'qute_test')
 