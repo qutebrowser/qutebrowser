0ca9cd361a3af4a0f73a9dba84f890096b9792fc
Florian Bruhin <git@the-compiler.org>
2016-01-13 00:00:55 +0100

    Fix webserver_sub_ssl file header.

--- tests/integration/webserver_sub_ssl.py
+++ tests/integration/webserver_sub_ssl.py
@@ -1,3 +1,27 @@
+# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+
+# Copyright 2016 Florian Bruhin (The Compiler) <mail@qutebrowser.org>
+#
+# This file is part of qutebrowser.
+#
+# qutebrowser is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# qutebrowser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Minimal flask webserver serving a Hello World via SSL.
+
+This script gets called as a QProcess from integration/conftest.py.
+"""
+
 import ssl
 import sys
 import json
@@ -5,12 +29,6 @@ import logging
 import os.path
 
 import flask
-
-
-"""Minimal flask webserver serving a Hello World via SSL.
-
-This script gets called as a QProcess from integration/conftest.py.
-"""
 
 
 app = flask.Flask(__name__)
