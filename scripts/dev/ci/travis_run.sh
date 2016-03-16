ee4fa6c118c5f3d14e6257acd8150a011b0fb29e
Florian Bruhin <git@the-compiler.org>
2016-03-16 07:23:17 +0100

    travis: Use prebuilt docker images

--- scripts/dev/ci/travis_run.sh
+++ scripts/dev/ci/travis_run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 if [[ $DOCKER ]]; then
-    docker build -t img misc/docker/$DOCKER
-    docker run --privileged -v $PWD:/outside img
+    # To build a fresh image:
+    # docker build -t img misc/docker/$DOCKER
+    # docker run --privileged -v $PWD:/outside img
+
+    docker run --privileged -v $PWD:/outside \
+        thecompiler/qutebrowser-manual:$DOCKER
 else
     args=()
     [[ $TESTENV == docs ]] && args=('--no-authors')
