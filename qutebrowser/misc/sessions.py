qutebrowser/qutebrowser

commit d83d2e442ed1a1e3e8b5b794506ff65d58efc16a

Work around getting an empty session title.
Fixes #879.
Supersedes #880.

qutebrowser/misc/sessions.py
@@ -143,10 +143,18 @@ class SessionManager(QObject):
         history = tab.page().history()
         for idx, item in enumerate(history.items()):
             qtutils.ensure_valid(item)
+
             item_data = {
                 'url': bytes(item.url().toEncoded()).decode('ascii'),
-                'title': item.title(),
             }
+
+            if not item.title():
+                # https://github.com/The-Compiler/qutebrowser/issues/879
+                if history.currentItemIndex() == idx:
+                    item_data['title'] = tab.page().mainFrame().title()
+                else:
+                    item_data['title'] = item_data['url']
+
             if item.originalUrl() != item.url():
                 encoded = item.originalUrl().toEncoded()
                 item_data['original-url'] = bytes(encoded).decode('ascii')

commit 272eb28d7ba08ed5672409b24a8ae1fa736ea792

Fix saving of history titles
This is a regression introduced in d83d2e4.

qutebrowser/misc/sessions.py
@@ -148,7 +148,9 @@ class SessionManager(QObject):
                 'url': bytes(item.url().toEncoded()).decode('ascii'),
             }
 
-            if not item.title():
+            if item.title():
+                item_data['title'] = item.title()
+            else:
                 # https://github.com/The-Compiler/qutebrowser/issues/879
                 if history.currentItemIndex() == idx:
                     item_data['title'] = tab.page().mainFrame().title()