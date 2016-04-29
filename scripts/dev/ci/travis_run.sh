e06a32ff42052140a8c2a3cd1e7028c8377c4697
Florian Bruhin <git@the-compiler.org>
2016-04-29 23:21:41 +0200

    Revert "travis: Switch to autobuilt Docker images"
    This reverts commit b2e52408f7f5d0bb8d6623521f04bbd6c1694947.
    Turns out only [testing] is on PyQt 5.6, and even with that the testsuite
    doesn't seem to load properly...

--- scripts/dev/ci/travis_run.sh
+++ scripts/dev/ci/travis_run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 if [[ $DOCKER ]]; then
+    # To build a fresh image:
+    # docker build -t img misc/docker/$DOCKER
+    # docker run --privileged -v $PWD:/outside img
+
     docker run --privileged -v $PWD:/outside \
-        thecompiler/qutebrowser:$DOCKER
+        thecompiler/qutebrowser-manual:$DOCKER
 else
     args=()
     [[ $TESTENV == docs ]] && args=('--no-authors')
