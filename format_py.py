98283c5098e2351fc647ae85337ba14ce1ad26da
anikamurarka <murarka.anika97@gmail.com>
2016-05-03 09:14:23 +0530

    Addressing changes

--- format_py.py
+++ format_py.py
@@ -2,22 +2,31 @@
 
 import os
 import autopep8
-
-
+import sys
 BASE_DIR = os.path.abspath(__file__)
 
 
 def fix_file(path):
+    p = ""
     for dirpath, _, filenames in os.walk(path):
+        if(os.path.exists(os.path.join(dirpath, 'bin/activate'))):
+            p = dirpath
         for f in filenames:
             if f.endswith('.py'):
                 filename = os.path.abspath(os.path.join(dirpath, f))
-                afile = open(filename, 'rw+')
-                source_code = afile.read()
-                afile.seek(0)
-                afile.truncate()
-                afile.write(autopep8.fix_code(source_code))
-                afile.close()
+                if (not bool(p)):
+                    if os.access(filename, os.W_OK):
+                        try:
+                            file1 = open(filename, 'r')
+                            source_code = file1.read()
+                            file1.seek(0)
+                            file1 = open(filename, 'w')
+                            file1.truncate()
+                            file1.write(autopep8.fix_code(source_code))
+                            file1.close()
+                        except:
+                            print filename
+                            print sys.exc_info()[0]
 
 
 if __name__ == "__main__":
