f58e2d91dca918c6741de64bfd55df74d464519e
Ryan Roden-Corrent <ryan@rcorre.net>
2016-05-23 21:12:52 -0400

    Add a keyhint blacklist.
    Replace the setting ui.show-keyhints with ui.keyhint-blacklist, which
    is a list of globs for keychains that shouldn't be hinted. This allows
    users to prevent showing keyhints for keychains they already know.
    
    keyhint-blacklist='*' is equivalent to show-keyhints=False.
    
    Resolves #1515.

--- qutebrowser/misc/keyhintwidget.py
+++ qutebrowser/misc/keyhintwidget.py
@@ -25,6 +25,7 @@ It is intended to help discoverability of keybindings.
 """
 
 import html
+import fnmatch
 
 from PyQt5.QtWidgets import QLabel, QSizePolicy
 from PyQt5.QtCore import pyqtSlot, pyqtSignal, Qt
@@ -39,7 +40,6 @@ class KeyHintView(QLabel):
 
     Attributes:
         _win_id: Window ID of parent.
-        _enabled: If False, do not show the window at all
 
     Signals:
         reposition_keyhint: Emitted when this widget should be resized.
@@ -61,9 +61,6 @@ class KeyHintView(QLabel):
         super().__init__(parent)
         self.setTextFormat(Qt.RichText)
         self._win_id = win_id
-        self.set_enabled()
-        cfg = objreg.get('config')
-        cfg.changed.connect(self.set_enabled)
         style.set_register_stylesheet(self)
         self.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Minimum)
         self.hide()
@@ -73,13 +70,6 @@ class KeyHintView(QLabel):
 
     def __repr__(self):
         return utils.get_repr(self, win_id=self._win_id)
-
-    @config.change_filter('ui', 'show-keyhints')
-    def set_enabled(self):
-        """Update self._enabled when the config changed."""
-        self._enabled = config.get('ui', 'show-keyhints')
-        if not self._enabled:
-            self.hide()
 
     def showEvent(self, e):
         """Adjust the keyhint size when it's freshly shown."""
@@ -93,15 +83,22 @@ class KeyHintView(QLabel):
         Args:
             prefix: The current partial keystring.
         """
-        if not prefix or not self._enabled:
+        if not prefix:
             self._show_timer.stop()
             self.hide()
             return
 
+        blacklist = config.get('ui', 'keyhint-blacklist') or []
         keyconf = objreg.get('key-config')
+
+        def blacklisted(keychain):
+            return any(fnmatch.fnmatchcase(keychain, glob)
+                       for glob in blacklist)
+
         bindings = [(k, v) for (k, v)
                     in keyconf.get_bindings_for(modename).items()
-                    if k.startswith(prefix) and not utils.is_special_key(k)]
+                    if k.startswith(prefix) and not utils.is_special_key(k) and
+                    not blacklisted(k)]
 
         if not bindings:
             self._show_timer.stop()
