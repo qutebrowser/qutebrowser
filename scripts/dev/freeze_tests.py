e6f3a200c8e5590f39b491de8ac2ee73139c6561
Florian Bruhin <git@the-compiler.org>
2016-06-01 12:39:29 +0200

    Include pytest-rerunfailures for frozen tests

--- scripts/dev/freeze_tests.py
+++ scripts/dev/freeze_tests.py
@@ -56,7 +56,8 @@ def get_build_exe_options():
     opts['includes'] += pytest.freeze_includes()  # pylint: disable=no-member
     opts['includes'] += ['unittest.mock', 'PyQt5.QtTest', 'hypothesis', 'bs4',
                          'httpbin', 'jinja2.ext', 'cherrypy.wsgiserver',
-                         'cherrypy.wsgiserver.wsgiserver3', 'pstats']
+                         'cherrypy.wsgiserver.wsgiserver3', 'pstats',
+                         'pytest-rerunfailures']
 
     httpbin_dir = os.path.dirname(httpbin.__file__)
     opts['include_files'] += [
