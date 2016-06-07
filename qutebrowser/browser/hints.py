20636a23435ee54a8ebfbb6153b3949cd44348fc
Florian Bruhin <git@the-compiler.org>
2016-06-07 14:30:46 +0200

    Get normal parser via objreg

--- qutebrowser/browser/hints.py
+++ qutebrowser/browser/hints.py
@@ -956,8 +956,9 @@ class HintManager(QObject):
                 filterstr is not None):
             # apply auto-follow-timeout
             timeout = config.get('hints', 'auto-follow-timeout')
-            man_inst = modeman.instance(self._win_id)
-            normal_parser = man_inst._parsers[usertypes.KeyMode.normal]
+            keyparsers = objreg.get('keyparsers', scope='window',
+                                    window=self._win_id)
+            normal_parser = keyparsers[usertypes.KeyMode.normal]
             normal_parser.set_inhibited_timeout(timeout)
             # unpacking gets us the first (and only) key in the dict.
             self.fire(*visible)
