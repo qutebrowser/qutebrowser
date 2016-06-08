bd111c2ab1444dbfcf27d7515785f086c42104b6
Florian Bruhin <git@the-compiler.org>
2016-06-08 07:16:27 +0200

    Fix lint

--- scripts/dev/recompile_requirements.py
+++ scripts/dev/recompile_requirements.py
@@ -103,7 +103,6 @@ def get_all_names():
 
 def main():
     """Re-compile the given (or all) requirement files."""
-
     names = sys.argv[1:] if len(sys.argv) > 1 else get_all_names()
 
     for name in names:
