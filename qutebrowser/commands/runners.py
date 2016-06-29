64731c2053c546a15093b39a81eb0a26ad7ea100
Jan Verbeek <ring@openmailbox.org>
2016-06-29 21:23:42 +0200

    Fix confusing indent

--- qutebrowser/commands/runners.py
+++ qutebrowser/commands/runners.py
@@ -287,7 +287,7 @@ class CommandRunner(QObject):
                 result.cmd.run(self._win_id, args)
 
             if (result.cmdline[0] != 'repeat-command' and
-                result.cmd.mode_allowed(usertypes.KeyMode.normal)):
+                    result.cmd.mode_allowed(usertypes.KeyMode.normal)):
                 global last_command
                 last_command = (self._parse_count(text)[1],
                                 count if count is not None else result.count)
