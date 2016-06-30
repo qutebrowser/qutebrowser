49d3e9ece877476b7cd43154f4abafc3f3328812
Florian Bruhin <git@the-compiler.org>
2016-06-30 17:40:10 +0200

    Revert "Include pytest-bdd with frozen tests"
    This reverts commit fcbb5b8bacadf8b9afaf4228d92dc658216a3c24.
    
    This triggers a RecursionError when frozen...

--- scripts/dev/run_frozen_tests.py
+++ scripts/dev/run_frozen_tests.py
@@ -30,9 +30,8 @@ import pytest_instafail
 import pytest_faulthandler
 import pytest_xvfb
 import pytest_rerunfailures
-import pytest_bdd.plugin
 
 sys.exit(pytest.main(plugins=[pytestqt.plugin, pytest_mock,
                               pytest_catchlog, pytest_instafail,
                               pytest_faulthandler, pytest_xvfb,
-                              pytest_rerunfailures, pytest_bdd.plugin]))
+                              pytest_rerunfailures]))
