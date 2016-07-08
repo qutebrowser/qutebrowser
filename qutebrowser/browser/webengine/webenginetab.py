7360ea69ba1eae30510fc068a3a13bcd22309a5f
Florian Bruhin <git@the-compiler.org>
2016-07-08 13:08:31 +0200

    Fix lint

--- qutebrowser/browser/webengine/webenginetab.py
+++ qutebrowser/browser/webengine/webenginetab.py
@@ -126,7 +126,6 @@ class WebEngineCaret(tabmod.AbstractCaret):
         log.stub()
 
 
-
 class WebEngineScroller(tabmod.AbstractScroller):
 
     """QtWebEngine implementations related to scrolling."""
