1d237b0569e4ab67def3b815b03747fde7254bd2
Florian Bruhin <git@the-compiler.org>
2016-07-11 13:00:37 +0200

    pylint: ignore wrong-import-position project-wide
    While it's more accurate with isort now, we get a wrong-import-position
    for everything in scripts/ where we have to do the sys.path magic first.

--- qutebrowser/utils/typing.py
+++ qutebrowser/utils/typing.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
 
-# pylint: disable=unused-import,wrong-import-position,bad-mcs-method-argument
+# pylint: disable=unused-import,bad-mcs-method-argument
 
 """Wrapper for Python 3.5's typing module.
 
