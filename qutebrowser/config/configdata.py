1bba1e3ea940ab62cd61dcb3370e06fd4e4ad7a1
Florian Bruhin <git@the-compiler.org>
2016-08-01 14:35:57 +0200

    Remove whitespace

--- qutebrowser/config/configdata.py
+++ qutebrowser/config/configdata.py
@@ -1243,7 +1243,7 @@ def data(readonly=False):
             ('completion',
              SettingValue(typ.Font(), DEFAULT_FONT_SIZE + ' ${_monospace}'),
              "Font used in the completion widget."),
-            
+
             ('completion.category',
               SettingValue(typ.Font(), 'bold ${completion}'),
              "Font used in the completion categories."),
