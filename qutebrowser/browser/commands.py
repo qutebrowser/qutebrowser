77dd2a105f9f2a50bd285ab6a3e281f63713d716
Florian Bruhin <git@the-compiler.org>
2016-08-03 08:57:05 +0200

    Add cmdutils.check_overflow in _set_current_index

--- qutebrowser/browser/commands.py
+++ qutebrowser/browser/commands.py
@@ -86,6 +86,7 @@ class CommandDispatcher:
 
     def _set_current_index(self, idx):
         """Convenience method to set the current widget index."""
+        cmdutils.check_overflow(idx, 'int')
         return self._tabbed_browser.setCurrentIndex(idx)
 
     def _current_index(self):
