79d2116b183edec8448643bd3fce525bd47915a2
Florian Bruhin <git@the-compiler.org>
2016-08-03 09:07:12 +0200

    Remove unneeded 'return'

--- qutebrowser/browser/commands.py
+++ qutebrowser/browser/commands.py
@@ -87,7 +87,7 @@ class CommandDispatcher:
     def _set_current_index(self, idx):
         """Convenience method to set the current widget index."""
         cmdutils.check_overflow(idx, 'int')
-        return self._tabbed_browser.setCurrentIndex(idx)
+        self._tabbed_browser.setCurrentIndex(idx)
 
     def _current_index(self):
         """Convenience method to get the current widget index."""
