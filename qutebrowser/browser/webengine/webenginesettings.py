5f5b395343ea9f2f12c3c8239c73452d8eb59e56
Florian Bruhin <git@the-compiler.org>
2016-08-03 12:16:13 +0200

    pylint: Ignore failing QtWebEngineWidgets import

--- qutebrowser/browser/webengine/webenginesettings.py
+++ qutebrowser/browser/webengine/webenginesettings.py
@@ -24,7 +24,9 @@ Module attributes:
                 constants.
 """
 
+# pylint: disable=no-name-in-module,import-error,useless-suppression
 from PyQt5.QtWebEngineWidgets import QWebEngineSettings
+# pylint: enable=no-name-in-module,import-error,useless-suppression
 
 from qutebrowser.config import websettings
 from qutebrowser.utils import objreg
