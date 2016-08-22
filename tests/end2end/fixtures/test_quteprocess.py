80a3920aad857f9d06d8640dec4b7a3e06a7379a
Florian Bruhin <git@the-compiler.org>
2016-08-22 07:23:54 +0200

    Revert "Revert "tests: Use getfixturevalue""
    This reverts commit c8fe21c85fbb94a4e198a0548ce75d9645ce9c43.

--- tests/end2end/fixtures/test_quteprocess.py
+++ tests/end2end/fixtures/test_quteprocess.py
@@ -70,7 +70,7 @@ class FakeRequest:
         self.config = config
         self._httpbin = httpbin
 
-    def getfuncargvalue(self, name):
+    def getfixturevalue(self, name):
         assert name == 'httpbin'
         return self._httpbin
 
