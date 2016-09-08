qutebrowser/qutebrowser

commit faa052ad6f2b04afcf5bc8bb3d1481c8f74a5d6d

Fix hang with multiple spaces in URL completion

qutebrowser/completion/models/sortfilter.py
@@ -69,7 +69,7 @@ class CompletionFilterModel(QSortFilterProxyModel):
         with debug.log_time(log.completion, 'Setting filter pattern'):
             self.pattern = val
             val = re.escape(val)
-            val = val.replace(r'\ ', r'.*')
+            val = val.replace(r'\ +', r'.*')
             self.pattern_re = re.compile(val, re.IGNORECASE)
             self.invalidate()
             sortcol = 0

tests/end2end/features/completion.feature
@@ -5,3 +5,20 @@ Feature: Command bar completion
         When I run :set-cmd-text -s :open
         And I run :completion-item-focus next
         Then no crash should happen
+
+    Scenario: Hang with many spaces in completion (#1919)
+        # Generate some history data
+        When I open data/numbers/1.txt
+        And I open data/numbers/2.txt
+        And I open data/numbers/3.txt
+        And I open data/numbers/4.txt
+        And I open data/numbers/5.txt
+        And I open data/numbers/6.txt
+        And I open data/numbers/7.txt
+        And I open data/numbers/8.txt
+        And I open data/numbers/9.txt
+        And I open data/numbers/10.txt
+        And I run :set-cmd-text :open a                             b
+        # Make sure qutebrowser doesn't hang
+        And I run :message-info "Still alive!"
+        Then the message "Still alive!" should be shown

commit 0a3853fcb78824372b4c19136c1c74562153c4e3

Fix handling of spaces in completion
This is a regression introduced in
faa052ad6f2b04afcf5bc8bb3d1481c8f74a5d6d.

Fixes #1934.

qutebrowser/completion/models/sortfilter.py
@@ -69,7 +69,7 @@ class CompletionFilterModel(QSortFilterProxyModel):
         with debug.log_time(log.completion, 'Setting filter pattern'):
             self.pattern = val
             val = re.escape(val)
-            val = val.replace(r'\ +', r'.*')
+            val = re.sub(r'\ +', r'.*', val)
             self.pattern_re = re.compile(val, re.IGNORECASE)
             self.invalidate()
             sortcol = 0

tests/unit/completion/test_sortfilter.py
@@ -62,6 +62,8 @@ def _extract_model_data(model):
 
 @pytest.mark.parametrize('pattern, data, expected', [
     ('foo', 'barfoobar', True),
+    ('foo bar', 'barfoobar', True),
+    ('foo   bar', 'barfoobar', True),
     ('foo', 'barFOObar', True),
     ('Foo', 'barfOObar', True),
     ('ab', 'aonebtwo', False),