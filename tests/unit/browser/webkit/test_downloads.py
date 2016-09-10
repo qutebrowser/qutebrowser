0ba74ad023e0a6d63dfb72710ccd2449213c7ee3
Florian Bruhin <git@the-compiler.org>
2016-09-10 15:52:01 +0200

    Add Qt model test for download model

---
+++ tests/unit/browser/webkit/test_downloads.py
@@ -0,0 +1,30 @@
+# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+
+# Copyright 2016 Florian Bruhin (The Compiler) <mail@qutebrowser.org>
+#
+# This file is part of qutebrowser.
+#
+# qutebrowser is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# qutebrowser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from PyQt5.QtCore import QUrl
+
+from qutebrowser.browser.webkit import downloads
+
+
+def test_download_model(qapp, qtmodeltester, config_stub, cookiejar_and_cache):
+    """Simple check for download model internals."""
+    config_stub.data = {'general': {'private-browsing': False}}
+    manager = downloads.DownloadManager(win_id=0)
+    qtmodeltester.check(manager)
