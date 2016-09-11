162e6d6483476bb81f44b6d8a88a93fd2cb0fd2b
Florian Bruhin <git@the-compiler.org>
2016-09-11 15:59:50 +0200

    Fix closing with no last-visible-main-window
    Fixes #1918

--- qutebrowser/mainwindow/mainwindow.py
+++ qutebrowser/mainwindow/mainwindow.py
@@ -492,12 +492,12 @@ class MainWindow(QWidget):
 
     def _do_close(self):
         """Helper function for closeEvent."""
-        last_visible = objreg.get('last-visible-main-window')
-        if self is last_visible:
-            try:
+        try:
+            last_visible = objreg.get('last-visible-main-window')
+            if self is last_visible:
                 objreg.delete('last-visible-main-window')
-            except KeyError:
-                pass
+        except KeyError:
+           pass
         objreg.get('session-manager').save_last_window_session()
         self._save_geometry()
         log.destroy.debug("Closing window {}".format(self.win_id))
