f2e471597de49b419be6ada5dd1e8f7bb76f2bb7
Spreadyy <sandro@012345.ch>
2016-09-27 23:24:12 +0200

    Lint test failed --> edited line length

--- qutebrowser/javascript/scroll.js
+++ qutebrowser/javascript/scroll.js
@@ -66,8 +66,18 @@ window._qutebrowser.scroll = (function() {
         var pos = {
             "px": {"x": window.scrollX, "y": window.scrollY},
             "scroll": {
-                "width": Math.max(body.scrollWidth, body.offsetWidth, elem.scrollWidth, elem.offsetWidth),
-                "height": Math.max(body.scrollHeight, body.offsetHeight, elem.scrollHeight, elem.offsetHeight),
+                "width": Math.max(
+                    body.scrollWidth,
+                    body.offsetWidth,
+                    elem.scrollWidth,
+                    elem.offsetWidth
+                ),
+                "height": Math.max(
+                    body.scrollHeight,
+                    body.offsetHeight,
+                    elem.scrollHeight,
+                    elem.offsetHeight
+                ),
             },
             "inner": {
                 "width": window.innerWidth,
