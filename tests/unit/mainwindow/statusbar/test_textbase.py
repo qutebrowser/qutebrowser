96b4ab41c7c1b7e94981458e5f23e3ac0d66b1fe
Florian Bruhin <git@the-compiler.org>
2016-10-26 14:54:35 +0200

    Re-add label.show() call
    We don't actually show a window (so no need to use
    waitForWindowShown/waitExposed), but we still need to make sure the
    label is shown.

--- tests/unit/mainwindow/statusbar/test_textbase.py
+++ tests/unit/mainwindow/statusbar/test_textbase.py
@@ -50,6 +50,8 @@ def test_elided_text(fake_statusbar, qtbot, elidemode, check):
 
     long_string = 'Hello world! ' * 100
     label.setText(long_string)
+    label.show()
+
     assert check(label._elided_text)
 
 
