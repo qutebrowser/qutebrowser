c7bfefeb083bac602c9b5123cbd086b28c486ef4
Florian Bruhin <git@the-compiler.org>
2016-10-26 08:08:00 +0200

    tests: Remove useless show() call
    We add it to a layout anyways, so it will already be shown.

--- tests/unit/mainwindow/statusbar/test_textbase.py
+++ tests/unit/mainwindow/statusbar/test_textbase.py
@@ -50,10 +50,6 @@ def test_elided_text(fake_statusbar, qtbot, elidemode, check):
 
     long_string = 'Hello world! ' * 100
     label.setText(long_string)
-
-    with qtbot.waitExposed(label):
-        label.show()
-
     assert check(label._elided_text)
 
 
