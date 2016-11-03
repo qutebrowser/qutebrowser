ced618eccb4c3adaab234296e70d298116c08e8f
Florian Bruhin <git@the-compiler.org>
2016-11-03 08:37:30 +0100

    Pass all arguments to pip on Travis

--- scripts/dev/ci/travis_install.sh
+++ scripts/dev/ci/travis_install.sh
@@ -54,7 +54,7 @@ brew_install() {
 
 pip_install() {
     # this uses python2
-    travis_retry sudo -H python -m pip install $1
+    travis_retry sudo -H python -m pip install "$@"
 }
 
 npm_install() {
