a9d48753efac02e9d1cead2ef51da2d1e3eb8a3d
Florian Bruhin <git@the-compiler.org>
2016-11-10 22:23:56 +0100

    Adjust docstring

--- qutebrowser/browser/webkit/certificateerror.py
+++ qutebrowser/browser/webkit/certificateerror.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Wrapper over a QSslCertificateError."""
+"""Wrapper over a QSslError."""
 
 
 from PyQt5.QtNetwork import QSslError
