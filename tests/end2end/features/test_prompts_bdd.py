db4a6063215c4730e913ec6ef493ac22c47dd656
Florian Bruhin <git@the-compiler.org>
2016-11-10 21:20:43 +0100

    Re-enable SSL download test on QtWebEngine

--- tests/end2end/features/test_prompts_bdd.py
+++ tests/end2end/features/test_prompts_bdd.py
@@ -21,15 +21,6 @@ import pytest_bdd as bdd
 bdd.scenarios('prompts.feature')
 
 
-@bdd.when("I clear SSL errors")
-def clear_ssl_errors(request, quteproc):
-    if request.config.webengine:
-        quteproc.terminate()
-        quteproc.start()
-    else:
-        quteproc.send_cmd(':debug-clear-ssl-errors')
-
-
 @bdd.when("I load an SSL page")
 def load_ssl_page(quteproc, ssl_server):
     # We don't wait here as we can get an SSL question.
