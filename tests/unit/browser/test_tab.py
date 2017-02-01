428e4959352d73b257321bf1deec2c5216d930af
Florian Bruhin <git@the-compiler.org>
2017-02-01 11:09:08 +0100

    Unconditionally disable test_tab
    This now also causes segfaults with Qt 5.8, so something is definitely wrong
    here...
    
    See #1638 and #2261

--- tests/unit/browser/test_tab.py
+++ tests/unit/browser/test_tab.py
@@ -115,8 +115,7 @@ class Tab(browsertab.AbstractTab):
         pass
 
 
-@pytest.mark.skipif(PYQT_VERSION < 0x050600,
-                    reason='Causes segfaults, see #1638')
+@pytest.mark.xfail(run=False, reason='Causes segfaults, see #1638')
 def test_tab(qtbot, view, config_stub, tab_registry, mode_manager):
     tab_w = Tab(win_id=0, mode_manager=mode_manager)
     qtbot.add_widget(tab_w)
