72bdf69fe34469ed3b9d6fd6cc35b09d52bf8e2d
Florian Bruhin <git@the-compiler.org>
2017-02-01 09:51:50 +0100

    Fix long line

--- qutebrowser/utils/version.py
+++ qutebrowser/utils/version.py
@@ -210,7 +210,8 @@ def _pdfjs_version():
     else:
         pdfjs_file = pdfjs_file.decode('utf-8')
         version_re = re.compile(
-            r"^ *(PDFJS\.version|var pdfjsVersion) = '([^']+)';$", re.MULTILINE)
+            r"^ *(PDFJS\.version|var pdfjsVersion) = '([^']+)';$",
+            re.MULTILINE)
 
         match = version_re.search(pdfjs_file)
         if not match:
