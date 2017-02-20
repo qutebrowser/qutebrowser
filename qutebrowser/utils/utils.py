c6645d47bab522a9aa9869fe19fb9ff93e697c01
Ryan Roden-Corrent <ryan@rcorre.net>
2017-02-19 21:39:15 -0500

    Remove newest_slice and StatusBar._option.
    newest_slice is no longer needed after the completion refactor. Now that
    history is based on the SQL backend, LIMIT is used instead.
    
    StatusBar._option is not used, though I'm not sure why vulture only caught it
    now.

--- qutebrowser/utils/utils.py
+++ qutebrowser/utils/utils.py
@@ -737,25 +737,6 @@ def sanitize_filename(name, replacement='_'):
     return name
 
 
-def newest_slice(iterable, count):
-    """Get an iterable for the n newest items of the given iterable.
-
-    Args:
-        count: How many elements to get.
-               0: get no items:
-               n: get the n newest items
-              -1: get all items
-    """
-    if count < -1:
-        raise ValueError("count can't be smaller than -1!")
-    elif count == 0:
-        return []
-    elif count == -1 or len(iterable) < count:
-        return iterable
-    else:
-        return itertools.islice(iterable, len(iterable) - count, len(iterable))
-
-
 def set_clipboard(data, selection=False):
     """Set the clipboard to some given data."""
     if selection and not supports_selection():
