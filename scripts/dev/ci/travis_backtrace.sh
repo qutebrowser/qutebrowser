b117d981a5443e309acac3596fcfde5f89f3f10c
Florian Bruhin <git@the-compiler.org>
2017-03-05 19:34:37 +0100

    Install debug packages on Ubuntu

--- scripts/dev/ci/travis_backtrace.sh
+++ scripts/dev/ci/travis_backtrace.sh
@@ -3,16 +3,15 @@
 #  Find all possible core files under current directory. Attempt
 #  to determine exe using file(1) and dump stack trace with gdb.
 #
-say () { printf "\033[91m%s\033[39m\n" "$@" >&2; }
-die () { say "$@"; exit 1; }
-
 
 case $TESTENV in
     py34-cov)
         exe=/usr/bin/python3.4
+        full=full
         ;;
     py3*-pyqt*)
         exe=$(readlink -f .tox/$TESTENV/bin/python)
+        full=
         ;;
     *)
         echo "Skipping coredump analysis in testenv $TESTENV!"
@@ -20,4 +19,4 @@ case $TESTENV in
         ;;
 esac
 
-find . -name *.core -o -name core -exec gdb --batch --quiet -ex "thread apply all bt full" "$exe" {} \;
+find . -name *.core -o -name core -exec gdb --batch --quiet -ex "thread apply all bt $full" "$exe" {} \;
--- scripts/dev/ci/travis_install.sh
+++ scripts/dev/ci/travis_install.sh
@@ -113,7 +113,7 @@ tox --version
 case $TESTENV in
     py34-cov)
         pip_install -r misc/requirements/requirements-codecov.txt
-        apt_install xvfb $pyqt_pkgs libpython3.4-dev gdb apport
+        apt_install xvfb $pyqt_pkgs libpython3.4-dev gdb apport libqt5webkit5-dbg python3-pyqt5-dbg python3-pyqt5.qtquick-dbg python3-pyqt5.qtwebkit-dbg python3-dbg
         check_pyqt
         ;;
     py3*-pyqt*)
