fd9b86a34017eed5edde19a231f99c8b0b330a65
Florian Bruhin <git@the-compiler.org>
2017-04-06 21:40:26 +0200

    Remove unused imports

--- qutebrowser/browser/webkit/network/networkreply.py
+++ qutebrowser/browser/webkit/network/networkreply.py
@@ -26,8 +26,8 @@
 
 """Special network replies.."""
 
-from PyQt5.QtNetwork import QNetworkReply, QNetworkRequest, QNetworkAccessManager
-from PyQt5.QtCore import pyqtSlot, QIODevice, QByteArray, QTimer, QUrl
+from PyQt5.QtNetwork import QNetworkReply, QNetworkRequest
+from PyQt5.QtCore import pyqtSlot, QIODevice, QByteArray, QTimer
 
 
 class FixedDataNetworkReply(QNetworkReply):
