3125b69d194309464ef0abcaa4f915ff4cf4316f
Florian Bruhin <git@the-compiler.org>
2017-04-25 06:43:31 +0200

    Fix no-cover pragma

--- qutebrowser/browser/webkit/cache.py
+++ qutebrowser/browser/webkit/cache.py
@@ -54,8 +54,8 @@ class DiskCache(QNetworkDiskCache):
         if size is None:
             size = 1024 * 1024 * 50  # default from QNetworkDiskCachePrivate
         # WORKAROUND for https://bugreports.qt.io/browse/QTBUG-59909
-        if (qtutils.version_check('5.7.1') and  # pragma: no cover
-                not qtutils.version_check('5.9')):
+        if (qtutils.version_check('5.7.1') and
+                not qtutils.version_check('5.9')):  # pragma: no cover
             size = 0
         self.setMaximumCacheSize(size)
 
