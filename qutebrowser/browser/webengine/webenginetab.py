b37d040d44e5e14f10f93b2d3acd02f8bee9d4ad
Florian Bruhin <git@the-compiler.org>
2017-06-14 10:57:32 +0200

    Add version check for 5.8 search segfault workaround

--- qutebrowser/browser/webengine/webenginetab.py
+++ qutebrowser/browser/webengine/webenginetab.py
@@ -614,9 +614,10 @@ class WebEngineTab(browsertab.AbstractTab):
 
     def shutdown(self):
         self.shutting_down.emit()
-        # WORKAROUND for
-        # https://bugreports.qt.io/browse/QTBUG-58563
-        self.search.clear()
+        if qtutils.version_check('5.8', exact=True):
+            # WORKAROUND for
+            # https://bugreports.qt.io/browse/QTBUG-58563
+            self.search.clear()
         self._widget.shutdown()
 
     def reload(self, *, force=False):
