defe140d98a6a82b1fba0c1440fb827a758b4733
Florian Bruhin <git@the-compiler.org>
2017-07-04 19:56:54 +0200

    build_release: Run tox with -vv

--- scripts/dev/build_release.py
+++ scripts/dev/build_release.py
@@ -64,7 +64,7 @@ def call_tox(toxenv, *args, python=sys.executable):
     env['PYTHON'] = python
     env['PATH'] = os.environ['PATH'] + os.pathsep + os.path.dirname(python)
     subprocess.check_call(
-        [sys.executable, '-m', 'tox', '-v', '-e', toxenv] + list(args),
+        [sys.executable, '-m', 'tox', '-vv', '-e', toxenv] + list(args),
         env=env)
 
 
