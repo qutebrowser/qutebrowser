7e36310e8a843a72690adbb88c79b209463f37e8
Florian Bruhin <git@the-compiler.org>
2017-07-21 13:18:20 +0200

    Shorten and update README

--- scripts/dev/ci/travis_run.sh
+++ scripts/dev/ci/travis_run.sh
@@ -4,7 +4,6 @@ if [[ $DOCKER ]]; then
     docker run --privileged -v $PWD:/outside -e QUTE_BDD_WEBENGINE=$QUTE_BDD_WEBENGINE -e DOCKER=$DOCKER qutebrowser/travis:$DOCKER
 else
     args=()
-    [[ $TESTENV == docs ]] && args=('--no-authors')
     [[ $TRAVIS_OS_NAME == osx ]] && args=('--qute-bdd-webengine' '--no-xvfb')
 
     tox -e $TESTENV -- "${args[@]}"
