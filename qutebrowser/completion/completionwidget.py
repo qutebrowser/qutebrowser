b61691684ec4f3a6c2f4682dd087bcd142a2bd63
Ryan Roden-Corrent <ryan@rcorre.net>
2017-07-22 18:06:16 -0400

    Clear selection when setting completion pattern.
    It doesn't make sense to have an active selection while you are
    filtering by entering text. You should be in one of two states:
    
    1. Tabbing through completions (valid selection)
    2. Entering a filter pattern (invalid selection)
    
    Fixes #2843, where a crash would occur after the following:
    
    1. tab to an item other than the first
    2. <backspace>
    3. re-type last character
    4. <ctrl-d>
    
    This would try to delete an out of range index.

--- qutebrowser/completion/completionwidget.py
+++ qutebrowser/completion/completionwidget.py
@@ -297,6 +297,7 @@ class CompletionView(QTreeView):
         self.pattern = pattern
         with debug.log_time(log.completion, 'Set pattern {}'.format(pattern)):
             self.model().set_pattern(pattern)
+            self.selectionModel().clear()
             self._maybe_update_geometry()
             self._maybe_show()
 
