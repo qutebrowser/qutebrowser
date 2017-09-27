fef1a6524752c18e63a33bda02d823cbe5de2519
Florian Bruhin <git@the-compiler.org>
2017-09-27 10:37:42 +0200

    Fix ListOrValue.to_doc signature

--- qutebrowser/config/configtypes.py
+++ qutebrowser/config/configtypes.py
@@ -524,17 +524,17 @@ class ListOrValue(BaseType):
         else:
             return self.valtype.to_str(value)
 
-    def to_doc(self, value):
+    def to_doc(self, value, indent=0):
         if value is None:
             return 'empty'
 
         if isinstance(value, list):
             if len(value) == 1:
-                return self.valtype.to_doc(value[0])
+                return self.valtype.to_doc(value[0], indent)
             else:
-                return self.listtype.to_doc(value)
+                return self.listtype.to_doc(value, indent)
         else:
-            return self.valtype.to_doc(value)
+            return self.valtype.to_doc(value, indent)
 
 
 class FlagList(List):
