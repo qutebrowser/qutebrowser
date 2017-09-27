3b2222ecc66b4a33d5e59649d656052664585735
Ryan Roden-Corrent <ryan@rcorre.net>
2017-09-26 22:26:28 -0400

    Ensure HistoryCategory works in isolation.
    While QSortFilterProxyModel emits layoutChanged when changing the
    pattern, QSqlQueryModel emits modelReset. As only layoutChanged was
    connected, a HistoryCategory would only work in a model that also had at
    least one ListCategory.
    
    The simplest solution is to have the parent model emit the signal
    directly. This also emits a single signal on a pattern change rather
    that one for each child model.
    
    Resolves #3016.

--- qutebrowser/completion/models/completionmodel.py
+++ qutebrowser/completion/models/completionmodel.py
@@ -60,8 +60,6 @@ class CompletionModel(QAbstractItemModel):
     def add_category(self, cat):
         """Add a completion category to the model."""
         self._categories.append(cat)
-        cat.layoutAboutToBeChanged.connect(self.layoutAboutToBeChanged)
-        cat.layoutChanged.connect(self.layoutChanged)
 
     def data(self, index, role=Qt.DisplayRole):
         """Return the item data for index.
@@ -179,8 +177,10 @@ class CompletionModel(QAbstractItemModel):
             pattern: The filter pattern to set.
         """
         log.completion.debug("Setting completion pattern '{}'".format(pattern))
+        self.layoutAboutToBeChanged.emit()
         for cat in self._categories:
             cat.set_pattern(pattern)
+        self.layoutChanged.emit()
 
     def first_item(self):
         """Return the index of the first child (non-category) in the model."""
