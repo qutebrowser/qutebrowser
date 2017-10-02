c6d140a40a77345c484bc8557f84f97d33539bfe
Josefson Fraga <josefson@users.noreply.github.com>
2017-10-02 00:26:47 -0400

    adding script to import history data from other browsers

---
+++ scripts/hist_importer.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+
+# This file is part of qutebrowser.
+
+# qutebrowser is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# qutebrowser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
+
+
+'''
+Tool to import browser history data from other browsers. Although, safari
+support is still on the way.
+'''
+
+
+import argparse
+import sqlite3
+import sys
+
+
+def parser():
+    """Parse command line arguments."""
+    description = 'This program is meant to extract browser history from your'\
+                  'previous browser and import them into qutebrowser.'
+    epilog = 'Databases:\n\tQute: Is named "history.sqlite" and can be found '\
+             'at your --basedir. In order to find where your basedir is you '\
+             'can run ":open qute:version" inside qutebrowser.'\
+             '\n\tFirerox: Is named "places.sqlite", and can be found at your'\
+             'system\'s profile folder. Check this link for where it is locat'\
+             'ed: http://kb.mozillazine.org/Profile_folder'\
+             '\n\tChrome: Is named "History", and can be found at the respec'\
+             'tive User Data Directory. Check this link for where it is locat'\
+             'ed: https://chromium.googlesource.com/chromium/src/+/master/'\
+             'docs/user_data_dir.md\n\n'\
+             'Example: $this_script.py -b firefox -s /Firefox/Profile/places.'\
+             'sqlite -d /qutebrowser/data/history.sqlite'
+    parser = argparse.ArgumentParser(
+        description=description, epilog=epilog,
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument('-b', '--browser', dest='browser', required=True,
+                        type=str, help='Browsers: {firefox, chrome, safari}')
+    parser.add_argument('-s', '--source', dest='source', required=True,
+                        type=str, help='Source: fullpath to the sqlite data'
+                        'base file from the source browser.')
+    parser.add_argument('-d', '--dest', dest='dest', required=True, type=str,
+                        help='Destination: The fullpath to the qutebrowser '
+                        'sqlite database')
+    return parser.parse_args()
+
+
+def open_db(db):
+    """Open connection with database."""
+    try:
+        conn = sqlite3.connect(db)
+        return conn
+    except Exception as e:
+        print('Error: {}'.format(e))
+        raise('Error: There was some error trying to to connect with the [{}]'
+              'database. Verify if the filepath is correct or is being used.'.
+              format(db))
+
+
+def extract(source, query):
+    """Performs extraction of (datetime,url,title) from source."""
+    try:
+        conn = open_db(source)
+        cursor = conn.cursor()
+        cursor.execute(query)
+        history = cursor.fetchall()
+        conn.close()
+        return history
+    except Exception as e:
+        # print('Error: {}'.format(e))
+        print(type(source))
+        raise('Error: There was some error trying to to connect with the [{}]'
+              'database. Verify if the filepath is correct or is being used.'.
+              format(str(source)))
+
+
+def clean(history):
+    """Receives a list of records:(datetime,url,title). And clean all records
+    in place, that has a NULL/None datetime attribute. Otherwise Qutebrowser
+    will throw errors."""
+    nulls = [record for record in history if record[0] is None]
+    for null_datetime in nulls:
+        history.remove(null_datetime)
+    return history
+
+
+def insert_qb(history, dest):
+    conn = open_db(dest)
+    cursor = conn.cursor()
+    cursor.executemany(
+        'INSERT INTO History (url,title,atime) VALUES (?,?,?)', history
+    )
+    conn.commit()
+    conn.close()
+
+
+def main():
+    args = parser()
+    browser = args.browser.lower()
+    source, dest = args.source, args.dest
+    query = {
+        'firefox': 'select url,title,last_visit_date/1000000 as date '
+        'from moz_places',
+        'chrome': 'select url,title,last_visit_time/10000000 as date '
+        'from urls',
+        'safari': None
+    }
+    if browser not in query:
+        sys.exit('Sorry, the selected browser: "{}" is not supported.'.format(
+                 browser))
+    else:
+        if browser == 'safari':
+            print('Sorry, currently we do not support this browser.')
+            sys.exit(1)
+        history = extract(source, query[browser])
+        history = clean(history)
+        insert_qb(history, dest)
+
+
+if __name__ == "__main__":
+    main()
