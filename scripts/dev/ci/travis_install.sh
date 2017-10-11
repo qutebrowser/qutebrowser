5f7ce74e66955e6a1c780bddd15becc31a4f887b
Florian Bruhin <git@the-compiler.org>
2017-10-11 11:28:53 +0200

    Install LibYAML on macOS on Travis
    See #2777

--- scripts/dev/ci/travis_install.sh
+++ scripts/dev/ci/travis_install.sh
@@ -83,7 +83,7 @@ elif [[ $TRAVIS_OS_NAME == osx ]]; then
     sudo -H python get-pip.py
 
     brew --version
-    brew_install python3 qt5 pyqt5
+    brew_install python3 qt5 pyqt5 libyaml
 
     pip_install -r misc/requirements/requirements-tox.txt
     python3 -m pip --version
