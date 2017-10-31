a5426ca4ac525d175d12e91635cf8d45a2add13c
Jay Kamat <jaygkamat@gmail.com>
2017-10-31 19:06:39 -0400

    Abort pinned tab prompt if tab is destroyed
    Closes #3223
    
    (cherry picked from commit cb7e6ab02d22995bff0728ee5ee78820221f2d2e)

--- qutebrowser/mainwindow/tabbedbrowser.py
+++ qutebrowser/mainwindow/tabbedbrowser.py
@@ -249,13 +249,14 @@ class TabbedBrowser(tabwidget.TabWidget):
     def tab_close_prompt_if_pinned(self, tab, force, yes_action):
         """Helper method for tab_close.
 
-        If tab is pinned, prompt. If everything is good, run yes_action.
+        If tab is pinned, prompt. If not, run yes_action.
+        If tab is destroyed, abort question.
         """
         if tab.data.pinned and not force:
             message.confirm_async(
                 title='Pinned Tab',
                 text="Are you sure you want to close a pinned tab?",
-                yes_action=yes_action, default=False)
+                yes_action=yes_action, default=False, abort_on=[tab.destroyed])
         else:
             yes_action()
 
