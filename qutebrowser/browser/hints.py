21364f135e94facd80a6ffbdacc139495e04b735
Florian Bruhin <git@the-compiler.org>
2017-11-10 10:23:16 +0100

    Improve documentation for :hint --rapid
    (cherry picked from commit 456c854f06da4f8dcc5ef96024a8be1252bd77db)

--- qutebrowser/browser/hints.py
+++ qutebrowser/browser/hints.py
@@ -616,8 +616,10 @@ class HintManager(QObject):
         """Start hinting.
 
         Args:
-            rapid: Whether to do rapid hinting. This is only possible with
-                   targets `tab` (with `tabs.background_tabs=true`), `tab-bg`,
+            rapid: Whether to do rapid hinting. With rapid hinting, the hint
+                   mode isn't left after a hint is followed, so you can easily
+                   open multiple links. This is only possible with targets
+                   `tab` (with `tabs.background_tabs=true`), `tab-bg`,
                    `window`, `run`, `hover`, `userscript` and `spawn`.
             add_history: Whether to add the spawned or yanked link to the
                          browsing history.
