ccb8e74998b5b186482db3bfe92917829431ab20
François Chavant <github@mail.chavant.info>
2017-12-03 09:29:38 +0100

    Use koalaman/shellcheck:latest

--- scripts/dev/ci/travis_run.sh
+++ scripts/dev/ci/travis_run.sh
@@ -16,14 +16,12 @@ elif [[ $TESTENV == eslint ]]; then
     eslint --color --report-unused-disable-directives .
 elif [[ $TESTENV == shellcheck ]]; then
     dev_scripts=$( find scripts/dev/ -name '*.sh' -print0 | xargs -0 )
-    # false positive: we are using 'find -exec +'
-    # shellcheck disable=SC2038
     userscripts=$( find misc/userscripts/ -type f -exec grep -lE '[/ ][bd]ash$|[/ ]sh$|[/ ]ksh$' {} + | xargs )
     IFS=" " read -r -a scripts <<< "$dev_scripts $userscripts"
     docker run \
            -v "$PWD:/outside" \
 	   -w /outside \
-           koalaman/shellcheck:stable "${scripts[@]}"
+           koalaman/shellcheck:latest "${scripts[@]}"
 else
     args=()
     [[ $TRAVIS_OS_NAME == osx ]] && args=('--qute-bdd-webengine' '--no-xvfb')
