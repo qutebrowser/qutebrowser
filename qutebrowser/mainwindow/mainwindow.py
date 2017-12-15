26a622c46d71996c42a2f1b6f5b380e2a6a9c620
Florian Bruhin <git@the-compiler.org>
2017-12-15 16:25:22 +0100

    Disable wrong inconsistent-return-statements

--- qutebrowser/mainwindow/mainwindow.py
+++ qutebrowser/mainwindow/mainwindow.py
@@ -103,7 +103,8 @@ def raise_window(window):
     QApplication.instance().alert(window)
 
 
-def get_target_window():
+# WORKAROUND for https://github.com/PyCQA/pylint/issues/1770
+def get_target_window():  # pylint: disable=inconsistent-return-statements
     """Get the target window for new tabs, or None if none exist."""
     try:
         win_mode = config.val.new_instance_open_target_window
