e254ea2fa7e617432c656ad7f9055c72af64e2bc
Artur Shaikhullin <ashaihullin@gmail.com>
2017-12-29 23:43:39 +0600

    Add license and description

--- qutebrowser/javascript/webengine_caret.js
+++ qutebrowser/javascript/webengine_caret.js
@@ -1,3 +1,28 @@
+/**
+ * Copyright 2016-2017 Florian Bruhin (The Compiler) <mail@qutebrowser.org>
+ *
+ * This file is part of qutebrowser.
+ *
+ * qutebrowser is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * qutebrowser is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Ported chrome-caretbrowsing extension.
+ * Create and control div caret, which listen commands from qutebrowser,
+ * change document selection model and div caret position.
+ */
+
 "use strict"; // eslint-disable-line max-lines
 
 window._qutebrowser.caret = (function() { // eslint-disable-line max-statements,max-len
