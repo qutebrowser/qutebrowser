981f5fd09b93a194f2e768697e6d260dfa078ece
Artur Shaikhullin <ashaihullin@gmail.com>
2017-12-30 23:20:29 +0600

    Remove unused import

--- tests/end2end/features/test_caret_bdd.py
+++ tests/end2end/features/test_caret_bdd.py
@@ -17,8 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
 
-# pylint: disable=unused-import
-import pytest
 import pytest_bdd as bdd
 
 from end2end.features.test_yankpaste_bdd import init_fake_clipboard
