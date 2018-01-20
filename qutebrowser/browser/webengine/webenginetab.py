501e4dba9c7cdccd408c8d1f26b50b7a5bae1875
Florian Bruhin <git@the-compiler.org>
2018-01-20 18:50:17 +0100

    Fix crash when getting signals for closed tabs
    Fixes #3498
    
    (cherry picked from commit 748de85ba2cc7bd8557a87fce88aab0ac3f3ad27)

--- qutebrowser/browser/webengine/webenginetab.py
+++ qutebrowser/browser/webengine/webenginetab.py
@@ -597,6 +597,9 @@ class WebEngineTab(browsertab.AbstractTab):
 
     @pyqtSlot()
     def _restore_zoom(self):
+        if sip.isdeleted(self._widget):
+            # https://github.com/qutebrowser/qutebrowser/issues/3498
+            return
         if self._saved_zoom is None:
             return
         self.zoom.set_factor(self._saved_zoom)
