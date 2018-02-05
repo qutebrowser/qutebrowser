8b29ce93ecec8590e2bb4135cca2fc7b2480265b
George Edward Bulmer <gebulmer@googlemail.com>
2018-02-05 21:40:12 +0000

    Add substitutions for the other 3 types

--- qutebrowser/commands/runners.py
+++ qutebrowser/commands/runners.py
@@ -65,7 +65,10 @@ def replace_variables(win_id, arglist):
             QUrl.DecodeReserved | QUrl.RemovePassword),
         'clipboard': utils.get_clipboard,
         'primary': lambda: utils.get_clipboard(selection=True),
-        'suburl': lambda: '{url}'
+        'suburl': lambda: '{url}',
+        'suburl:pretty': lambda: '{url:pretty}',
+        'subclipboard': lambda: '{clipboard}',
+        'subprimary': lambda: '{primary}',
     }
     values = {}
     args = []
