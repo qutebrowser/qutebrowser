53e7d13c2d35c083bf820fbfbcd8271f8001f52e
Florian Bruhin <git@the-compiler.org>
2018-02-08 10:42:55 +0100

    Skip failing scrolling tests on Qt 5.10 on Travis
    See #3572

--- tests/conftest.py
+++ tests/conftest.py
@@ -64,6 +64,10 @@ def _apply_platform_markers(config, item):
         ('no_ci', 'CI' in os.environ, "Skipped on CI."),
         ('issue2478', utils.is_windows and config.webengine,
          "Broken with QtWebEngine on Windows"),
+        ('issue3572',
+         qtutils.version_check('5.10', compiled=False, exact=True) and
+         config.webengine and 'TRAVIS' in os.environ,
+         "Broken with QtWebEngine with Qt 5.10 on Travis"),
         ('unicode_locale', sys.getfilesystemencoding() == 'ascii',
          "Skipped because of ASCII locale"),
     ]
