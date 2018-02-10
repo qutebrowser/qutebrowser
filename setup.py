f25e706e11a3ea56014ec8390937acb5f9998813
Florian Bruhin <git@the-compiler.org>
2018-02-10 20:08:33 +0100

    Add Python 3.7 compatibility to setup.py

--- setup.py
+++ setup.py
@@ -95,6 +95,7 @@ try:
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
             'Topic :: Internet',
             'Topic :: Internet :: WWW/HTTP',
             'Topic :: Internet :: WWW/HTTP :: Browsers',
