0a9c0a1385f1491070b186372a1fd2a33c7952ff
Marc Jauvin <marc.jauvin@gmail.com>
2018-02-12 07:16:01 -0500

    on_load_finished() signal handler for scroll-pos
    remove get_page()

--- qutebrowser/browser/webengine/webenginetab.py
+++ qutebrowser/browser/webengine/webenginetab.py
@@ -490,13 +490,16 @@ class WebEngineHistory(browsertab.AbstractHistory):
     def load_items(self, items):
         stream, _data, cur_data = tabhistory.serialize(items)
         qtutils.deserialize_stream(stream, self._history)
+        @pyqtSlot()
+        def _on_load_finished():
+            self._tab.scroller.to_point(cur_data['scroll-pos'])
+            self._tab.load_finished.disconnect(_on_load_finished)
         if cur_data is not None:
             if 'zoom' in cur_data:
                 self._tab.zoom.set_factor(cur_data['zoom'])
             if ('scroll-pos' in cur_data and
                     self._tab.scroller.pos_px() == QPoint(0, 0)):
-                self._tab.get_page().loadFinished.connect(functools.partial(
-                    self._tab.scroller.to_point, cur_data['scroll-pos']))
+                self._tab.load_finished.connect(_on_load_finished)
 
 
 class WebEngineZoom(browsertab.AbstractZoom):
@@ -642,9 +645,6 @@ class WebEngineTab(browsertab.AbstractTab):
             return
         self.zoom.set_factor(self._saved_zoom)
         self._saved_zoom = None
-
-    def get_page(self):
-        return self._widget.page()
 
     def openurl(self, url):
         self._saved_zoom = self.zoom.factor()
