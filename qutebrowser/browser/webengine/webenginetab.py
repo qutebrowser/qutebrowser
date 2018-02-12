68b12e6e9fcf225a10f2d11672d65c04a17d5d8f
Marc Jauvin <marc.jauvin@gmail.com>
2018-02-12 07:31:02 -0500

    add missing lines

--- qutebrowser/browser/webengine/webenginetab.py
+++ qutebrowser/browser/webengine/webenginetab.py
@@ -490,10 +490,12 @@ class WebEngineHistory(browsertab.AbstractHistory):
     def load_items(self, items):
         stream, _data, cur_data = tabhistory.serialize(items)
         qtutils.deserialize_stream(stream, self._history)
+
         @pyqtSlot()
         def _on_load_finished():
             self._tab.scroller.to_point(cur_data['scroll-pos'])
             self._tab.load_finished.disconnect(_on_load_finished)
+
         if cur_data is not None:
             if 'zoom' in cur_data:
                 self._tab.zoom.set_factor(cur_data['zoom'])
