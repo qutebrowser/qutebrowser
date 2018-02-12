9397cc74c1cd175d6a0ff08384290d8235fde050
George Edward Bulmer <gebulmer@googlemail.com>
2018-02-12 14:24:53 +0000

    Pylint indentation fix

--- qutebrowser/config/configexc.py
+++ qutebrowser/config/configexc.py
@@ -94,8 +94,8 @@ class ConfigErrorDesc:
     def __str__(self):
         if self.traceback:
             return '{} - {}: {}'.format(self.text,
-                                         self.exception.__class__.__name__,
-                                         self.exception)
+                                        self.exception.__class__.__name__,
+                                        self.exception)
         return '{}: {}'.format(self.text, self.exception)
 
     def with_text(self, text):
