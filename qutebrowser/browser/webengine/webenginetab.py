2ffb1604d36d442223819e69f2522664c251d2ce
Jay Kamat <jaygkamat@gmail.com>
2018-02-21 10:01:27 -0500

    Convert search to blue selection when entering caret mode

--- qutebrowser/browser/webengine/webenginetab.py
+++ qutebrowser/browser/webengine/webenginetab.py
@@ -218,6 +218,13 @@ class WebEngineCaret(browsertab.AbstractCaret):
         if mode != usertypes.KeyMode.caret:
             return
 
+        # Clear search, replace with blue selection
+        if self._tab.search.search_displayed:
+            # We are currently in search mode.
+            # convert the search to a blue selection so we can operate on it
+            # https://bugreports.qt.io/browse/QTBUG-60673
+            self._tab.search.clear()
+
         self._tab.run_js_async(
             javascript.assemble('caret', 'setPlatform', sys.platform))
         self._js_call('setInitialCursor')
