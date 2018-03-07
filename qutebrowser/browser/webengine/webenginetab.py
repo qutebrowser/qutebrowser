87c66447511b5e72e08e089dd036fd2478f56f54
Florian Bruhin <git@the-compiler.org>
2018-03-07 23:54:58 +0100

    Add predicted_navigation for reload()
    This should avoid a double-reload for 'tsh' etc.

--- qutebrowser/browser/webengine/webenginetab.py
+++ qutebrowser/browser/webengine/webenginetab.py
@@ -706,6 +706,7 @@ class WebEngineTab(browsertab.AbstractTab):
         self._widget.shutdown()
 
     def reload(self, *, force=False):
+        self.predicted_navigation.emit(self.url())
         if force:
             action = QWebEnginePage.ReloadAndBypassCache
         else:
--- qutebrowser/browser/webkit/webkittab.py
+++ qutebrowser/browser/webkit/webkittab.py
@@ -701,6 +701,7 @@ class WebKitTab(browsertab.AbstractTab):
         self._widget.shutdown()
 
     def reload(self, *, force=False):
+        self.predicted_navigation.emit(self.url())
         if force:
             action = QWebPage.ReloadAndBypassCache
         else:
