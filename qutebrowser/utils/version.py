c01f674234780b9198d5a1bd2b6f1c32f4c97462
Florian Bruhin <git@the-compiler.org>
2018-03-08 12:27:41 +0100

    Add Chromium versions to _chromium_version comment

--- qutebrowser/utils/version.py
+++ qutebrowser/utils/version.py
@@ -305,7 +305,19 @@ def _pdfjs_version():
 
 
 def _chromium_version():
-    """Get the Chromium version for QtWebEngine."""
+    """Get the Chromium version for QtWebEngine.
+
+    This can also be checked by looking at this file with the right Qt tag:
+    https://github.com/qt/qtwebengine/blob/dev/tools/scripts/version_resolver.py#L41
+
+    Quick reference:
+    Qt 5.7:  Chromium 49
+    Qt 5.8:  Chromium 53
+    Qt 5.9:  Chromium 56
+    Qt 5.10: Chromium 61
+    Qt 5.11: Chromium 63
+    Qt 5.12: Chromium 65 (?)
+    """
     if QWebEngineProfile is None:
         # This should never happen
         return 'unavailable'
