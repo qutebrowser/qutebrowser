4b6bfd066c34c413cc77ba6c60eeaa31d85d17d7
Florian Bruhin <git@the-compiler.org>
2018-03-14 18:12:29 +0100

    Don't emit predicted_navigation for reloads at all
    When we reload a page because of a config change, we won't get another
    titleChanged signal (at least sometimes).
    
    Also, the predicted_navigation signal is worthless when reloading anyways, as
    we're going to load the same URL and not something different.
    
    Fixes #3718
    
    (cherry picked from commit 0418a865c17c26720219e33a67c88410a6ac7181)

--- qutebrowser/browser/webengine/webenginetab.py
+++ qutebrowser/browser/webengine/webenginetab.py
@@ -706,9 +706,6 @@ class WebEngineTab(browsertab.AbstractTab):
         self._widget.shutdown()
 
     def reload(self, *, force=False):
-        if self.url().isValid():
-            self.predicted_navigation.emit(self.url())
-
         if force:
             action = QWebEnginePage.ReloadAndBypassCache
         else:
