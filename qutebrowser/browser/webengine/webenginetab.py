da8b6fb50a2d02ac9e34cfada448d17f5d93171d
Florian Bruhin <git@the-compiler.org>
2018-03-19 14:11:01 +0100

    Decrease maximum repetitions for QtWebEngine scrolling
    At least for Qt debug builds, 5000 seems to be much too much.
    See #3661

--- qutebrowser/browser/webengine/webenginetab.py
+++ qutebrowser/browser/webengine/webenginetab.py
@@ -379,7 +379,7 @@ class WebEngineScroller(browsertab.AbstractScroller):
 
     def _repeated_key_press(self, key, count=1, modifier=Qt.NoModifier):
         """Send count fake key presses to this scroller's WebEngineTab."""
-        for _ in range(min(count, 5000)):
+        for _ in range(min(count, 1000)):
             self._tab.key_press(key, modifier)
 
     @pyqtSlot(QPointF)
