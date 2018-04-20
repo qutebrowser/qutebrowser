92211caea462c41675093954e89a2191e2ea72ae
Jordyn/The Linux Geek <onlinecloud1@gmail.com>
2018-04-20 00:29:48 -0500

    Ignore blank URLs when applying per-domain stylesheets

--- qutebrowser/browser/browsertab.py
+++ qutebrowser/browser/browsertab.py
@@ -813,6 +813,12 @@ class AbstractTab(QWidget):
         self.url_changed.emit(url)
 
         url = self.url(requested=True)
+
+        # Ignore blank QUrls to avoid crashes.
+        if not url.isValid():
+            log.webview.debug("Not updating per-domain stylesheets due to the QUrl being blank")
+            return
+
         self._update_stylesheet(url)
 
     @pyqtSlot()
