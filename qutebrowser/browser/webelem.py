qutebrowser/qutebrowser

commit 545539f28d86c162f0e7f803480ad7fe24b6870f

Do more sophisticated clicking for hints with QtWebEngine
We now use click() or focus() in JS if possible, or manually follow links in a
href attribute.

While this probably introduces some new corner cases, it fixes a handful of
older ones:

- window.open() in JS can now be handled correctly as we don't need hacks in
  createWindow anymore.
- Focusing input fields with images now works - fixes #1613, #1879
- Hinting now works better on QtWebEngine with Qt 5.8 - fixes #2273

Also see #70.

qutebrowser/browser/webelem.py
@@ -33,7 +33,8 @@ from PyQt5.QtCore import QUrl, Qt, QEvent, QTimer
 from PyQt5.QtGui import QMouseEvent
 
 from qutebrowser.config import config
-from qutebrowser.utils import log, usertypes, utils, qtutils
+from qutebrowser.keyinput import modeman
+from qutebrowser.utils import log, usertypes, utils, qtutils, objreg
 
 
 Group = usertypes.enum('Group', ['all', 'links', 'images', 'url', 'prevnext',
@@ -322,14 +323,8 @@ class AbstractWebElement(collections.abc.MutableMapping):
             raise Error("Element position is out of view!")
         return pos
 
-    def click(self, click_target):
-        """Simulate a click on the element."""
-        # FIXME:qtwebengine do we need this?
-        # self._widget.setFocus()
-
-        # For QtWebKit
-        self._tab.data.override_target = click_target
-
+    def _click_fake_event(self, click_target):
+        """Send a fake click event to the element."""
         pos = self._mouse_pos()
 
         log.webelem.debug("Sending fake click to {!r} at position {} with "
@@ -363,6 +358,70 @@ class AbstractWebElement(collections.abc.MutableMapping):
             if self.is_text_input() and self.is_editable():
                 self._tab.caret.move_to_end_of_document()
         QTimer.singleShot(0, after_click)
+
+    def _click_editable(self):
+        """Fake a click on an editable input field."""
+        raise NotImplementedError
+
+    def _click_js(self, click_target):
+        """Fake a click by using the JS .click() method."""
+        raise NotImplementedError
+
+    def _click_href(self, click_target):
+        """Fake a click on an element with a href by opening the link."""
+        baseurl = self._tab.url()
+        url = self.resolve_url(baseurl)
+        if url is None:
+            self._click_fake_event(click_target)
+            return
+
+        if click_target in [usertypes.ClickTarget.tab,
+                            usertypes.ClickTarget.tab_bg]:
+            background = click_target == usertypes.ClickTarget.tab_bg
+            tabbed_browser = objreg.get('tabbed-browser', scope='window',
+                                        window=self._tab.win_id)
+            tabbed_browser.tabopen(url, background=background)
+        elif click_target == usertypes.ClickTarget.window:
+            from qutebrowser.mainwindow import mainwindow
+            window = mainwindow.MainWindow()
+            window.show()
+            window.tabbed_browser.tabopen(url)
+        else:
+            raise ValueError("Unknown ClickTarget {}".format(click_target))
+
+    def click(self, click_target, *, force_event=False):
+        """Simulate a click on the element.
+
+        Args:
+            click_target: An usertypes.ClickTarget member, what kind of click
+                          to simulate.
+            force_event: Force generating a fake mouse event.
+        """
+        if force_event:
+            self._click_fake_event(click_target)
+            return
+
+        href_tags = ['a', 'area', 'link']
+        if click_target == usertypes.ClickTarget.normal:
+            if self.tag_name() in href_tags:
+                log.webelem.debug("Clicking via JS click()")
+                self._click_js(click_target)
+            elif self.is_editable(strict=True):
+                log.webelem.debug("Clicking via JS focus()")
+                self._click_editable()
+                modeman.enter(self._tab.win_id, usertypes.KeyMode.insert,
+                              'clicking input')
+            else:
+                self._click_fake_event(click_target)
+        elif click_target in [usertypes.ClickTarget.tab,
+                              usertypes.ClickTarget.tab_bg,
+                              usertypes.ClickTarget.window]:
+            if self.tag_name() in href_tags:
+                self._click_href(click_target)
+            else:
+                self._click_fake_event(click_target)
+        else:
+            raise ValueError("Unknown ClickTarget {}".format(click_target))
 
     def hover(self):
         """Simulate a mouse hover over the element."""

qutebrowser/browser/webengine/webengineelem.py
@@ -22,9 +22,10 @@
 
 """QtWebEngine specific part of the web element API."""
 
-from PyQt5.QtCore import QRect
+from PyQt5.QtCore import QRect, Qt, QPoint
+from PyQt5.QtGui import QMouseEvent
 
-from qutebrowser.utils import log, javascript
+from qutebrowser.utils import log, javascript, usertypes
 from qutebrowser.browser import webelem
 
 
@@ -148,4 +149,18 @@ class WebEngineElement(webelem.AbstractWebElement):
     def remove_blank_target(self):
         js_code = javascript.assemble('webelem', 'remove_blank_target',
             self._id)
+        self._tab.run_js_async(js_code)
+
+    def _click_editable(self):
+        # WORKAROUND for https://bugreports.qt.io/browse/QTBUG-58515
+        ev = QMouseEvent(QMouseEvent.MouseButtonPress, QPoint(0, 0),
+                         QPoint(0, 0), QPoint(0, 0), Qt.NoButton, Qt.NoButton,
+                         Qt.NoModifier, Qt.MouseEventSynthesizedBySystem)
+        self._tab.send_event(ev)
+        # This actually "clicks" the element by calling focus() on it in JS.
+        js_code = javascript.assemble('webelem', 'focus', self._id)
+        self._tab.run_js_async(js_code)
+
+    def _click_js(self, _click_target):
+        js_code = javascript.assemble('webelem', 'click', self._id)
         self._tab.run_js_async(js_code)

qutebrowser/browser/webkit/webkitelem.py
@@ -297,6 +297,20 @@ class WebKitElement(webelem.AbstractWebElement):
                 break
             elem = elem._parent()  # pylint: disable=protected-access
 
+    def _click_editable(self):
+        self._elem.evaluateJavaScript('this.focus();')
+
+    def _click_js(self, click_target):
+        if self.get('target') == '_blank':
+            # QtWebKit does nothing in this case for some reason...
+            self._click_fake_event(click_target)
+        else:
+            self._elem.evaluateJavaScript('this.click();')
+
+    def _click_fake_event(self, click_target):
+        self._tab.data.override_target = click_target
+        super()._click_fake_event(click_target)
+
 
 def get_child_frames(startframe):
     """Get all children recursively of a given QWebFrame.

doc/help/commands.asciidoc
@@ -1036,7 +1036,7 @@ Clear the currently entered key chain.
 
 [[click-element]]
 === click-element
-Syntax: +:click-element [*--target* 'target'] 'filter' 'value'+
+Syntax: +:click-element [*--target* 'target'] [*--force-event*] 'filter' 'value'+
 
 Click the element matching the given filter.
 
@@ -1049,6 +1049,7 @@ The given filter needs to result in exactly one element, otherwise, an error is 
 
 ==== optional arguments
 * +*-t*+, +*--target*+: How to open the clicked element (normal/tab/tab-bg/window).
+* +*-f*+, +*--force-event*+: Force generating a fake click event.
 
 [[command-accept]]
 === command-accept

qutebrowser/browser/commands.py
@@ -1545,7 +1545,8 @@ class CommandDispatcher:
     @cmdutils.argument('filter_', choices=['id'])
     def click_element(self, filter_: str, value, *,
                       target: usertypes.ClickTarget=
-                      usertypes.ClickTarget.normal):
+                      usertypes.ClickTarget.normal,
+                      force_event=False):
         """Click the element matching the given filter.
 
         The given filter needs to result in exactly one element, otherwise, an
@@ -1556,6 +1557,7 @@ class CommandDispatcher:
                      id: Get an element based on its ID.
             value: The value to filter for.
             target: How to open the clicked element (normal/tab/tab-bg/window).
+            force_event: Force generating a fake click event.
         """
         tab = self._current_widget()
 
@@ -1565,7 +1567,7 @@ class CommandDispatcher:
                 message.error("No element found with id {}!".format(value))
                 return
             try:
-                elem.click(target)
+                elem.click(target, force_event=force_event)
             except webelem.Error as e:
                 message.error(str(e))
                 return

qutebrowser/browser/mouse.py
@@ -93,7 +93,10 @@ class MouseEventFilter(QObject):
             return True
 
         self._ignore_wheel_event = True
-        self._tab.elements.find_at_pos(e.pos(), self._mousepress_insertmode_cb)
+
+        if e.button() != Qt.NoButton:
+            self._tab.elements.find_at_pos(e.pos(),
+                                           self._mousepress_insertmode_cb)
 
         return False
 

qutebrowser/browser/webengine/webview.py
@@ -73,10 +73,8 @@ class WebEngineView(QWebEngineView):
             The new QWebEngineView object.
         """
         debug_type = debug.qenum_key(QWebEnginePage, wintype)
-        background_tabs = config.get('tabs', 'background-tabs')
 
-        log.webview.debug("createWindow with type {}, background_tabs "
-                          "{}".format(debug_type, background_tabs))
+        log.webview.debug("createWindow with type {}".format(debug_type))
 
         try:
             background_tab_wintype = QWebEnginePage.WebBrowserBackgroundTab
@@ -85,30 +83,22 @@ class WebEngineView(QWebEngineView):
             # this with a newer Qt...
             background_tab_wintype = 0x0003
 
-        if wintype == QWebEnginePage.WebBrowserWindow:
+        click_target = {
             # Shift-Alt-Click
-            target = usertypes.ClickTarget.window
-        elif wintype == QWebEnginePage.WebDialog:
+            QWebEnginePage.WebBrowserWindow: usertypes.ClickTarget.window,
+            # ?
+            QWebEnginePage.WebDialog: usertypes.ClickTarget.tab,
+            # Middle-click / Ctrl-Click with Shift
+            QWebEnginePage.WebBrowserTab: usertypes.ClickTarget.tab_bg,
+            # Middle-click / Ctrl-Click
+            background_tab_wintype: usertypes.ClickTarget.tab,
+        }
+
+        if wintype == QWebEnginePage.WebDialog:
             log.webview.warning("{} requested, but we don't support "
                                 "that!".format(debug_type))
-            target = usertypes.ClickTarget.tab
-        elif wintype == QWebEnginePage.WebBrowserTab:
-            # Middle-click / Ctrl-Click with Shift
-            # FIXME:qtwebengine this also affects target=_blank links...
-            if background_tabs:
-                target = usertypes.ClickTarget.tab
-            else:
-                target = usertypes.ClickTarget.tab_bg
-        elif wintype == background_tab_wintype:
-            # Middle-click / Ctrl-Click
-            if background_tabs:
-                target = usertypes.ClickTarget.tab_bg
-            else:
-                target = usertypes.ClickTarget.tab
-        else:
-            raise ValueError("Invalid wintype {}".format(debug_type))
 
-        tab = shared.get_tab(self._win_id, target)
+        tab = shared.get_tab(self._win_id, click_target[wintype])
 
         # WORKAROUND for https://bugreports.qt.io/browse/QTBUG-54419
         vercheck = qtutils.version_check

qutebrowser/javascript/webelem.js
@@ -191,5 +191,15 @@ window._qutebrowser.webelem = (function() {
         }
     };
 
+    funcs.click = function(id) {
+        var elem = elements[id];
+        elem.click();
+    };
+
+    funcs.focus = function(id) {
+        var elem = elements[id];
+        elem.focus();
+    };
+
     return funcs;
 })();

commit 89f4333df15d21f223a61abbf6efa6c25e2fa899

Make sure external schemes are clickable via hints
This issue was probably introduced in 545539f28d86c162f0e7f803480ad7fe24b6870f
- with JavaScript, we can't "click" on an external link.

There might be a better solution using
QWebEngineSettings::setUnknownUrlSchemePolicy(QWebEngineSettings::AllowAllUnknownUrlSchemes)
temporarily when using hints with PyQt 5.11.

Fixes #2833

qutebrowser/browser/webelem.py
@@ -307,6 +307,10 @@ class AbstractWebElement(collections.abc.MutableMapping):
         href_tags = ['a', 'area', 'link']
         return self.tag_name() in href_tags and 'href' in self
 
+    def _requires_user_interaction(self):
+        """Return True if clicking this element needs user interaction."""
+        raise NotImplementedError
+
     def _mouse_pos(self):
         """Get the position to click/hover."""
         # Click the center of the largest square fitting into the top/left
@@ -405,7 +409,7 @@ class AbstractWebElement(collections.abc.MutableMapping):
             return
 
         if click_target == usertypes.ClickTarget.normal:
-            if self.is_link():
+            if self.is_link() and not self._requires_user_interaction():
                 log.webelem.debug("Clicking via JS click()")
                 self._click_js(click_target)
             elif self.is_editable(strict=True):

qutebrowser/browser/webengine/webengineelem.py
@@ -27,7 +27,7 @@ from PyQt5.QtGui import QMouseEvent
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWebEngineWidgets import QWebEngineSettings
 
-from qutebrowser.utils import log, javascript
+from qutebrowser.utils import log, javascript, urlutils
 from qutebrowser.browser import webelem
 
 
@@ -197,6 +197,13 @@ class WebEngineElement(webelem.AbstractWebElement):
     def _move_text_cursor(self):
         if self.is_text_input() and self.is_editable():
             self._js_call('move_cursor_to_end')
+
+    def _requires_user_interaction(self):
+        baseurl = self._tab.url()
+        url = self.resolve_url(baseurl)
+        if url is None:
+            return True
+        return url.scheme() not in urlutils.WEBENGINE_SCHEMES
 
     def _click_editable(self, click_target):
         # WORKAROUND for https://bugreports.qt.io/browse/QTBUG-58515

qutebrowser/browser/webkit/webkitelem.py
@@ -305,6 +305,9 @@ class WebKitElement(webelem.AbstractWebElement):
         if self.is_text_input() and self.is_editable():
             self._tab.caret.move_to_end_of_document()
 
+    def _requires_user_interaction(self):
+        return False
+
     def _click_editable(self, click_target):
         ok = self._elem.evaluateJavaScript('this.focus(); true;')
         if ok:

qutebrowser/utils/urlutils.py
@@ -39,6 +39,21 @@ from qutebrowser.browser.network import pac
 # https://github.com/qutebrowser/qutebrowser/issues/108
 
 
+# URL schemes supported by QtWebEngine
+WEBENGINE_SCHEMES = [
+    'about',
+    'data',
+    'file',
+    'filesystem',
+    'ftp',
+    'http',
+    'https',
+    'javascript',
+    'ws',
+    'wss',
+]
+
+
 class InvalidUrlError(ValueError):
 
     """Error raised if a function got an invalid URL.