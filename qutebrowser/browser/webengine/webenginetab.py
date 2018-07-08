d78dcf376fb449ad077c392806964e498020ec6c
Philippe Mongeau <ph.mongeau@gmail.com>
2018-07-08 19:43:40 -0400

    add TabHistory.from_qt, fix undo when closing unloaded tab, fix tab title when unloading.

--- qutebrowser/browser/webengine/webenginetab.py
+++ qutebrowser/browser/webengine/webenginetab.py
@@ -503,12 +503,11 @@ class WebEngineHistory(browsertab.AbstractHistory):
 
     def current_idx(self):
         if self.to_load:
-            idx = next(
-                (i for i, item in enumerate(self.to_load) if item.active),
-                None)
-            if idx is None:
-                idx = len(self.to_load) - 1
-            return idx
+            for i, item in enumerate(self.to_load):
+                if item.active:
+                    return i
+            return len(self.to_load) - 1
+
         return self._history.currentItemIndex()
 
     def can_go_back(self):
@@ -534,6 +533,10 @@ class WebEngineHistory(browsertab.AbstractHistory):
             scheme = self._tab.url().scheme()
             if scheme in ['view-source', 'chrome']:
                 raise browsertab.WebTabError("Can't serialize special URL!")
+
+        if self.to_load:
+            _stream, data, _cur_data = tabhistory.serialize(self.to_load)
+            return data
         return qtutils.serialize(self._history)
 
     def deserialize(self, data):
@@ -1039,6 +1042,10 @@ class WebEngineTab(browsertab.AbstractTab):
         self._widget.load(url)
 
     def url(self, requested=False):
+        if not self.loaded and self.history.to_load:
+            idx = self.history.current_idx()
+            return self.history.to_load[idx].url
+
         page = self._widget.page()
         if requested:
             return page.requestedUrl()
--- qutebrowser/browser/webkit/webkittab.py
+++ qutebrowser/browser/webkit/webkittab.py
@@ -504,6 +504,12 @@ class WebKitHistory(browsertab.AbstractHistory):
     """QtWebKit implementations related to page history."""
 
     def current_idx(self):
+        if self.to_load:
+            for i, item in enumerate(self.to_load):
+                if item.active:
+                    return i
+            return len(self.to_load) - 1
+
         return self._history.currentItemIndex()
 
     def can_go_back(self):
@@ -520,6 +526,9 @@ class WebKitHistory(browsertab.AbstractHistory):
         self._history.goToItem(item)
 
     def serialize(self):
+        if self.to_load:
+            _stream, data, _cur_data = tabhistory.serialize(self.to_load)
+            return data
         return qtutils.serialize(self._history)
 
     def deserialize(self, data):
@@ -680,6 +689,10 @@ class WebKitTab(browsertab.AbstractTab):
         self._widget.openurl(url)
 
     def url(self, requested=False):
+        if not self.loaded and self.history.to_load:
+            idx = self.history.current_idx()
+            return self.history.to_load[idx].url
+
         frame = self._widget.page().mainFrame()
         if requested:
             return frame.requestedUrl()
