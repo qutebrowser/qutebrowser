9b452074d21bde933ae7ce5aa9abd84875f743aa
bitraid <bitraid@protonmail.ch>
2018-07-25 19:11:22 +0300

    Shell script for NSIS: fix quotes

--- misc/nsis/mkunlist.cmd
+++ misc/nsis/mkunlist.cmd
@@ -17,20 +17,20 @@ if not exist "%DIST%" exit 2
 if exist "%ULIST%" del "%ULIST%" || exit 3
 if exist "%DLIST%" del "%DLIST%" || exit 3
 
-for /r "%DIST%" %%i in (*) do call:AddToNSH %%i Delete "%ULIST%"
+for /r "%DIST%" %%i in (*) do call:AddToNSH "%%i" Delete "%ULIST%"
 
 rem '*' doesn't catch hidden files and there are a couple files starting with
 rem a '.', which will appear as hidden if mapped from a linux file system.
-for /f "tokens=*" %%i in ('dir "%DIST%" /a:h-d /b /s') do call:AddToNSH %%i Delete "%ULIST%"
+for /f "tokens=*" %%i in ('dir "%DIST%" /a:h-d /b /s') do call:AddToNSH "%%i" Delete "%ULIST%"
 
-for /r "%DIST%" %%i in (.) do call:AddToNSH %%i RMDir "%DLIST%"
+for /r "%DIST%" %%i in (.) do call:AddToNSH "%%i" RMDir "%DLIST%"
 
 sort /r "%DLIST%" >> "%ULIST%"
 del "%DLIST%"
 goto:eof
 
 :AddToNSH
-set "FN=%1"
+set "FN=%~1"
 set "FN=!FN:%DIST%=!"
 set "FN=!FN:$=$$!"
 (echo:%2 "$INSTDIR!FN!") >> %3
