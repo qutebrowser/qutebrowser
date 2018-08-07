bb8901f48f50a29d8bb7845879b356674427e805
Florian Bruhin <git@the-compiler.org>
2018-08-07 16:26:41 +0200

    Use exist_ok=True for os.path.makedirs
    See #2570
    
    (cherry picked from commit 1cb547a8de9228be81db912857cc0a61116d8c05)

--- qutebrowser/browser/downloads.py
+++ qutebrowser/browser/downloads.py
@@ -79,10 +79,7 @@ def download_dir():
     else:
         ddir = directory
 
-    try:
-        os.makedirs(ddir)
-    except FileExistsError:
-        pass
+    os.makedirs(ddir, exist_ok=True)
 
     return ddir
 
@@ -692,9 +689,7 @@ class AbstractDownloadItem(QObject):
         global last_used_directory
 
         try:
-            os.makedirs(os.path.dirname(self._filename))
-        except FileExistsError:
-            pass
+            os.makedirs(os.path.dirname(self._filename), exist_ok=True)
         except OSError as e:
             self._die(e.strerror)
 
