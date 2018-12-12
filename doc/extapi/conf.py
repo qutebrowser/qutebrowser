5a1be2ff165bf56d50154cf08263ddda5751548e
Florian Bruhin <me@the-compiler.org>
2018-12-12 10:48:16 +0100

    Order members like in source

--- doc/extapi/conf.py
+++ doc/extapi/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
 ]
+autodoc_member_order = 'bysource'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
