37813191e8a65a35b3478a5b72d4d9886224cd9e
Florian Bruhin <me@the-compiler.org>
2019-03-18 11:22:43 +0100

    Fix double-registering of schemes in test_webenginesettings

--- qutebrowser/browser/webengine/webenginequtescheme.py
+++ qutebrowser/browser/webengine/webenginequtescheme.py
@@ -163,6 +163,7 @@ def init():
     classes.
     """
     if QWebEngineUrlScheme is not None:
+        assert QWebEngineUrlScheme.schemeByName(b'qute') is None
         scheme = QWebEngineUrlScheme(b'qute')
         scheme.setFlags(QWebEngineUrlScheme.LocalScheme |
                         QWebEngineUrlScheme.LocalAccessAllowed)
