eabda4074e41690c2bd95b936e598de16fca5f7d
Florian Bruhin <me@the-compiler.org>
2019-03-28 09:36:43 +0100

    Adjust comment

--- qutebrowser/browser/hints.py
+++ qutebrowser/browser/hints.py
@@ -70,8 +70,10 @@ class HintLabel(QLabel):
         self._context = context
         self.elem = elem
 
+        # Make sure we can style the background via a style sheet, and we don't
+        # get any extra text indent from Qt.
+        # The real stylesheet lives in mainwindow.py for performance reasons..
         self.setAttribute(Qt.WA_StyledBackground, True)
-        # Zero out extra indent provided.
         self.setIndent(0)
 
         self._context.tab.contents_size_changed.connect(self._move_to_elem)
