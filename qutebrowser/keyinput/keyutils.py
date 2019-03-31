2ba0dd690c4d1d12c36c6398ef7a3d2d3d4fa02d
user202729 <25191436+user202729@users.noreply.github.com>
2019-03-31 16:59:53 +0700

    Make keyutils.is_special function do what it says in the docstring
    This was broken in PR #3742.

--- qutebrowser/keyinput/keyutils.py
+++ qutebrowser/keyinput/keyutils.py
@@ -71,8 +71,7 @@ def is_special(key, modifiers):
     _assert_plain_key(key)
     _assert_plain_modifier(modifiers)
     return not (_is_printable(key) and
-                modifiers in [Qt.ShiftModifier, Qt.NoModifier,
-                              Qt.KeypadModifier])
+                modifiers in [Qt.ShiftModifier, Qt.NoModifier])
 
 
 def is_modifier_key(key):
@@ -350,8 +349,7 @@ class KeyInfo:
                 key_string = key_string.lower()
 
         # "special" binding
-        assert (is_special(self.key, self.modifiers) or
-                self.modifiers == Qt.KeypadModifier)
+        assert is_special(self.key, self.modifiers)
         modifier_string = _modifiers_to_string(modifiers)
         return '<{}{}>'.format(modifier_string, key_string)
 
