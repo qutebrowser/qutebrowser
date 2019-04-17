qutebrowser/qutebrowser

commit 66ef69d4f359968910e0f7f88a0d3bf28bb4f047

Make ;; work properly with failing commands
See #4720

qutebrowser/commands/runners.py
@@ -286,12 +286,13 @@ class CommandRunner(QObject):
         self._parser = CommandParser(partial_match=partial_match)
         self._win_id = win_id
 
-    def run(self, text, count=None):
+    def run(self, text, count=None, *, safely=False):
         """Parse a command from a line of text and run it.
 
         Args:
             text: The text to parse.
             count: The count to pass to the command.
+            safely: Show CmdError exceptions as messages.
         """
         record_last_command = True
         record_macro = True
@@ -305,7 +306,14 @@ class CommandRunner(QObject):
                 args = result.args
             else:
                 args = replace_variables(self._win_id, result.args)
-            result.cmd.run(self._win_id, args, count=count)
+
+            try:
+                result.cmd.run(self._win_id, args, count=count)
+            except cmdexc.Error as e:
+                if safely:
+                    message.error(str(e), stack=traceback.format_exc())
+                else:
+                    raise
 
             if result.cmdline[0] == 'repeat-command':
                 record_last_command = False
@@ -325,7 +333,4 @@ class CommandRunner(QObject):
     @pyqtSlot(str)
     def run_safely(self, text, count=None):
         """Run a command and display exceptions in the statusbar."""
-        try:
-            self.run(text, count)
-        except cmdexc.Error as e:
-            message.error(str(e), stack=traceback.format_exc())
+        self.run(text, count, safely=True)

tests/end2end/features/misc.feature
@@ -391,6 +391,12 @@ Feature: Various utility commands.
         And I run :command-accept
         Then the message "Hello World" should be shown
 
+    ## https://github.com/qutebrowser/qutebrowser/issues/4720
+    Scenario: Chaining failing commands
+        When I run :scroll x ;; message-info foo
+        Then the error "Invalid value 'x' for direction - *" should be shown
+        And the message "foo" should be shown
+
     # We can't run :message-i as startup command, so we use
     # :set-cmd-text
 

commit 2d2fbd0349640b37ba3d9a512ebbdcaca2f27b44

Fix error handling when parsing commands
This fixes a crash when doing :foo, introduced in
66ef69d4f359968910e0f7f88a0d3bf28bb4f047

qutebrowser/commands/runners.py
@@ -21,6 +21,8 @@
 
 import traceback
 import re
+import typing
+import contextlib
 
 import attr
 from PyQt5.QtCore import pyqtSlot, QUrl, QObject
@@ -286,6 +288,17 @@ class CommandRunner(QObject):
         self._parser = CommandParser(partial_match=partial_match)
         self._win_id = win_id
 
+    @contextlib.contextmanager
+    def _handle_error(self, safely) -> typing.Iterator[None]:
+        """Show exceptions as errors if safely=True is given."""
+        try:
+            yield
+        except cmdexc.Error as e:
+            if safely:
+                message.error(str(e), stack=traceback.format_exc())
+            else:
+                raise
+
     def run(self, text, count=None, *, safely=False):
         """Parse a command from a line of text and run it.
 
@@ -301,19 +314,21 @@ class CommandRunner(QObject):
                                   window=self._win_id)
         cur_mode = mode_manager.mode
 
-        for result in self._parser.parse_all(text):
+        parsed = None
+        with self._handle_error(safely):
+            parsed = self._parser.parse_all(text)
+
+        if parsed is None:
+            return
+
+        for result in parsed:
             if result.cmd.no_replace_variables:
                 args = result.args
             else:
                 args = replace_variables(self._win_id, result.args)
 
-            try:
+            with self._handle_error(safely):
                 result.cmd.run(self._win_id, args, count=count)
-            except cmdexc.Error as e:
-                if safely:
-                    message.error(str(e), stack=traceback.format_exc())
-                else:
-                    raise
 
             if result.cmdline[0] == 'repeat-command':
                 record_last_command = False