4873238ac0e936fe63a918c648ade66a66e1af6f
Florian Bruhin <me@the-compiler.org>
2019-04-22 13:35:48 +0200

    Remove unused import

--- tests/unit/misc/test_guiprocess.py
+++ tests/unit/misc/test_guiprocess.py
@@ -22,7 +22,7 @@
 import logging
 
 import pytest
-from PyQt5.QtCore import QProcess, QIODevice
+from PyQt5.QtCore import QProcess
 
 from qutebrowser.misc import guiprocess
 from qutebrowser.utils import usertypes
