a1de502b088449321f94489049b3b165c1d04e8a
Jay Kamat <jaygkamat@gmail.com>
2019-06-08 15:48:35 -0700

    Fix crash on redirect on qt5.14

--- qutebrowser/utils/usertypes.py
+++ qutebrowser/utils/usertypes.py
@@ -450,6 +450,7 @@ class NavigationRequest:
         'form_resubmitted',  # QtWebKit only
         'back_forward',
         'reloaded',
+        'redirect',  # QtWebEngine >5.14 only
         'other'
     ])
 
