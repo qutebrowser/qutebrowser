86294dddb7157ceea64318a820437bb80c6527b8
Peter Rice <peter@peterrice.xyz>
2019-08-25 18:04:17 -0400

    replace f-string with str.format

--- qutebrowser/browser/shared.py
+++ qutebrowser/browser/shared.py
@@ -311,7 +311,7 @@ def netrc_authentication(url, authenticator):
     try:
         net = netrc.netrc(config.val.content.netrc_file)
         if url.port():
-            authenticators = net.authenticators(f"{url.host()}:{url.port()}")
+            authenticators = net.authenticators("{}:{}".format(url.host(), url.port()))
         if not authenticators:
             authenticators = net.authenticators(url.host())
         if authenticators is not None:
