e8fab5175782772111ffde79095943599324f4ed
Florian Bruhin <me@the-compiler.org>
2019-09-02 22:07:59 +0200

    Use pathlib to find out test basedir
    This simplifies things a bit and also fixes an issue happening after
    https://github.com/pytest-dev/pytest/pull/5792 - presumably, the casing of the
    drive letter changed somehow.

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,6 +24,7 @@
 import os
 import sys
 import warnings
+import pathlib
 import ctypes
 import ctypes.util
 
@@ -139,11 +140,10 @@ def pytest_collection_modifyitems(config, items):
             item.add_marker('gui')
 
         if hasattr(item, 'module'):
-            module_path = os.path.relpath(
-                item.module.__file__,
-                os.path.commonprefix([__file__, item.module.__file__]))
+            test_basedir = pathlib.Path(__file__).parent
+            module_path = pathlib.Path(item.module.__file__)
+            module_root_dir = module_path.relative_to(test_basedir).parts[0]
 
-            module_root_dir = module_path.split(os.sep)[0]
             assert module_root_dir in ['end2end', 'unit', 'helpers',
                                        'test_conftest.py']
             if module_root_dir == 'end2end':
