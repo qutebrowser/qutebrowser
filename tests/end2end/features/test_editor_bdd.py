083d0af1302f87a0c41f553f89f6736f2a215a92
Florian Bruhin <me@the-compiler.org>
2019-12-03 14:18:15 +0100

    Increase timeout

--- tests/end2end/features/test_editor_bdd.py
+++ tests/end2end/features/test_editor_bdd.py
@@ -143,7 +143,7 @@ def set_up_editor_wait(quteproc, tmpdir, text, editor_pid_watcher):
 @bdd.when("I wait until the editor has started")
 def wait_editor(qtbot, editor_pid_watcher):
     if not editor_pid_watcher.has_pidfile:
-        with qtbot.wait_signal(editor_pid_watcher.appeared):
+        with qtbot.wait_signal(editor_pid_watcher.appeared, timeout=5000):
             pass
 
 
