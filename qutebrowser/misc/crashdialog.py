742789b8ca77f1c4f79dfc5abdcb5a52ff28a327
Florian Bruhin <me@the-compiler.org>
2020-01-25 14:56:20 +0100

    Update stacktrace link

--- qutebrowser/misc/crashdialog.py
+++ qutebrowser/misc/crashdialog.py
@@ -507,7 +507,7 @@ class FatalCrashDialog(_CrashDialog):
         text = ("<b>qutebrowser was restarted after a fatal crash.</b><br/>"
                 "<br/>Note: Crash reports for fatal crashes sometimes don't "
                 "contain the information necessary to fix an issue. Please "
-                "follow the steps in <a href='https://github.com/The-Compiler/"
+                "follow the steps in <a href='https://github.com/qutebrowser/"
                 "qutebrowser/blob/master/doc/stacktrace.asciidoc'>"
                 "stacktrace.asciidoc</a> to submit a stacktrace.<br/>")
         self._lbl.setText(text)
