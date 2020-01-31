ffdb1611f03652cd84b8a2ffbd9bd1a92fc0c7a7
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2020-01-31 12:08:35 +0000

    Fix tree_prefix is empty

--- qutebrowser/misc/sessions.py
+++ qutebrowser/misc/sessions.py
@@ -436,9 +436,14 @@ class SessionManager(QObject):
             raise SessionError(e)
 
     def _load_tree(self, tabbed_browser, tree_data):
-        root_data = tree_data.get(0)   # root node always has uid 0
+        tree_keys = list(tree_data.keys())
+        if not tree_keys:
+            return
+
+        root_data = tree_data.get(tree_keys[0])
         if root_data is None:
             return
+
         root_node = tabbed_browser.widget.tree_root
 
         def recursive_load_node(uid):
