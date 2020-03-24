67996f6b7c1d7dff924d15ae0746ad09ad4d7576
Florian Bruhin <me@the-compiler.org>
2020-03-24 19:19:08 +0100

    Improve WhatsApp quirk
    Probably faster to search for the link rather than a full-text search, and it
    works in any language.
    
    (cherry picked from commit 46fef6bc16de0782e9ba6dc1b6242057364c4264)

--- qutebrowser/javascript/whatsapp_web_quirk.user.js
+++ qutebrowser/javascript/whatsapp_web_quirk.user.js
@@ -5,10 +5,11 @@
 // Quirk for WhatsApp Web, based on:
 // https://github.com/jiahaog/nativefier/issues/719#issuecomment-443809630
 
-if (document.body.innerText.replace(/\n/g, ' ').search(
-        /whatsapp works with.*to use whatsapp.*update/i) !== -1) {
-    navigator.serviceWorker.getRegistration().then(function (r) {
-        r.unregister();
+"use strict";
+
+if (document.querySelector("a[href='https://support.google.com/chrome/answer/95414']")) {
+    navigator.serviceWorker.getRegistration().then((registration) => {
+        registration.unregister();
         document.location.reload();
     });
 }
