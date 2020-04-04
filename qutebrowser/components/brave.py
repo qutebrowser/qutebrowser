27eddf45f527441e39bff6b75173d0945f154a54
Árni Dagur <agudmundsson@fc-md.umd.edu>
2020-04-04 14:21:16 -0400

    Support 'file://' block lists

--- qutebrowser/components/brave.py
+++ qutebrowser/components/brave.py
@@ -40,6 +40,17 @@ logger = logging.getLogger('misc')
 _host_blocker = typing.cast('HostBlocker', None)
 
 
+# TODO: Move this code somewhere so that `adblock.py` can make use of it too.
+class _FakeDownload(downloads.TempDownload):
+
+    """A download stub to use on_download_finished with local files."""
+
+    def __init__(self,  # pylint: disable=super-init-not-called
+                 fileobj: typing.IO[bytes]) -> None:
+        self.fileobj = fileobj
+        self.successful = True
+
+
 class BraveAdBlocker:
 
     """Manage blocked hosts based from /etc/hosts-like files.
@@ -107,10 +118,37 @@ class BraveAdBlocker:
         self._done_count = 0
         logger.info("Downloading adblock filter lists...")
         for url in config.val.content.ad_blocking.lists:
-            download = downloads.download_temp(url)
-            self._in_progress.append(download)
-            download.finished.connect(
-                functools.partial(self._on_download_finished, download))
+            if url.scheme() == "file":
+                # The URL describes a local file on disk if the url scheme is
+                # "file://". We handle those as a special case.
+                filename = url.toLocalFile()
+                if os.path.isdir(filename):
+                    for entry in os.scandir(filename):
+                        if entry.is_file():
+                            self._import_local(entry.path)
+                else:
+                    self._import_local(filename)
+            else:
+                download = downloads.download_temp(url)
+                self._in_progress.append(download)
+                download.finished.connect(
+                    functools.partial(self._on_download_finished, download))
+
+    def _import_local(self, filename: str) -> None:
+        """Adds the contents of a file to the blocklist.
+
+        Args:
+            filename: path to a local file to import.
+        """
+        try:
+            fileobj = open(filename, "rb")
+        except OSError as e:
+            message.error("adblock: Error while reading {}: {}".format(
+                filename, e.strerror))
+            return
+        download = _FakeDownload(fileobj)
+        self._in_progress.append(download)
+        self._on_download_finished(download)
 
     def _on_lists_downloaded(self) -> None:
         """Install block lists after files have been downloaded."""
@@ -141,9 +179,11 @@ class BraveAdBlocker:
             assert download.fileobj is not None
             try:
                 download.fileobj.seek(0)
-                text = io.TextIOWrapper(download.fileobj)
+                text = io.TextIOWrapper(download.fileobj, encoding="utf-8")
                 self._engine.add_filter_list(text.read())
                 text.close()
+            except UnicodeDecodeError:
+                message.info("Block list is not valid utf-8")
             finally:
                 download.fileobj.close()
         if not self._in_progress:
