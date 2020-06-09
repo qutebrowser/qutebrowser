qutebrowser/qutebrowser

commit df4a011d48fa9f12fccf59ebca0954ccdf646d20

Put files into the config dir with :config-write-py and rel. paths

qutebrowser/config/configcommands.py
@@ -269,6 +269,8 @@ class ConfigCommands:
         if filename is None:
             filename = os.path.join(standarddir.config(), 'config.py')
         else:
+            if not os.path.isabs(filename):
+                filename = os.path.join(standarddir.config(), filename)
             filename = os.path.expanduser(filename)
 
         if os.path.exists(filename) and not force:

tests/unit/config/test_configcommands.py
@@ -388,6 +388,12 @@ class TestWritePy:
         lines = confpy.read_text('utf-8').splitlines()
         assert '# Autogenerated config.py' in lines
 
+    def test_relative_path(self, commands, config_tmpdir):
+        confpy = config_tmpdir / 'config2.py'
+        commands.config_write_py('config2.py')
+        lines = confpy.read_text('utf-8').splitlines()
+        assert '# Autogenerated config.py' in lines
+
     def test_existing_file(self, commands, tmpdir):
         confpy = tmpdir / 'config.py'
         confpy.ensure()

commit 5e74b62c67620c99150e37c7f61072ba9afb4ae7

Fix ~ with :config-write-py
df4a011d48fa9f12fccf59ebca0954ccdf646d20 introduced a bug because the
os.path.isabs check was done before os.path.expanduser - and isabs('~/...')
will always be False.

qutebrowser/config/configcommands.py
@@ -460,9 +460,9 @@ class ConfigCommands:
         if filename is None:
             filename = standarddir.config_py()
         else:
+            filename = os.path.expanduser(filename)
             if not os.path.isabs(filename):
                 filename = os.path.join(standarddir.config(), filename)
-            filename = os.path.expanduser(filename)
 
         if os.path.exists(filename) and not force:
             raise cmdutils.CommandError("{} already exists - use --force to "

tests/unit/config/test_configcommands.py
@@ -635,6 +635,19 @@ class TestWritePy:
         lines = confpy.read_text('utf-8').splitlines()
         assert '# Autogenerated config.py' in lines
 
+    @pytest.mark.posix
+    def test_expanduser(self, commands, monkeypatch, tmpdir):
+        """Make sure that using a path with ~/... works correctly."""
+        home = tmpdir / 'home'
+        home.ensure(dir=True)
+        monkeypatch.setenv('HOME', str(home))
+
+        commands.config_write_py('~/config.py')
+
+        confpy = home / 'config.py'
+        lines = confpy.read_text('utf-8').splitlines()
+        assert '# Autogenerated config.py' in lines
+
     def test_existing_file(self, commands, tmpdir):
         confpy = tmpdir / 'config.py'
         confpy.ensure()