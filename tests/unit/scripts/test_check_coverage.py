846adef2113680a383b9395cdaf5a92ad984e308
Florian Bruhin <me@the-compiler.org>
2020-06-10 16:23:43 +0200

    tests: Remove unused import

--- tests/unit/scripts/test_check_coverage.py
+++ tests/unit/scripts/test_check_coverage.py
@@ -18,7 +18,6 @@
 # You should have received a copy of the GNU General Public License
 # along with qutebrowser.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
 import sys
 import pathlib
 
