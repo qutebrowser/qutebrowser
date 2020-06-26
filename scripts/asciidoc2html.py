7aa3304dad130f517ca7880e3b039bf5aa132b76
Florian Bruhin <me@the-compiler.org>
2020-06-26 08:43:42 +0200

    asciidoc2html: Resolve path for sys.path
    For some reason, __file__ was "scripts/dev/../asciidoc2html.py", causing
    .parents[1] to point to "scripts/dev/" instead of "scripts/".
    
    See #5534
    
    (cherry picked from commit eed0373feb1a1750aa834b9c8d3524ddb1833780)

--- scripts/asciidoc2html.py
+++ scripts/asciidoc2html.py
@@ -31,7 +31,7 @@ import argparse
 import io
 import pathlib
 
-sys.path.insert(0, str(pathlib.Path(__file__).parents[1]))
+sys.path.insert(0, str(pathlib.Path(__file__).resolve().parents[1]))
 
 from scripts import utils
 
