9ed36464304e684e636605c96fd9ea56b808d2ff
Florian Bruhin <me@the-compiler.org>
2020-07-30 19:03:24 +0200

    Upgrade to pytest 6.0.1

--- scripts/dev/run_pylint_on_tests.py
+++ scripts/dev/run_pylint_on_tests.py
@@ -60,8 +60,6 @@ def main():
         'compare-to-empty-string',
         # directories without __init__.py...
         'import-error',
-        # WORKAROUND for https://github.com/pytest-dev/pytest/issues/7558
-        'not-callable',
     ]
 
     toxinidir = sys.argv[1]
