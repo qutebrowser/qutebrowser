7e54177647324a0d2ff5dddbe039aeb1b3e97ec9
user202729 <25191436+user202729@users.noreply.github.com>
2020-07-31 11:04:40 +0700

    Watch the edited file's directory for file removal and recreation

--- qutebrowser/misc/editor.py
+++ qutebrowser/misc/editor.py
@@ -27,7 +27,7 @@ from PyQt5.QtCore import (pyqtSignal, pyqtSlot, QObject, QProcess,
 
 from qutebrowser.config import config
 from qutebrowser.utils import message, log
-from qutebrowser.misc import guiprocess
+from qutebrowser.misc import guiprocess, throttle
 from qutebrowser.qt import sip
 
 
@@ -44,6 +44,8 @@ class ExternalEditor(QObject):
         _watcher: A QFileSystemWatcher to watch the edited file for changes.
                   Only set if watch=True.
         _content: The last-saved text of the editor.
+        _fail_on_last_change: Whether qutebrowser failed to read the file
+                              on the last file modification.
 
     Signals:
         file_updated: The text in the edited file was updated.
@@ -60,15 +62,21 @@ class ExternalEditor(QObject):
         self._proc = None
         self._remove_file = None
         self._watcher = QFileSystemWatcher(parent=self) if watch else None
+        self._on_change_delayed = throttle.Delayed(
+            self._on_change, 100, parent=self)
         self._content = None
+        self._fail_on_last_change = False  # type: bool
 
     def _cleanup(self):
         """Clean up temporary files after the editor closed."""
         assert self._remove_file is not None
-        if self._watcher is not None and self._watcher.files():
-            failed = self._watcher.removePaths(self._watcher.files())
-            if failed:
-                log.procs.error("Failed to unwatch paths: {}".format(failed))
+        if self._watcher is not None:
+            paths = self._watcher.files() + self._watcher.directories()
+            if paths:
+                failed = self._watcher.removePaths(paths)
+                if failed:
+                    log.procs.error("Failed to unwatch paths: {}".format(
+                        failed))
 
         if self._filename is None or not self._remove_file:
             # Could not create initial file.
@@ -100,7 +108,7 @@ class ExternalEditor(QObject):
             # on_proc_error.
             return
         # do a final read to make sure we don't miss the last signal
-        self._on_file_changed(self._filename)
+        self._on_change(self._filename)
         self.editing_finished.emit()
         self._cleanup()
 
@@ -152,20 +160,29 @@ class ExternalEditor(QObject):
                 fobj.write(text)
             return fobj.name
 
-    @pyqtSlot(str)
-    def _on_file_changed(self, path):
+    def _on_change(self, _path: str) -> None:
         try:
-            with open(path, 'r', encoding=config.val.editor.encoding) as f:
+            with open(self._filename, 'r',
+                      encoding=config.val.editor.encoding) as f:
                 text = f.read()
         except OSError as e:
             # NOTE: Do not replace this with "raise CommandError" as it's
             # executed async.
-            message.error("Failed to read back edited file: {}".format(e))
+            if not self._fail_on_last_change:
+                # the error message should not be shown repeatedly
+                message.error("Failed to read back edited file: {}".format(e))
+                self._fail_on_last_change = True
             return
         log.procs.debug("Read back: {}".format(text))
+        self._fail_on_last_change = False
         if self._content != text:
             self._content = text
             self.file_updated.emit(text)
+
+        if (self._filename not in self._watcher.files() and
+                not self._watcher.addPath(self._filename)):
+            log.procs.error("Failed to rewatch path: {}"
+                            .format(self._filename))
 
     def edit_file(self, filename):
         """Edit the file with the given filename."""
@@ -195,8 +212,15 @@ class ExternalEditor(QObject):
             if not ok:
                 log.procs.error("Failed to watch path: {}"
                                 .format(self._filename))
+            directory = os.path.dirname(self._filename)
+            if not self._watcher.addPath(directory):
+                log.procs.error("Failed to watch path: {}"
+                                .format(directory))
             self._watcher.fileChanged.connect(  # type: ignore[attr-defined]
-                self._on_file_changed)
+                self._on_change_delayed)
+            self._watcher.directoryChanged.connect(
+                # type: ignore[attr-defined]
+                self._on_change_delayed)
 
         args = [self._sub_placeholder(arg, line, column) for arg in editor[1:]]
         log.procs.debug("Calling \"{}\" with args {}".format(executable, args))
