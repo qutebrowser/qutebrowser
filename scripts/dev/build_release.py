qutebrowser/qutebrowser

commit 76eed0aa9c4f01f8282a9f305b8fde64588d26c4

tox: Simplify pyinstaller envs

scripts/dev/build_release.py
@@ -275,12 +275,12 @@ def build_windows():
 
     utils.print_title("Running pyinstaller 32bit")
     _maybe_remove(out_32)
-    call_tox('pyinstaller32', '-r', python=python_x86)
+    call_tox('pyinstaller-32', '-r', python=python_x86)
     shutil.move(out_pyinstaller, out_32)
 
     utils.print_title("Running pyinstaller 64bit")
     _maybe_remove(out_64)
-    call_tox('pyinstaller', '-r', python=python_x64)
+    call_tox('pyinstaller-64', '-r', python=python_x64)
     shutil.move(out_pyinstaller, out_64)
 
     utils.print_title("Running 32bit smoke test")

tox.ini
@@ -7,6 +7,7 @@
 envlist = py38-pyqt515-cov,mypy,misc,vulture,flake8,pylint,pyroma,check-manifest,eslint,yamllint
 distshare = {toxworkdir}
 skipsdist = true
+minversion = 3.15
 
 [testenv]
 setenv =
@@ -158,7 +159,7 @@ commands =
     {envpython} scripts/dev/check_doc_changes.py {posargs}
     {envpython} scripts/asciidoc2html.py {posargs}
 
-[testenv:pyinstaller]
+[testenv:pyinstaller-{64,32}]
 basepython = {env:PYTHON:python3}
 pip_version = pip
 passenv = APPDATA HOME PYINSTALLER_DEBUG
@@ -168,17 +169,6 @@ deps =
     -r{toxinidir}/misc/requirements/requirements-pyqt.txt
 commands =
     {envbindir}/pyinstaller --noconfirm misc/qutebrowser.spec
-
-[testenv:pyinstaller32]
-# A copy of the pyinstaller environment above, to be used for 32-bit on Windows
-# to make sure the both installations are separated.
-# This doesn't actually do anything 32-bit specific, that part happens by
-# setting the PYTHON environment variable accordingly.
-basepython = {[testenv:pyinstaller]basepython}
-pip_version = {[testenv:pyinstaller]pip_version}
-passenv = {[testenv:pyinstaller]passenv}
-deps = {[testenv:pyinstaller]deps}
-commands = {[testenv:pyinstaller]commands}
 
 [testenv:eslint]
 basepython = python3

commit 5663bd24f5ee37ac0ac974ada737eb1409921e97

Fix tox environment name for macOS
This was a regression introduced in
76eed0aa9c4f01f8282a9f305b8fde64588d26c4.

scripts/dev/build_release.py
@@ -201,7 +201,7 @@ def build_mac():
     utils.print_title("Updating 3rdparty content")
     update_3rdparty.run(ace=False, pdfjs=True, fancy_dmg=False)
     utils.print_title("Building .app via pyinstaller")
-    call_tox('pyinstaller', '-r')
+    call_tox('pyinstaller-64', '-r')
     utils.print_title("Patching .app")
     patch_mac_app()
     utils.print_title("Building .dmg")