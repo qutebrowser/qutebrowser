c79eb71e8edbdf5319cbafef95904ff792ab67b9
Florian Bruhin <me@the-compiler.org>
2020-10-26 10:16:52 +0100

    scripts: Add changelog URL for filelock

--- scripts/dev/recompile_requirements.py
+++ scripts/dev/recompile_requirements.py
@@ -132,6 +132,7 @@ CHANGELOG_URLS = {
     'beautifulsoup4': 'https://bazaar.launchpad.net/~leonardr/beautifulsoup/bs4/view/head:/CHANGELOG',
     'check-manifest': 'https://github.com/mgedmin/check-manifest/blob/master/CHANGES.rst',
     'yamllint': 'https://github.com/adrienverge/yamllint/blob/master/CHANGELOG.rst',
+    'filelock': 'https://github.com/benediktschmitt/py-filelock/commits/master',
 }
 
 # PyQt versions which need SIP v4
