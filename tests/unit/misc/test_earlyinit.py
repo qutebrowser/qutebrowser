f083e3801b3aa4cefd70e1a61c034b3fc7684271
Florian Bruhin <me@the-compiler.org>
2020-11-20 11:25:56 +0100

    Bring back earlyinit.qt_version() tests
    Those were deleted with no apparent reason (by accident?) in
    3772dc5930e55416ac1eef0ec7a1f612bdf7d617.

--- tests/unit/misc/test_earlyinit.py
+++ tests/unit/misc/test_earlyinit.py
@@ -31,3 +31,20 @@ def test_init_faulthandler_stderr_none(monkeypatch, attr):
     """Make sure init_faulthandler works when sys.stderr/__stderr__ is None."""
     monkeypatch.setattr(sys, attr, None)
     earlyinit.init_faulthandler()
+
+
+@pytest.mark.parametrize('same', [True, False])
+def test_qt_version(same):
+    if same:
+        qt_version_str = '5.14.0'
+        expected = '5.14.0'
+    else:
+        qt_version_str = '5.13.0'
+        expected = '5.14.0 (compiled 5.13.0)'
+    actual = earlyinit.qt_version(qversion='5.14.0', qt_version_str=qt_version_str)
+    assert actual == expected
+
+
+def test_qt_version_no_args():
+    """Make sure qt_version without arguments at least works."""
+    earlyinit.qt_version()
