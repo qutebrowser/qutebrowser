9fbefd70775b6e4158143afcba4d3011e16e75a6
Florian Bruhin <me@the-compiler.org>
2020-12-03 10:50:35 +0100

    scripts: Handle comments in recompile_requirements

--- scripts/dev/recompile_requirements.py
+++ scripts/dev/recompile_requirements.py
@@ -357,6 +357,7 @@ def _get_changed_files():
 def parse_versioned_line(line):
     """Parse a requirements.txt line into name/version."""
     if '==' in line:
+        line = line.rsplit('#', maxsplit=1)[0]  # Strip comments
         name, version = line.split('==')
         if ';' in version:  # pip environment markers
             version = version.split(';')[0].strip()
