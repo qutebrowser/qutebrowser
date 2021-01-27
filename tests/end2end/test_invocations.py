7b95a6cdb81107418c8a5bca1e393586e4b5cfd9
Florian Bruhin <me@the-compiler.org>
2021-01-27 17:05:41 +0100

    Skip broken test on Windows
    Proper fix in progress...

--- tests/end2end/test_invocations.py
+++ tests/end2end/test_invocations.py
@@ -445,6 +445,7 @@ def test_preferred_colorscheme(request, quteproc_new):
     'Qt version changed',
     None,
 ])
+@pytest.mark.skipif(utils.is_windows, reason='Currently broken')
 def test_service_worker_workaround(
         request, server, quteproc_new, short_tmpdir, reason):
     """Make sure we remove the QtWebEngine Service Worker directory if configured."""
