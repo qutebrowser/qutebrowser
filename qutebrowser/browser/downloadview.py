qutebrowser/qutebrowser

commit 68b81511e5a0228cd37b2907065fd6d96aa7876f

Add 'Copy URL' to download context menu
See #1078

qutebrowser/browser/downloadview.py
@@ -140,6 +140,8 @@ class DownloadView(QListView):
             actions.append(("Remove", item.remove))
         else:
             actions.append(("Cancel", item.cancel))
+        actions.append(("Copy URL", functools.partial(
+            utils.set_clipboard, item.url().toDisplayString())))
         if model.can_clear():
             actions.append((None, None))
             actions.append(("Remove all finished", model.download_clear))

commit d832ce5f8de04ba94eb11738619a384c796ad300

Fix crash when right-clicking download view
This is a regression introduced in
68b81511e5a0228cd37b2907065fd6d96aa7876f.

See #1078
Fixes #6087

qutebrowser/browser/downloadview.py
@@ -140,8 +140,10 @@ class DownloadView(QListView):
             actions.append(("Remove", item.remove))
         else:
             actions.append(("Cancel", item.cancel))
-        actions.append(("Copy URL", functools.partial(
-            utils.set_clipboard, item.url().toDisplayString())))
+
+        if item is not None:
+            actions.append(("Copy URL", functools.partial(
+                utils.set_clipboard, item.url().toDisplayString())))
         if model.can_clear():
             actions.append((None, None))
             actions.append(("Remove all finished", model.download_clear))