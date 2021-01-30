88040e539c5e9631f9cde9301d9b31f808a97356
Florian Bruhin <me@the-compiler.org>
2021-01-30 15:56:44 +0100

    Also preload JavaScript quirks files
    I thought about implementing recursive globbing for preloading, but that
    would also potentially preload things we don't necessarily need
    preloaded, such as html/doc/*.html.
    
    Fixes #6098
    
    (cherry picked from commit 62c71975a2a1d270d77c283fb6d912be50cb929b)

--- qutebrowser/utils/utils.py
+++ qutebrowser/utils/utils.py
@@ -237,7 +237,11 @@ def _glob_resources(
 def preload_resources() -> None:
     """Load resource files into the cache."""
     resource_path = _resource_path('')
-    for subdir, ext in [('html', '.html'), ('javascript', '.js')]:
+    for subdir, ext in [
+            ('html', '.html'),
+            ('javascript', '.js'),
+            ('javascript/quirks', '.js'),
+    ]:
         for name in _glob_resources(resource_path, subdir, ext):
             _resource_cache[name] = read_file(name)
 
