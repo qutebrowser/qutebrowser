d92fa2c66e7a7eb65b3e12d723b47f4e97a1462a
Andrew MacFie <amacfie@sent.com>
2021-02-11 11:48:05 -0500

    Fix suffixless files and aborting

--- qutebrowser/browser/webengine/webenginedownloads.py
+++ qutebrowser/browser/webengine/webenginedownloads.py
@@ -137,8 +137,8 @@ class DownloadItem(downloads.AbstractDownloadItem):
     def _ask_confirm_question(self, title, msg, *, custom_yes_action=None,
                               custom_no_action=None):
         yes_action = custom_yes_action or self._after_set_filename
-        no_action = custom_no_action or functools.partial(
-            self.cancel, remove_data=False)
+        cancel_action = functools.partial(self.cancel, remove_data=False)
+        no_action = custom_no_action or cancel_action
         question = usertypes.Question()
         question.title = title
         question.text = msg
@@ -146,7 +146,7 @@ class DownloadItem(downloads.AbstractDownloadItem):
         question.mode = usertypes.PromptMode.yesno
         question.answered_yes.connect(yes_action)
         question.answered_no.connect(no_action)
-        question.cancelled.connect(no_action)
+        question.cancelled.connect(cancel_action)
         self.cancelled.connect(question.abort)
         self.error.connect(question.abort)
         message.global_bridge.ask(question, blocking=True)
