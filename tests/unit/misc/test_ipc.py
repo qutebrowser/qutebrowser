0d96cd69f659466842a11f4303638560e1099117
Lembrun <amadeusk7@free.fr>
2021-02-28 23:12:05 +0100

    Replaced os.stat method by the pathlib equivalent #3

--- tests/unit/misc/test_ipc.py
+++ tests/unit/misc/test_ipc.py
@@ -322,7 +322,7 @@ class TestListen:
         print('sockdir: {} / owner {} / mode {:o}'.format(
             sockdir, dir_stat.st_uid, dir_stat.st_mode))
         print('sockfile: {} / owner {} / mode {:o}'.format(
-            sockfile, file_stat.st_uid, file_stat.st_mode))
+            sockfile_path, file_stat.st_uid, file_stat.st_mode))
 
         assert file_owner_ok or dir_owner_ok
         assert file_mode_ok or dir_mode_ok
