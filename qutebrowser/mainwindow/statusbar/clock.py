96b8445678b131970b3db47b66eef1e033542137
Nicholas Schwab <git@nicholas-schwab.de>
2021-04-09 20:08:32 +0200

    Added widget to display a clock in the statusbar.

---
+++ qutebrowser/mainwindow/statusbar/clock.py
@@ -0,0 +1,45 @@
+# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+
+# Copyright 2014-2021 Florian Bruhin (The Compiler) <mail@qutebrowser.org>
+#
+# This file is part of qutebrowser.
+#
+# qutebrowser is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# qutebrowser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with qutebrowser.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Clock displayed in the statusbar."""
+from datetime import datetime
+
+from PyQt5.QtCore import Qt, QTimer
+
+from qutebrowser.mainwindow.statusbar import textbase
+
+
+class Clock(textbase.TextBase):
+
+    """Shows current time and date in the statusbar."""
+
+    def __init__(self, parent=None):
+        super().__init__(parent, elidemode=Qt.ElideNone)
+        self.format = None
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self._show_time)
+
+    def _show_time(self):
+        self.setText(datetime.now().strftime(self.format))
+
+    def show(self) -> None:
+        self.timer.start(100)
+        self._show_time()
+        super().show()
