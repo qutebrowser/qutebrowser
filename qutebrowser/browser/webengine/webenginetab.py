2e4ca779c68a65034fcd4448fa8c0952ed3f0a1d
Florian Bruhin <me@the-compiler.org>
2021-04-23 11:18:46 +0200

    Set print resolution to 300dpi
    See https://codereview.qt-project.org/c/qt/qtwebengine/+/344042

--- qutebrowser/browser/webengine/webenginetab.py
+++ qutebrowser/browser/webengine/webenginetab.py
@@ -91,6 +91,8 @@ class WebEnginePrinting(browsertab.AbstractPrinting):
     def to_printer(self, printer, callback=None):
         if callback is None:
             callback = lambda _ok: None
+
+        printer.setResolution(300)
         self._widget.page().print(printer, callback)
 
 
