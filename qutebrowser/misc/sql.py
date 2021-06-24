16b39d79be51a044e40c5aea3c7ead4206662d16
Javier Ayres <javier@lufte.net>
2021-06-24 09:22:34 -0300

    Update module's stringdoc
    Co-authored-by: Florian Bruhin <me@the-compiler.org>

--- qutebrowser/misc/sql.py
+++ qutebrowser/misc/sql.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with qutebrowser.  If not, see <https://www.gnu.org/licenses/>.
 
-"""Provides access to an in-memory sqlite database."""  # I guess this is an old comment
+"""Provides access to sqlite databases."""
 
 import collections
 import dataclasses
