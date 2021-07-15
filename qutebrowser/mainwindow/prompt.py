59354d350adf81a66a07794e7e004ede7bf10070
Jason Rosenzweig <mail4jasonr@gmail.com>
2021-07-15 22:49:37 +0800

    Further fixed errors in linters

--- qutebrowser/mainwindow/prompt.py
+++ qutebrowser/mainwindow/prompt.py
@@ -637,9 +637,11 @@ class FilenamePrompt(_BasePrompt):
         """Get rid of non-matching directories."""
         num_rows = self._file_model.rowCount(self._root_index)
         if self._to_complete and self._to_complete[0] == '.':
-            self._file_model.setFilter(self._file_model.filter() | QDir.Hidden) # type: ignore
+            self._file_model.setFilter(
+                self._file_model.filter() | QDir.Hidden)    # type: ignore[arg-type, operator]
         else:
-            self._file_model.setFilter(self._file_model.filter() & ~QDir.Hidden) # type: ignore
+            self._file_model.setFilter(
+                self._file_model.filter() & ~QDir.Hidden)   # type: ignore[arg-type, operator]
         for row in range(num_rows):
             index = self._file_model.index(row, 0, self._root_index)
             filename = index.data()
