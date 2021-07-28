55bfe82b0f474fe9720e23eb2e97e50c6eb353b9
Florian Bruhin <me@the-compiler.org>
2021-07-28 14:33:55 +0200

    Release v2.3.1

--- qutebrowser/__init__.py
+++ qutebrowser/__init__.py
@@ -26,7 +26,7 @@ __copyright__ = "Copyright 2014-2021 Florian Bruhin (The Compiler)"
 __license__ = "GPL"
 __maintainer__ = __author__
 __email__ = "mail@qutebrowser.org"
-__version__ = "2.3.0"
+__version__ = "2.3.1"
 __version_info__ = tuple(int(part) for part in __version__.split('.'))
 __description__ = "A keyboard-driven, vim-like browser based on PyQt5."
 
