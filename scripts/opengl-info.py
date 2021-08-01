3d41b3993084b2a6c8dae0934472a7a06c3558a0
Florian Bruhin <me@the-compiler.org>
2021-08-01 13:50:39 +0200

    opengl-info: Fix lint

--- scripts/opengl-info.py
+++ scripts/opengl-info.py
@@ -1,4 +1,27 @@
-from PyQt5.QtGui import QOpenGLContext, QOpenGLVersionProfile, QOffscreenSurface, QGuiApplication
+#!/usr/bin/env python3
+# vim: ft=python fileencoding=utf-8 sts=4 sw=4 et:
+
+# Copyright 2020-2021 Florian Bruhin (The Compiler) <mail@qutebrowser.org>
+
+# This file is part of qutebrowser.
+#
+# qutebrowser is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# qutebrowser is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with qutebrowser.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Show information about the OpenGL setup."""
+
+from PyQt5.QtGui import (QOpenGLContext, QOpenGLVersionProfile,
+                         QOffscreenSurface, QGuiApplication)
 
 app = QGuiApplication([])
 
@@ -12,15 +35,15 @@ assert ok
 ok = ctx.makeCurrent(surface)
 assert ok
 
-print("GLES: {}".format(ctx.isOpenGLES()))
+print(f"GLES: {ctx.isOpenGLES()}")
 
 vp = QOpenGLVersionProfile()
 vp.setVersion(2, 0)
 
 vf = ctx.versionFunctions(vp)
-print("Vendor: {}".format(vf.glGetString(vf.GL_VENDOR)))
-print("Renderer: {}".format(vf.glGetString(vf.GL_RENDERER)))
-print("Version: {}".format(vf.glGetString(vf.GL_VERSION)))
-print("Shading language version: {}".format(vf.glGetString(vf.GL_SHADING_LANGUAGE_VERSION)))
+print(f"Vendor: {vf.glGetString(vf.GL_VENDOR)}")
+print(f"Renderer: {vf.glGetString(vf.GL_RENDERER)}")
+print(f"Version: {vf.glGetString(vf.GL_VERSION)}")
+print(f"Shading language version: {vf.glGetString(vf.GL_SHADING_LANGUAGE_VERSION)}")
 
 ctx.doneCurrent()
