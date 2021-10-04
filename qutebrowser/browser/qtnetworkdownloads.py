654110c98508e346af170807976d2683173c3bb9
Florian Bruhin <me@the-compiler.org>
2021-10-04 11:52:38 +0200

    Use new signal name for QNetworkReply::error

--- qutebrowser/browser/qtnetworkdownloads.py
+++ qutebrowser/browser/qtnetworkdownloads.py
@@ -151,7 +151,7 @@ class DownloadItem(downloads.AbstractDownloadItem):
         reply.setReadBufferSize(16 * 1024 * 1024)  # 16 MB
         reply.downloadProgress.connect(self.stats.on_download_progress)
         reply.finished.connect(self._on_reply_finished)
-        reply.error.connect(self._on_reply_error)
+        reply.errorOccurred.connect(self._on_reply_error)
         reply.readyRead.connect(self._on_ready_read)
         reply.metaDataChanged.connect(self._on_meta_data_changed)
         self._retry_info = _RetryInfo(request=reply.request(),
