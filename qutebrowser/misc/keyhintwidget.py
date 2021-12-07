f1f1eec3faf8a331f2c8a099894006c32c3dd40d
Jimmy <jimmy@spalge.com>
2021-12-08 09:00:18 +1300

    Fix keyhint widget sequence match enum comparison
    Change it from a truthy check to a != Nomatch check.
    
    Also to to improve formatting but the line is probably still too long.

--- qutebrowser/misc/keyhintwidget.py
+++ qutebrowser/misc/keyhintwidget.py
@@ -30,6 +30,7 @@ import re
 
 from PyQt6.QtWidgets import QLabel, QSizePolicy
 from PyQt6.QtCore import pyqtSlot, pyqtSignal, Qt
+from PyQt6.QtGui import QKeySequence
 
 from qutebrowser.config import config, stylesheet
 from qutebrowser.utils import utils, usertypes
@@ -109,10 +110,13 @@ class KeyHintView(QLabel):
             return cmd and cmd.takes_count()
 
         bindings_dict = config.key_instance.get_bindings_for(mode.name)
-        bindings = [(k, v) for (k, v) in sorted(bindings_dict.items())
-                    if keyutils.KeySequence.parse(prefix).matches(k) and
-                    not blacklisted(str(k)) and
-                    (takes_count(v) or not countstr)]
+        bindings = [
+            (k, v)
+            for (k, v) in sorted(bindings_dict.items())
+            if keyutils.KeySequence.parse(prefix).matches(k) != QKeySequence.SequenceMatch.NoMatch
+            and not blacklisted(str(k))
+            and (takes_count(v) or not countstr)
+        ]
 
         if not bindings:
             self._show_timer.stop()
