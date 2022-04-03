cd4429db25746acbec3ef434935959d0f5f66224
Jimmy <jimmy@spalge.com>
2022-04-04 11:00:51 +1200

    Remove 3.6 pins from requirements files.
    Then regenerate the relevant files.
    Also drop dataclasses from requirements files.
    TODO: should we drop the dataclasses-types requirement for mypy too?

--- qutebrowser/misc/earlyinit.py
+++ qutebrowser/misc/earlyinit.py
@@ -235,7 +235,6 @@ def check_libraries():
     modules = {
         'jinja2': _missing_str("jinja2"),
         'yaml': _missing_str("PyYAML"),
-        'dataclasses': _missing_str("dataclasses"),
         'PyQt5.QtQml': _missing_str("PyQt5.QtQml"),
         'PyQt5.QtSql': _missing_str("PyQt5.QtSql"),
         'PyQt5.QtOpenGL': _missing_str("PyQt5.QtOpenGL"),
