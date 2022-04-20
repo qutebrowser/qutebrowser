9d87bf3329078f8b082f1da9f32efe8528687740
Florian Bruhin <me@the-compiler.org>
2022-04-20 15:41:39 +0200

    qt 6: Force debug level for webenginecontext message

--- qutebrowser/utils/log.py
+++ qutebrowser/utils/log.py
@@ -483,6 +483,11 @@ def qt_message_handler(msg_type: QtCore.QtMsgType,
 
     if any(msg.strip().startswith(pattern) for pattern in suppressed_msgs):
         level = logging.DEBUG
+    elif context.category == "qt.webenginecontext" and (
+        msg.strip().startswith("GL Type: ") or  # Qt 6.3
+        msg.strip().startswith("GLImplementation:")  # Qt 6.2
+    ):
+        level = logging.DEBUG
     else:
         level = qt_to_logging[msg_type]
 
