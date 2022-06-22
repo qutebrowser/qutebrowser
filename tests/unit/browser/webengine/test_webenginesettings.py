qutebrowser/qutebrowser

commit 4a6df3a8e84780e9f58dbda31c3a9bfa1e35cebe

Add setting to allow pasting from clipboard
Closes #5256
Supersedes and closes #6315

qutebrowser/browser/webengine/webenginesettings.py
@@ -117,8 +117,6 @@ class WebEngineSettings(websettings.AbstractSettings):
             Attr(QWebEngineSettings.JavascriptEnabled),
         'content.javascript.can_open_tabs_automatically':
             Attr(QWebEngineSettings.JavascriptCanOpenWindows),
-        'content.javascript.can_access_clipboard':
-            Attr(QWebEngineSettings.JavascriptCanAccessClipboard),
         'content.plugins':
             Attr(QWebEngineSettings.PluginsEnabled),
         'content.hyperlink_auditing':
@@ -199,6 +197,21 @@ class WebEngineSettings(websettings.AbstractSettings):
         QWebEngineSettings.FantasyFont: QFont.Fantasy,
     }
 
+    _JS_CLIPBOARD_SETTINGS = {
+        'none': {
+            QWebEngineSettings.WebAttribute.JavascriptCanAccessClipboard: False,
+            QWebEngineSettings.WebAttribute.JavascriptCanPaste: False,
+        },
+        'access': {
+            QWebEngineSettings.WebAttribute.JavascriptCanAccessClipboard: True,
+            QWebEngineSettings.WebAttribute.JavascriptCanPaste: False,
+        },
+        'access-paste': {
+            QWebEngineSettings.WebAttribute.JavascriptCanAccessClipboard: True,
+            QWebEngineSettings.WebAttribute.JavascriptCanPaste: True,
+        },
+    }
+
     def set_unknown_url_scheme_policy(
             self, policy: Union[str, usertypes.Unset]) -> bool:
         """Set the UnknownUrlSchemePolicy to use.
@@ -215,9 +228,22 @@ class WebEngineSettings(websettings.AbstractSettings):
             self._settings.setUnknownUrlSchemePolicy(new_value)
         return old_value != new_value
 
+    def _set_js_clipboard(self, value: Union[str, usertypes.Unset]) -> None:
+        attr_access = QWebEngineSettings.WebAttribute.JavascriptCanAccessClipboard
+        attr_paste = QWebEngineSettings.WebAttribute.JavascriptCanPaste
+
+        if isinstance(value, usertypes.Unset):
+            self._settings.resetAttribute(attr_access)
+            self._settings.resetAttribute(attr_paste)
+        else:
+            for attr, attr_val in self._JS_CLIPBOARD_SETTINGS[value].items():
+                self._settings.setAttribute(attr, attr_val)
+
     def _update_setting(self, setting, value):
         if setting == 'content.unknown_url_scheme_policy':
             return self.set_unknown_url_scheme_policy(value)
+        elif setting == 'content.javascript.clipboard':
+            return self._set_js_clipboard(value)
         return super()._update_setting(setting, value)
 
     def init_settings(self):

doc/faq.asciidoc
@@ -354,9 +354,9 @@ There is a total of four possible approaches to get dark websites:
   of the Dark Reader extension. This is mostly untested, though.
 
 How do I make copy to clipboard buttons work?::
-You can `:set content.javascript.can_access_clipboard true`, or
-`:set -u some.domain content.javascript.can_access_clipboard true` if you want to limit
-the setting to `some.domain`.
+You can `:set content.javascript.clipboard access` to allow this globally (not
+recommended!), or `:set -u some.domain content.javascript.clipboad access` if
+you want to limit the setting to `some.domain`.
 
 
 == Troubleshooting

doc/help/settings.asciidoc
@@ -167,9 +167,9 @@
 |<<content.hyperlink_auditing,content.hyperlink_auditing>>|Enable hyperlink auditing (`<a ping>`).
 |<<content.images,content.images>>|Load images automatically in web pages.
 |<<content.javascript.alert,content.javascript.alert>>|Show javascript alerts.
-|<<content.javascript.can_access_clipboard,content.javascript.can_access_clipboard>>|Allow JavaScript to read from or write to the clipboard.
 |<<content.javascript.can_close_tabs,content.javascript.can_close_tabs>>|Allow JavaScript to close tabs.
 |<<content.javascript.can_open_tabs_automatically,content.javascript.can_open_tabs_automatically>>|Allow JavaScript to open new tabs without user interaction.
+|<<content.javascript.clipboard,content.javascript.clipboard>>|Allow JavaScript to read from or write to the clipboard.
 |<<content.javascript.enabled,content.javascript.enabled>>|Enable JavaScript.
 |<<content.javascript.log,content.javascript.log>>|Log levels to use for JavaScript console logging messages.
 |<<content.javascript.modal_dialog,content.javascript.modal_dialog>>|Use the standard JavaScript modal dialog for `alert()` and `confirm()`.
@@ -2333,17 +2333,6 @@ Type: <<types,Bool>>
 
 Default: +pass:[true]+
 
-[[content.javascript.can_access_clipboard]]
-=== content.javascript.can_access_clipboard
-Allow JavaScript to read from or write to the clipboard.
-With QtWebEngine, writing the clipboard as response to a user interaction is always allowed.
-
-This setting supports link:configuring{outfilesuffix}#patterns[URL patterns].
-
-Type: <<types,Bool>>
-
-Default: +pass:[false]+
-
 [[content.javascript.can_close_tabs]]
 === content.javascript.can_close_tabs
 Allow JavaScript to close tabs.
@@ -2365,6 +2354,23 @@ This setting supports link:configuring{outfilesuffix}#patterns[URL patterns].
 Type: <<types,Bool>>
 
 Default: +pass:[false]+
+
+[[content.javascript.clipboard]]
+=== content.javascript.clipboard
+Allow JavaScript to read from or write to the clipboard.
+With QtWebEngine, writing the clipboard as response to a user interaction is always allowed.
+
+This setting supports link:configuring{outfilesuffix}#patterns[URL patterns].
+
+Type: <<types,String>>
+
+Valid values:
+
+ * +none+: Disable access to clipboard.
+ * +access+: Allow reading from and writing to the clipboard.
+ * +access-paste+: Allow accessing the clipboard and pasting clipboard content.
+
+Default: +pass:[none]+
 
 [[content.javascript.enabled]]
 === content.javascript.enabled

qutebrowser/browser/webkit/webkitsettings.py
@@ -57,8 +57,9 @@ class WebKitSettings(websettings.AbstractSettings):
             Attr(QWebSettings.JavascriptCanOpenWindows),
         'content.javascript.can_close_tabs':
             Attr(QWebSettings.JavascriptCanCloseWindows),
-        'content.javascript.can_access_clipboard':
-            Attr(QWebSettings.JavascriptCanAccessClipboard),
+        'content.javascript.clipboard':
+            Attr(QWebSettings.JavascriptCanAccessClipboard,
+                 converter=lambda val: val != "none"),
         'content.plugins':
             Attr(QWebSettings.PluginsEnabled),
         'content.webgl':

qutebrowser/config/configdata.yml
@@ -869,9 +869,14 @@ content.javascript.alert:
   type: Bool
   desc: Show javascript alerts.
 
-content.javascript.can_access_clipboard:
-  default: false
-  type: Bool
+content.javascript.clipboard:
+  default: none
+  type:
+    name: String
+    valid_values:
+      - none: Disable access to clipboard.
+      - access: Allow reading from and writing to the clipboard.
+      - access-paste: Allow accessing the clipboard and pasting clipboard content.
   supports_pattern: true
   desc: >-
     Allow JavaScript to read from or write to the clipboard.

qutebrowser/config/configfiles.py
@@ -422,6 +422,12 @@ class YamlMigrations(QObject):
             false_value='load-insecurely',
             ask_value='ask',
         )
+        self._migrate_renamed_bool(
+            old_name='content.javascript.can_access_clipboard',
+            new_name='content.javascript.clipboard',
+            true_value='access',
+            false_value='none',
+        )
 
         for setting in ['colors.webpage.force_dark_color_scheme',
                         'colors.webpage.prefers_color_scheme_dark']:

commit dd68a84bc94da0492e3e49eaea629e604fe8ebee

Correctly set initial content.{javascript.clipboard,default_encoding}
Those are handled separately from the other settings, but were never initialized
properly in init_settings().

For content.javascript.clipboard, this is a recent regression introduced in
4a6df3a8e84780e9f58dbda31c3a9bfa1e35cebe. For content.default_encoding, this has
been around since 2018: 3956f81e730463adcba05d92d0043155609aa422.

Fixes #7281

qutebrowser/browser/webengine/webenginesettings.py
@@ -237,11 +237,13 @@ class WebEngineSettings(websettings.AbstractSettings):
             self.set_unknown_url_scheme_policy(value)
         elif setting == 'content.javascript.clipboard':
             self._set_js_clipboard(value)
+        # NOTE: When adding something here, also add it to init_settings()!
         super()._update_setting(setting, value)
 
     def init_settings(self):
         super().init_settings()
         self.update_setting('content.unknown_url_scheme_policy')
+        self.update_setting('content.javascript.clipboard')
 
 
 class ProfileSetter:

qutebrowser/config/websettings.py
@@ -167,7 +167,8 @@ class AbstractSettings:
     def _update_setting(self, setting: str, value: Any) -> None:
         """Update the given setting/value.
 
-        Unknown settings are ignored."""
+        Unknown settings are ignored.
+        """
         if setting in self._ATTRIBUTES:
             self.set_attribute(setting, value)
         elif setting in self._FONT_SIZES:
@@ -176,6 +177,7 @@ class AbstractSettings:
             self.set_font_family(setting, value)
         elif setting == 'content.default_encoding':
             self.set_default_text_encoding(value)
+        # NOTE: When adding something here, also add it to init_settings()!
 
     def update_setting(self, setting: str) -> None:
         """Update the given setting."""
@@ -197,6 +199,7 @@ class AbstractSettings:
         for setting in (list(self._ATTRIBUTES) + list(self._FONT_SIZES) +
                         list(self._FONT_FAMILIES)):
             self.update_setting(setting)
+        self.update_setting('content.default_encoding')
 
 
 @debugcachestats.register(name='user agent cache')

tests/unit/browser/webengine/test_webenginesettings.py
@@ -22,15 +22,20 @@ import logging
 import pytest
 
 QtWebEngineWidgets = pytest.importorskip('PyQt5.QtWebEngineWidgets')
+QWebEngineSettings = QtWebEngineWidgets.QWebEngineSettings
 
 from qutebrowser.browser.webengine import webenginesettings
 from qutebrowser.utils import usertypes
 
 
 @pytest.fixture
-def global_settings(monkeypatch, default_profile):
+def settings(default_profile):
     wrapper = webenginesettings._SettingsWrapper()
-    settings = webenginesettings.WebEngineSettings(wrapper)
+    return webenginesettings.WebEngineSettings(wrapper)
+
+
+@pytest.fixture
+def global_settings(monkeypatch, settings):
     settings.init_settings()
     monkeypatch.setattr(webenginesettings, '_global_settings', settings)
 
@@ -55,6 +60,62 @@ def private_profile(monkeypatch):
     profile.setter = webenginesettings.ProfileSetter(profile)
     monkeypatch.setattr(webenginesettings, 'private_profile', profile)
     return profile
+
+
+@pytest.mark.parametrize("setting, value, getter, expected", [
+    # attribute
+    (
+        "content.images", False,
+        lambda settings:
+            settings.testAttribute(QWebEngineSettings.WebAttribute.AutoLoadImages),
+        False,
+    ),
+    # font size
+    (
+        "fonts.web.size.default", 23,
+        lambda settings:
+            settings.fontSize(QWebEngineSettings.FontSize.DefaultFontSize),
+        23,
+    ),
+    # font family
+    (
+        "fonts.web.family.standard", "Comic Sans MS",
+        lambda settings:
+            settings.fontFamily(QWebEngineSettings.FontFamily.StandardFont),
+        "Comic Sans MS",
+    ),
+    # encoding
+    (
+        "content.default_encoding", "utf-16",
+        lambda settings: settings.defaultTextEncoding(),
+        "utf-16",
+    ),
+    # unknown URL scheme policy
+    (
+        "content.unknown_url_scheme_policy", "allow-all",
+        lambda settings: settings.unknownUrlSchemePolicy(),
+        QWebEngineSettings.UnknownUrlSchemePolicy.AllowAllUnknownUrlSchemes,
+    ),
+    # JS clipboard
+    (
+        "content.javascript.clipboard", "access",
+        lambda settings: settings.testAttribute(
+            QWebEngineSettings.WebAttribute.JavascriptCanAccessClipboard),
+        True,
+    ),
+])
+def test_initial_settings(settings, config_stub, default_profile,
+                          setting, value, getter, expected):
+    """Make sure initial setting values are applied correctly."""
+    qt_settings = default_profile.settings()
+    initial = getter(qt_settings)
+    assert initial != expected  # no point in testing for the Qt default
+
+    config_stub.set_obj(setting, value)
+    settings.init_settings()
+
+    actual = getter(qt_settings)
+    assert actual == expected
 
 
 def test_big_cache_size(config_stub, default_profile):