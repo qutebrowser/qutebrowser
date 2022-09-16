816415707274e1a3757a6c5cada6b38de3cc43c3
Florian Bruhin <me@the-compiler.org>
2022-09-16 14:55:50 +0200

    mypy: Make VENDORED_SIP private
    Follow-up to 76ca4a9dd61e7179881044757d83d2bf5928819d.

--- qutebrowser/qt/sip.py
+++ qutebrowser/qt/sip.py
@@ -7,25 +7,25 @@ from qutebrowser.qt import machinery
 
 # While upstream recommends using PyQt6.sip ever since PyQt6 5.11, some distributions
 # still package later versions of PyQt6 with a top-level "sip" rather than "PyQt6.sip".
-VENDORED_SIP=False
+_VENDORED_SIP = False
 
 if machinery.USE_PYSIDE6:
     raise machinery.Unavailable()
 elif machinery.USE_PYQT5:
     try:
         from PyQt5.sip import *
-        VENDORED_SIP=True
+        _VENDORED_SIP = True
     except ImportError:
         pass
 elif machinery.USE_PYQT6:
     try:
         from PyQt6.sip import *
-        VENDORED_SIP=True
+        _VENDORED_SIP = True
     except ImportError:
         pass
 
 else:
     raise machinery.UnknownWrapper()
 
-if not VENDORED_SIP:
+if not _VENDORED_SIP:
     from sip import *  # type: ignore[import]  # pylint: disable=import-error
