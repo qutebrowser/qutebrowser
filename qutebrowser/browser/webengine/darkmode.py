6f9fda9b6ebe6577b0bc9003d0c3e10b997263c2
Florian Bruhin <me@the-compiler.org>
2022-09-16 14:38:31 +0200

    mypy: Make type for _PREFERRED_COLOR_SCHEME_DEFINITIONS more concrete
    Follow-up to b0e3dcef819d9e24361051d2dc85f6f0e8762aa6.

--- qutebrowser/browser/webengine/darkmode.py
+++ qutebrowser/browser/webengine/darkmode.py
@@ -296,7 +296,8 @@ _DEFINITIONS[Variant.qt_63] = _DEFINITIONS[Variant.qt_515_3].copy_add_setting(
 )
 
 
-_PREFERRED_COLOR_SCHEME_DEFINITIONS: Mapping[Variant, Mapping[Any, str]] = {
+_SettingValType = Union[str, usertypes.Unset]
+_PREFERRED_COLOR_SCHEME_DEFINITIONS: Mapping[Variant, Mapping[_SettingValType, str]] = {
     Variant.qt_515_2: {
         # 0: no-preference (not exposed)
         "dark": "1",
