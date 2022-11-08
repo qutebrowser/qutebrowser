1387c999fa81af670f192b22e5186509dc769206
Florian Bruhin <me@the-compiler.org>
2022-11-08 16:17:31 +0100

    Skip problematic QtWebKit tests with OpenSSL 3
    See https://github.com/cherrypy/cheroot/issues/517 and #7467

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,6 +22,7 @@
 import os
 import pathlib
 import sys
+import ssl
 
 import pytest
 import hypothesis
@@ -111,6 +112,10 @@ def _apply_platform_markers(config, item):
          pytest.mark.skipif,
          sys.getfilesystemencoding() == 'ascii',
          "Skipped because of ASCII locale"),
+        ('qtwebkit_openssl3_skip',
+         pytest.mark.skipif,
+         not config.webengine and ssl.OPENSSL_VERSION_INFO[0] == 3,
+         "Failing due to cheroot: https://github.com/cherrypy/cheroot/issues/346"),
     ]
 
     for searched_marker, new_marker_kind, condition, default_reason in markers:
