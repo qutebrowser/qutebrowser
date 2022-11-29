6e89107a9c750bd60c5af472590662c2d675d596
Florian Bruhin <me@the-compiler.org>
2022-11-29 08:23:42 +0100

    Add a quirk for Array.at
    Trivial to polyfill, needed by various pages in the wild,
    and only natively available with Qt 6.2.
    
    Closes #7501
    Also see #7237 and 726d5e614b1f80c339084eba7d1c240bf467fa9b
    
    (cherry picked from commit ce64e5d899666bd469bf07bd0fecbc75d4bfed3a)

--- qutebrowser/browser/webengine/webenginetab.py
+++ qutebrowser/browser/webengine/webenginetab.py
@@ -1220,7 +1220,11 @@ class _WebEngineScripts(QObject):
             _Quirk(
                 'object_fromentries',
                 predicate=versions.webengine < utils.VersionNumber(5, 13),
-            )
+            ),
+            _Quirk(
+                'array_at',
+                predicate=versions.webengine < utils.VersionNumber(6, 2),
+            ),
         ]
 
         for quirk in quirks:
