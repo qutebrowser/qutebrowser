772c39cea29848459d21f563228ca1d360f69d4b
Florian Bruhin <me@the-compiler.org>
2023-06-29 10:30:55 +0200

    scripts: Add a help to docker generate script

--- scripts/dev/ci/docker/generate.py
+++ scripts/dev/ci/docker/generate.py
@@ -21,22 +21,29 @@
 """Generate Dockerfiles for qutebrowser's CI."""
 
 import sys
+import argparse
 
 import jinja2
+
+
+CONFIGS = {
+    'archlinux-webkit': {'webengine': False, 'unstable': False, 'qt6': False},
+    'archlinux-webengine': {'webengine': True, 'unstable': False, 'qt6': False},
+    'archlinux-webengine-qt6': {'webengine': True, 'unstable': False, 'qt6': True},
+    'archlinux-webengine-unstable': {'webengine': True, 'unstable': True, 'qt6': False},
+    'archlinux-webengine-unstable-qt6': {'webengine': True, 'unstable': True, 'qt6': True},
+}
 
 
 def main():
     with open('Dockerfile.j2') as f:
         template = jinja2.Template(f.read(), trim_blocks=True, lstrip_blocks=True)
 
-    image = sys.argv[1]
-    config = {
-        'archlinux-webkit': {'webengine': False, 'unstable': False, 'qt6': False},
-        'archlinux-webengine': {'webengine': True, 'unstable': False, 'qt6': False},
-        'archlinux-webengine-qt6': {'webengine': True, 'unstable': False, 'qt6': True},
-        'archlinux-webengine-unstable': {'webengine': True, 'unstable': True, 'qt6': False},
-        'archlinux-webengine-unstable-qt6': {'webengine': True, 'unstable': True, 'qt6': True},
-    }[image]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", choices=CONFIGS)
+    args = parser.parse_args()
+
+    config = CONFIGS[args.config]
 
     with open('Dockerfile', 'w') as f:
         f.write(template.render(**config))
