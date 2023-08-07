b5f71d2b503f015102d31b19b4e7535c95900168
Florian Bruhin <me@the-compiler.org>
2023-08-07 12:17:26 +0200

    notifications: Don't complain on upgraded server spec version
    If a server gets fixed and now advertises spec 1.2, there is no reason we should
    complain about things.
    
    See https://github.com/phuhl/linux_notification_center/commit/5427acd551ce6dc4c74bdf8090904c6d254b74f1

--- qutebrowser/browser/webengine/notification.py
+++ qutebrowser/browser/webengine/notification.py
@@ -856,12 +856,15 @@ class DBusNotificationAdapter(AbstractNotificationAdapter):
             log.misc.debug(f"Enabling quirks {quirks}")
             self._quirks = quirks
 
-        expected_spec_version = self._quirks.spec_version or self.SPEC_VERSION
-        if spec_version != expected_spec_version:
+        expected_spec_versions = [self.SPEC_VERSION]
+        if self._quirks.spec_version is not None:
+            expected_spec_versions.append(self._quirks.spec_version)
+
+        if spec_version not in expected_spec_versions:
             log.misc.warning(
                 f"Notification server ({name} {ver} by {vendor}) implements "
-                f"spec {spec_version}, but {expected_spec_version} was expected. "
-                f"If {name} is up to date, please report a qutebrowser bug.")
+                f"spec {spec_version}, but {'/'.join(expected_spec_versions)} was "
+                f"expected. If {name} is up to date, please report a qutebrowser bug.")
 
         # https://specifications.freedesktop.org/notification-spec/latest/ar01s08.html
         icon_key_overrides = {
