d08cdb5a02d01f3e72513eb89ee52b73538b2baf
pylbrecht <pylbrecht@mailbox.org>
2024-05-22 20:46:09 +0200

    Implement StatusBarWidget for TabIndex

--- qutebrowser/mainwindow/statusbar/tabindex.py
+++ qutebrowser/mainwindow/statusbar/tabindex.py
@@ -4,14 +4,21 @@
 
 """TabIndex displayed in the statusbar."""
 
+from qutebrowser.mainwindow.statusbar.widget import StatusBarWidget
 from qutebrowser.qt.core import pyqtSlot
 
 from qutebrowser.mainwindow.statusbar import textbase
 
 
-class TabIndex(textbase.TextBase):
+class TabIndex(StatusBarWidget, textbase.TextBase):
 
     """Shows current tab index and number of tabs in the statusbar."""
+
+    def enable(self):
+        self.show()
+
+    def disable(self):
+        self.hide()
 
     @pyqtSlot(int, int)
     def on_tab_index_changed(self, current, count):
