9847275831436eef5f6fd39772c312d0abd3f3e3
pylbrecht <pylbrecht@mailbox.org>
2024-05-22 20:39:43 +0200

    Implement StatusBarWidget for KeyString

--- qutebrowser/mainwindow/statusbar/keystring.py
+++ qutebrowser/mainwindow/statusbar/keystring.py
@@ -4,15 +4,22 @@
 
 """Keychain string displayed in the statusbar."""
 
+from qutebrowser.mainwindow.statusbar.widget import StatusBarWidget
 from qutebrowser.qt.core import pyqtSlot
 
 from qutebrowser.mainwindow.statusbar import textbase
 from qutebrowser.utils import usertypes
 
 
-class KeyString(textbase.TextBase):
+class KeyString(StatusBarWidget, textbase.TextBase):
 
     """Keychain string displayed in the statusbar."""
+
+    def enable(self):
+        self.show()
+
+    def disable(self):
+        self.hide()
 
     @pyqtSlot(usertypes.KeyMode, str)
     def on_keystring_updated(self, _mode, keystr):
