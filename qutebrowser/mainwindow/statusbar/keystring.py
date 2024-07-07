3f17485ab555b25ade901b70fea53f802ada5412
pylbrecht <pylbrecht@mailbox.org>
2024-07-07 14:46:57 +0200

    Move KeyString's enable/disable logic to separate class

--- qutebrowser/mainwindow/statusbar/keystring.py
+++ qutebrowser/mainwindow/statusbar/keystring.py
@@ -10,10 +10,21 @@ from qutebrowser.mainwindow.statusbar import textbase
 from qutebrowser.utils import usertypes
 
 
-class KeyString(textbase.TextBase):
+class KeyStringWidget(textbase.TextBase):
 
     """Keychain string displayed in the statusbar."""
 
     @pyqtSlot(usertypes.KeyMode, str)
     def on_keystring_updated(self, _mode, keystr):
         self.setText(keystr)
+
+
+class KeyString:
+    def __init__(self, widget: KeyStringWidget):
+        self.widget = widget
+
+    def enable(self):
+        self.widget.show()
+
+    def disable(self):
+        self.widget.hide()
