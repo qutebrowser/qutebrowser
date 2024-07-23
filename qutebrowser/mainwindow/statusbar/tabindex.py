ff5ad0d791a30d6bed342f0ecec50d3941eca4ef
pylbrecht <pylbrecht@mailbox.org>
2024-07-23 21:32:53 +0200

    Move logic from TabIndexWidget to TabIndex

--- qutebrowser/mainwindow/statusbar/tabindex.py
+++ qutebrowser/mainwindow/statusbar/tabindex.py
@@ -4,21 +4,16 @@
 
 """TabIndex displayed in the statusbar."""
 
-from qutebrowser.mainwindow.statusbar.item import StatusBarItem
-from qutebrowser.qt.core import pyqtSlot
-
 from qutebrowser.mainwindow.statusbar import textbase
+from qutebrowser.mainwindow.statusbar.item import StatusBarItem
 
 
 class TabIndexWidget(textbase.TextBaseWidget):
 
     """Shows current tab index and number of tabs in the statusbar."""
 
-    @pyqtSlot(int, int)
-    def on_tab_index_changed(self, current, count):
-        """Update tab index when tab changed."""
-        self.setText('[{}/{}]'.format(current + 1, count))
-
 
 class TabIndex(StatusBarItem):
-    pass
+    def on_tab_index_changed(self, current, count):
+        """Update tab index when tab changed."""
+        self.widget.setText('[{}/{}]'.format(current + 1, count))
--- tests/unit/mainwindow/statusbar/test_tabindex.py
+++ tests/unit/mainwindow/statusbar/test_tabindex.py
@@ -6,17 +6,17 @@
 
 import pytest
 
-from qutebrowser.mainwindow.statusbar.tabindex import TabIndexWidget
+from qutebrowser.mainwindow.statusbar.tabindex import TabIndex, TabIndexWidget
 
 
 @pytest.fixture
 def tabindex(qtbot):
-    widget = TabIndexWidget()
-    qtbot.add_widget(widget)
+    widget = TabIndex(widget=TabIndexWidget())
+    qtbot.add_widget(widget.widget)
     return widget
 
 
 def test_tab_change(tabindex):
     """Make sure the tab index gets set correctly when switching tabs."""
     tabindex.on_tab_index_changed(0, 2)
-    assert tabindex.text() == '[1/2]'
+    assert tabindex.widget.text() == '[1/2]'
