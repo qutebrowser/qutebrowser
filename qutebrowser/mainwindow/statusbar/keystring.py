0af091462fba9947df1775b5f414e51b4daa99c9
pylbrecht <pylbrecht@mailbox.org>
2024-07-23 21:40:54 +0200

    Move logic from KeyStringWidget to KeyString

--- qutebrowser/mainwindow/statusbar/keystring.py
+++ qutebrowser/mainwindow/statusbar/keystring.py
@@ -5,7 +5,6 @@
 """Keychain string displayed in the statusbar."""
 
 from qutebrowser.mainwindow.statusbar.item import StatusBarItem
-from qutebrowser.qt.core import pyqtSlot
 
 from qutebrowser.mainwindow.statusbar import textbase
 from qutebrowser.utils import usertypes
@@ -15,10 +14,7 @@ class KeyStringWidget(textbase.TextBaseWidget):
 
     """Keychain string displayed in the statusbar."""
 
-    @pyqtSlot(usertypes.KeyMode, str)
-    def on_keystring_updated(self, _mode, keystr):
-        self.setText(keystr)
-
 
 class KeyString(StatusBarItem):
-    pass
+    def on_keystring_updated(self, _mode: usertypes.KeyMode, keystr: str):
+        self.widget.setText(keystr)
