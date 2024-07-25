c65b057f461adabed3f485cf9d78e45fb66cc23c
pylbrecht <pylbrecht@mailbox.org>
2024-07-25 21:26:22 +0200

    Move logic from PercentageWidget to Percentage

--- qutebrowser/mainwindow/statusbar/bar.py
+++ qutebrowser/mainwindow/statusbar/bar.py
@@ -411,7 +411,7 @@ class StatusBar(QWidget):
         """Notify sub-widgets when the tab has been changed."""
         self.url.widget.on_tab_changed(tab)
         self.prog.on_tab_changed(tab)
-        self.percentage.widget.on_tab_changed(tab)
+        self.percentage.on_tab_changed(tab)
         self.backforward.on_tab_changed(tab)
         self.maybe_hide()
         assert tab.is_private == self._color_flags.private
--- qutebrowser/mainwindow/statusbar/percentage.py
+++ qutebrowser/mainwindow/statusbar/percentage.py
@@ -5,7 +5,7 @@
 """Scroll percentage displayed in the statusbar."""
 
 from qutebrowser.mainwindow.statusbar.item import StatusBarItem
-from qutebrowser.qt.core import pyqtSlot, Qt
+from qutebrowser.qt.core import Qt
 
 from qutebrowser.mainwindow.statusbar import textbase
 from qutebrowser.misc import throttle
@@ -17,23 +17,20 @@ class PercentageWidget(textbase.TextBaseWidget):
     """Reading percentage displayed in the statusbar."""
 
     def __init__(self, parent=None):
-        """Constructor. Set percentage to 0%."""
         super().__init__(parent, elidemode=Qt.TextElideMode.ElideNone)
+
+
+class Percentage(StatusBarItem):
+    def __init__(self, widget: PercentageWidget):
+        """Constructor. Set percentage to 0%."""
+        super().__init__(widget)
         self._strings = self._calc_strings()
-        self._set_text = throttle.Throttle(self.setText, 100, parent=self)
+        self._set_text = throttle.Throttle(self.widget.setText, 100, parent=self.widget)
         self.set_perc(0, 0)
 
     def set_raw(self):
         self._strings = self._calc_strings(raw=True)
 
-    def _calc_strings(self, raw=False):
-        """Pre-calculate strings for the statusbar."""
-        fmt = '[{:02}]' if raw else '[{:02}%]'
-        strings = {i: fmt.format(i) for i in range(1, 100)}
-        strings.update({0: '[top]', 100: '[bot]'})
-        return strings
-
-    @pyqtSlot(int, int)
     def set_perc(self, x, y):
         """Setter to be used as a Qt slot.
 
@@ -48,7 +45,9 @@ class PercentageWidget(textbase.TextBaseWidget):
         """Update scroll position when tab changed."""
         self.set_perc(*tab.scroller.pos_perc())
 
-
-class Percentage(StatusBarItem):
-    def set_raw(self):
-        self.widget.set_raw()
+    def _calc_strings(self, raw=False):
+        """Pre-calculate strings for the statusbar."""
+        fmt = '[{:02}]' if raw else '[{:02}%]'
+        strings = {i: fmt.format(i) for i in range(1, 100)}
+        strings.update({0: '[top]', 100: '[bot]'})
+        return strings
