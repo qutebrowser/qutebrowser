3a3c03a4df4f7aed9dcc77538949a3370a22a24a
Ruben Gonzalez <neorubenx@gmail.com>
2024-12-06 18:49:23 +0200

    In POSIX sh, echo flags are undefined

--- scripts/open_url_in_instance.sh
+++ scripts/open_url_in_instance.sh
@@ -5,7 +5,7 @@
 _url="$1"
 _qb_version='1.0.4'
 _proto_version=1
-_ipc_socket="${XDG_RUNTIME_DIR}/qutebrowser/ipc-$(echo -n "$USER" | md5sum | cut -d' ' -f1)"
+_ipc_socket="${XDG_RUNTIME_DIR}/qutebrowser/ipc-$(printf '%s' "$USER" | md5sum | cut -d' ' -f1)"
 _qute_bin="/usr/bin/qutebrowser"
 
 printf '{"args": ["%s"], "target_arg": null, "version": "%s", "protocol_version": %d, "cwd": "%s"}\n' \
