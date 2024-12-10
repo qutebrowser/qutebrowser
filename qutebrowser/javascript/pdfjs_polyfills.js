c462cf62e4a2b4bbb89c5072a824ef716c7f7a59
Florian Bruhin <me@the-compiler.org>
2024-12-10 22:15:00 +0100

    pdfjs: Add some comments to polyfills

--- qutebrowser/javascript/pdfjs_polyfills.js
+++ qutebrowser/javascript/pdfjs_polyfills.js
@@ -1,7 +1,14 @@
 /* eslint-disable strict */
 /* (this file gets used as a snippet) */
 
+/*
+SPDX-FileCopyrightText: Florian Bruhin (The Compiler) <mail@qutebrowser.org>
+SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
 (function() {
+    // Chromium 119 / QtWebEngine 6.8
+    // https://caniuse.com/mdn-javascript_builtins_promise_withresolvers
     if (typeof Promise.withResolvers === "undefined") {
         Promise.withResolvers = function() {
             let resolve, reject
