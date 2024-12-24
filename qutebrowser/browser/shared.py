f2493a2a29bb65852afd486960dbbb56425c92b2
Twix <83093438+Twix53791@users.noreply.github.com>
2024-12-24 16:20:41 +0100

    Update shared.py

--- qutebrowser/browser/shared.py
+++ qutebrowser/browser/shared.py
@@ -570,11 +570,10 @@ def _validated_selected_files(
             message.warning("More than one file/folder chosen, using only the first")
             selected_files = selected_files[:1]
     for selected_file in selected_files:
-        # if not os.path.exists(selected_file) and qb_mode != FileSelectionMode.download:
-        if not os.path.exists(selected_file):
+        if not os.path.exists(selected_file) and qb_mode != FileSelectionMode.download:
             message.warning(f"Ignoring non-existent file '{selected_file}'")
             continue
-        if qb_mode == FileSelectionMode.folder or qb_mode == FileSelectionMode.download:
+        if qb_mode == FileSelectionMode.folder:
             if not os.path.isdir(selected_file):
                 message.warning(
                     f"Expected folder but got file, ignoring '{selected_file}'"
@@ -582,8 +581,7 @@ def _validated_selected_files(
                 continue
         else:
             # pylint: disable=else-if-used
-            # if qb_mode != FileSelectionMode.download and not os.path.isfile(selected_file):
-            if not os.path.isfile(selected_file):
+            if qb_mode != FileSelectionMode.download and not os.path.isfile(selected_file):
                 message.warning(
                     f"Expected file but got folder, ignoring '{selected_file}'"
                 )
