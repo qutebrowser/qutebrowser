68a987adc4fb55856887212f00baa7a1f76ee0c4
Twix <83093438+Twix53791@users.noreply.github.com>
2024-12-24 13:45:45 +0100

    syntax

--- qutebrowser/browser/shared.py
+++ qutebrowser/browser/shared.py
@@ -582,7 +582,7 @@ def _validated_selected_files(
         else:
             # pylint: disable=else-if-used
             # if qb_mode != FileSelectionMode.download and not os.path.isfile(selected_file):
-            if not os.path.isfile(selected_file:
+            if not os.path.isfile(selected_file):
                 message.warning(
                     f"Expected file but got folder, ignoring '{selected_file}'"
                 )
