2aa74caa64eae719e70914f619df80ea269ef8f4
Gabriel <gr20052512@gmail.com>
2025-01-25 14:40:54 -0300

    scripts: look for a file in case there is no url

--- scripts/open_url_in_instance.sh
+++ scripts/open_url_in_instance.sh
@@ -3,13 +3,20 @@
 # author: Thore Bödecker (foxxx0)
 
 _url="$(echo $@ | grep -oE '\w+://.+')"
+if [ -z $_url ]; then
+	for _el in $@; do
+		if [ -e $_el ]; then
+			_url=$_el
+		fi
+	done
+fi
 _qb_version='1.0.4'
 _proto_version=1
 _ipc_socket="${XDG_RUNTIME_DIR}/qutebrowser/ipc-$(printf '%s' "$USER" | md5sum | cut -d' ' -f1)"
 _qute_bin="/usr/bin/qutebrowser"
 
 printf '{"args": ["%s"], "target_arg": null, "version": "%s", "protocol_version": %d, "cwd": "%s"}\n' \
-       "${_url}" \
-       "${_qb_version}" \
-       "${_proto_version}" \
-       "${PWD}" | socat -lf /dev/null - UNIX-CONNECT:"${_ipc_socket}" || "$_qute_bin" "$@" &
+	"${_url}" \
+	"${_qb_version}" \
+	"${_proto_version}" \
+	"${PWD}" | socat -lf /dev/null - UNIX-CONNECT:"${_ipc_socket}" || "$_qute_bin" "$@" &
