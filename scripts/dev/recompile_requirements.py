161ce7bfcef309f23f66c59609aa62a7030f6c42
toofar <toofar@spalge.com>
2025-03-29 17:57:00 +1300

    Allow setuptools and pip in compiled requirements files
    This aligns with the direction of pip-compile and uv, and removes some
    code from this script. See:
    
        https://github.com/astral-sh/uv/issues/1353
        https://github.com/jazzband/pip-tools/issues/989#issuecomment-1134985118
    
    The code being removed worked fine. So if it turns out that installing a
    newer setuptools or pip in virtualenvs break stuff, it's fine to add
    back.

--- scripts/dev/recompile_requirements.py
+++ scripts/dev/recompile_requirements.py
@@ -400,10 +400,6 @@ def get_updated_requirements_compile(filename, venv_dir, comments):
 
         run_pip(venv_dir, 'install', '-U', 'uv', quiet=not utils.ON_CI)
         uv_command = "uv pip compile -U".split()
-        if os.path.basename(filename) != "requirements-tox.txt-raw":
-            # Ignore pip dependencies to match pip freeze.
-            for package in ["pip", "setuptools"]:
-                uv_command += ["--no-emit-package", package]
         if comments["pre"]:
             uv_command.extend("--prerelease allow".split())
         # Other interesting options:
