f98a967b21c15bf17d491994723954b550dd6a4b
toofar <toofar@spalge.com>
2025-06-06 16:19:23 +1200

    Add test for aur packaging workflow.
    Previously I've added comparison methods for testing alternate direct
    implementations of setup.py. Now I'm working through the actual
    packaging workflows that I can find.
    
    The source I am following is here: https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=qutebrowser-git
    
    For the replacement I've substituted:
    
    * setup.py build -> python3 -m build
        * I think these are a little different because `setup.py build` puts
          all the source file on disk, but build outputs a wheel and
          tarball.
    * setup.py install -> pip install
        * It's a bit awkward using a different frontend than the other
          makefile target (build vs pip) but with build we would have to
          call pip to install the wheel anyway. On the other hand, pip
          doesn't seem to have an option to just create a wheel
        * The only difference in outputs is that previously it was passing
          `--optimize=1` to setup.py, pip install doesn't seem to have an
          option to pass a specific optimization level
    
    The diff is included in the source code currently, I'll move it to a GH
    comment when I've gone through a few more workflows.

--- scripts/test_build.sh
+++ scripts/test_build.sh
@@ -8,7 +8,7 @@ echo "'after' branch: $cur_branch"
 echo "'before' branch: main"
 
 cd "$work_tree"
-git stash
+stash_output="$(git stash)"
 cd -
 
 scratch_dir="/tmp/qute_test_build"
@@ -33,7 +33,8 @@ cd "$scratch_dir"
 # Can also compare `pip3 show -v qutebrowser`
 # TODO: look at updating adapting makefile and packaging workflows
 #
-# Example packing workflows:
+# Example packing workflows (review changes in output and any changes to build
+#   commands needed):
 # * https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=qutebrowser-git
 # * https://gitlab.archlinux.org/archlinux/packaging/packages/qutebrowser/-/blob/main/PKGBUILD?ref_type=heads
 # * https://github.com/qutebrowser/qutebrowser-debian/blob/master/debian/rules
@@ -61,7 +62,7 @@ run_in_venv() {
   python3 -m venv "$venv"
   activate "$venv"
   checkout "$branch" >&2
-  $cmd >&2
+  eval $cmd >&2
   cd "$scratch_dir/$venv"
   ls -R > ../"$venv".list
   cd - >&2
@@ -82,8 +83,6 @@ compare_editable() {
   file_list_after="$(run_in_venv after "$cur_branch" "pip install -e $work_tree")"
   diff -u "$file_list_before" "$file_list_after"
 }
-
-compare_editable
 
 ################
 # setup.py build vs python3 -m build --wheel
@@ -114,8 +113,6 @@ compare_build() {
   diff -u build_before.list build_after.list
 }
 
-compare_build
-
 ################
 # setup.py install
 ################
@@ -129,6 +126,7 @@ setuppy_install() {
 compare_install() {
   file_list_before="$(run_in_venv before main setuppy_install)"
 
+  # Could also do `pip install -t ... .` and skip the separate wheel build.
   python3 -m venv build
   activate build
   pip3 install build setuptools
@@ -142,10 +140,123 @@ compare_install() {
   diff -u "$file_list_before" "$file_list_after"
 }
 
-compare_install
+################
+# qutebrowser-git aur package
+################
+aur_package() {
+  dest="$1"
+  # This package script is split into two parts, build and package.
+  # The build part runs `setup.py build`, but the package part seems to call
+  # that as a pre-req of `setup.py install` anyway, so I'm not sure what the
+  # point of it is.
+  # The package step install the python package to a directory (using a
+  # makefile command).
+  # Tentative recommendation:
+  # * remove setup.py invocation from build step if not needed
+  # * change makefile command to use `pip3 install -t ... .`
+
+  # https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=qutebrowser-git
+  cd "$work_tree"
+  rm -r build dest
+  python3 -m pip install asciidoc
+  # "build" -- this is the same as compare_build but we prepare docs too
+  # Requires asciidoc and docbook-xsl
+  python scripts/asciidoc2html.py
+  a2x -L -f manpage doc/qutebrowser.1.asciidoc
+  # The PKGBUILD calls setup.py directly. I'm deferring to the makefile here
+  # so that we can run a different command based on the branch we are on. Also
+  # this step doesn't seem to affect the package output.
+  make -f misc/Makefile all
+
+  # "package"
+  # This looks like it does `setup.py build` as part of `install` anyway
+  # Requires distutils for byte compiling with `setup.py install`
+  # Prints a banner warning saying setup.py install will stop working October
+  # 31 and points to: https://blog.ganssle.io/articles/2021/10/setup-py-deprecated.html
+  # Neither pip install of build let you specify an optimisation level for
+  #   byte compiling.
+  make -f misc/Makefile DESTDIR="$dest" PREFIX=/usr install
+
+  git reset --hard HEAD
+}
+compare_package_aur() {
+  # This (setup.py install) sometimes fails when byte compiling with
+  # "ModuleNotFoundError: No module named 'distutils'". I'm not sure why,
+  # sometimes it succeeds though!
+  run_in_venv before main "python3 -m pip install setuptools && aur_package $scratch_dir/aur-before"
+  cd "$scratch_dir/aur-before"
+  # Remove optimized compiled files to make the diff smaller -- known
+  # difference.
+  ls -R | grep -v "opt-1.pyc" > ../aur-before.list
+  cd -
+
+  run_in_venv after "$cur_branch" "aur_package $scratch_dir/aur-after"
+  cd "$scratch_dir/aur-after"
+  ls -R > ../aur-after.list
+
+  diff -u ../aur-before.list ../aur-after.list
+  cd -
+
+  # --- ../aur-before.list  2025-06-08 16:11:00.425813175 +1200
+  # +++ ../aur-after.list   2025-06-08 16:11:17.941359552 +1200
+  # @@ -17,7 +17,7 @@
+  #
+  #  ./usr/lib/python3.13/site-packages:
+  #  qutebrowser
+  # -qutebrowser-3.5.0-py3.13.egg-info
+  # +qutebrowser-3.5.0.dist-info
+  #
+  #  ./usr/lib/python3.13/site-packages/qutebrowser:
+  #  api
+  # @@ -727,14 +727,19 @@
+  #  utils.cpython-313.pyc
+  #  version.cpython-313.pyc
+  #
+  # -./usr/lib/python3.13/site-packages/qutebrowser-3.5.0-py3.13.egg-info:
+  # -dependency_links.txt
+  # +./usr/lib/python3.13/site-packages/qutebrowser-3.5.0.dist-info:
+  # +direct_url.json
+  #  entry_points.txt
+  # -PKG-INFO
+  # -requires.txt
+  # -SOURCES.txt
+  # +INSTALLER
+  # +licenses
+  # +METADATA
+  # +RECORD
+  # +REQUESTED
+  #  top_level.txt
+  # -zip-safe
+  # +WHEEL
+  # +
+  # +./usr/lib/python3.13/site-packages/qutebrowser-3.5.0.dist-info/licenses:
+  # +LICENSE
+  #
+  #  ./usr/share:
+  #  applications
+  # @@ -837,6 +842,7 @@
+  #  mkvenv.py
+  #  opengl_info.py
+  #  open_url_in_instance.sh
+  # +test_build.sh
+  #  utils.py
+  #
+  #  ./usr/share/qutebrowser/userscripts:
+}
+
+
+# Comparisons of primitive build/install commands https://github.com/qutebrowser/qutebrowser/pull/8560#issuecomment-2934333806
+#compare_editable
+#compare_build
+#compare_install
+
+# Comparisons of packaging workflows
+compare_package_aur
 
 # Restore work tree
 cd "$work_tree"
 checkout "$cur_branch"
-git stash pop
+if [ "$stash_output" != "No local changes to save" ] ;then
+  git stash pop
+fi
 
