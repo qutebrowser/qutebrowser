45bcc7833d11b5fdef6d75b117c897173a9af0f6
Freya Bruhin <me@the-compiler.org>
2026-02-26 16:37:23 +0100

    Drop unnecessary hasattr()
    See https://foss.heptapod.net/mercurial/tortoisehg/thg/-/commit/1b8a661ebcfc180c04c02c790c7471f3d19cd084
    (where we originally got that code from, in 210ce8ca7cb16e1d0ce859baa64ce56a5e1894cf).
    
    Thanks to killiandesse for spotting this!
    
    Closes #8880

--- qutebrowser/misc/crashsignal.py
+++ qutebrowser/misc/crashsignal.py
@@ -352,7 +352,7 @@ class SignalHandler(QObject):
         for sig, handler in self._handlers.items():
             self._orig_handlers[sig] = signal.signal(sig, handler)
 
-        if utils.is_posix and hasattr(signal, 'set_wakeup_fd'):
+        if utils.is_posix:
             # pylint: disable=import-error,no-member,useless-suppression
             import fcntl
             read_fd, write_fd = os.pipe()
